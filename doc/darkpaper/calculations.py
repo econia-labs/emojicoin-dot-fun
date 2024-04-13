1a4315ebc8f0cd3bc910cd9719aed5c8a21864a6
alnoki <43892045+alnoki@users.noreply.github.com>
2024-04-13 15:27:31 -0700

    Sort sections, add more CPAMM content

--- doc/darkpaper/calculations.py
+++ doc/darkpaper/calculations.py
@@ -3,6 +3,7 @@ from math import sqrt
 M_A = 10_000
 C_E = 750_000
 P_S = 0.01
+SCALE_TO_SUBUNITS = 10**8
 
 assert M_A > C_E * P_S, f"M_A must be greater than {C_E * P_S}"
 assert M_A < 2 * C_E * P_S, f"M_A must be less than {2 * C_E * P_S}"
@@ -39,6 +40,7 @@ def get_q_v_c(m_a, c_e, p_s):
 def get_p_l(m_a, c_e, p_s):
     return ((m_a - c_e * p_s) ** 2) / ((c_e**2) * p_s)
 
+
 def get_L_i(m_a, c_e, p_s):
     return (m_a - c_e * p_s) / sqrt(p_s)
 
@@ -68,6 +70,20 @@ print(f"b_v_c: {b_v_c}")
 print(f"q_v_c: {q_v_c}")
 print(f"p_l: {p_l}")
 print(f"L_i: {L_i}")
+print()
+
+print(f"MARKET_CAP: {int(M_A * SCALE_TO_SUBUNITS): _}")
+print(f"EMOJICOIN_REMAINDER: {int(r_e * SCALE_TO_SUBUNITS): _}")
+print(f"EMOJICOIN_SUPPLY: {int(s_e * SCALE_TO_SUBUNITS): _}")
+print(f"LP_TOKENS_INITIAL: {int(L_i * SCALE_TO_SUBUNITS): _}")
+print(f"BASE_REAL_FLOOR: {int(0): _}")
+print(f"QUOTE_REAL_FLOOR: {int(0): _}")
+print(f"BASE_REAL_CEILING: {int(C_E * SCALE_TO_SUBUNITS): _}")
+print(f"QUOTE_REAL_CEILING: {int(q_r_c * SCALE_TO_SUBUNITS): _}")
+print(f"BASE_VIRTUAL_FLOOR: {int(b_v_f * SCALE_TO_SUBUNITS): _}")
+print(f"QUOTE_VIRTUAL_FLOOR: {int(q_v_f * SCALE_TO_SUBUNITS): _}")
+print(f"BASE_VIRTUAL_CEILING: {int(b_v_f * SCALE_TO_SUBUNITS): _}")
+print(f"QUOTE_VIRTUAL_CEILING: {int(q_v_f * SCALE_TO_SUBUNITS): _}")
 
 # Check assorted systems of equations.
 assert q_r_c / P_S == r_e
