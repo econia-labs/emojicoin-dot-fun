80d33647c362f9a16393a5d3340b0d4dd4300145
alnoki <43892045+alnoki@users.noreply.github.com>
2024-04-13 14:09:40 -0700

    Add assorted tweaks, initial LP token amounts

--- doc/darkpaper/calculations.py
+++ doc/darkpaper/calculations.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 M_A = 10_000
 C_E = 750_000
 P_S = 0.01
@@ -37,6 +39,9 @@ def get_q_v_c(m_a, c_e, p_s):
 def get_p_l(m_a, c_e, p_s):
     return ((m_a - c_e * p_s) ** 2) / ((c_e**2) * p_s)
 
+def get_L_i(m_a, c_e, p_s):
+    return (m_a - c_e * p_s) / sqrt(p_s)
+
 
 q_r_c = get_q_r_c(M_A, C_E, P_S)
 r_e = get_r_e(M_A, C_E, P_S)
@@ -46,6 +51,7 @@ q_v_f = get_q_v_f(M_A, C_E, P_S)
 b_v_c = get_b_v_c(M_A, C_E, P_S)
 q_v_c = get_q_v_c(M_A, C_E, P_S)
 p_l = get_p_l(M_A, C_E, P_S)
+L_i = get_L_i(M_A, C_E, P_S)
 
 
 print(f"M_A: {M_A}")
@@ -61,6 +67,7 @@ print(f"q_v_f: {q_v_f}")
 print(f"b_v_c: {b_v_c}")
 print(f"q_v_c: {q_v_c}")
 print(f"p_l: {p_l}")
+print(f"L_i: {L_i}")
 
 # Check assorted systems of equations.
 assert q_r_c / P_S == r_e
@@ -75,3 +82,4 @@ assert q_v_c == q_r_c + q_v_f
 assert p_l == q_v_f / b_v_c
 assert C_E == (b_v_c * q_r_c) / (q_v_f + q_r_c)
 assert q_r_c == (C_E * q_v_c) / (b_v_f + C_E)
+assert L_i == sqrt(q_r_c * r_e)
