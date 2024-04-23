f5c753a627192434370c5d95557c7c3e4bad7a48
xbtmatt <xbtchunky@gmail.com>
2024-04-23 01:28:30 -0700

    Change views.test.ts to use more sensible inputs for test

--- src/typescript/tests/e2e/views.test.ts
+++ src/typescript/tests/e2e/views.test.ts
@@ -1,4 +1,4 @@
-import { Account, Network } from "@aptos-labs/ts-sdk";
+import { Account, type HexInput, Network } from "@aptos-labs/ts-sdk";
 import { getAptosClient } from "../../src/helpers/aptos-client";
 import { fundAccounts } from "../../src/helpers/fund-accounts";
 import { publishPackage } from "../../src/cli/publish";
@@ -7,7 +7,7 @@ import * as EmojicoinDotFun from "../../src/emojicoin_dot_fun/emojicoin-dot-fun"
 
 jest.setTimeout(30000);
 
-describe("registers a market successfully", () => {
+describe("it tests view functions", () => {
   const { aptos } = getAptosClient();
   const publisher = Account.generate();
 
@@ -16,7 +16,7 @@ describe("registers a market successfully", () => {
     await fundAccounts(aptos, [publisher]);
   });
 
-  it("publishes the emojicoin_dot_fun smart contract", async () => {
+  it("it tests several different emojis that are and aren't supported", async () => {
     const moduleName = EMOJICOIN_DOT_FUN_MODULE_NAME;
     const packageName = moduleName;
     const publishResult = await publishPackage({
@@ -36,42 +36,20 @@ describe("registers a market successfully", () => {
     await aptos.waitForTransaction({ transactionHash });
     const moduleAddress = publisher.accountAddress.toString();
 
-    const emojis = [
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "e298afefb88f" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({
-        moduleAddress,
-        hex_bytes: new Uint8Array([240, 159, 167, 159]),
-      })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "f09faa80" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "f09fa693" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "f09fa611" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "f09fa691" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "f09f8f9fefb88f" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "e2ad90" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "e2ad90ff" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-      new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: "00e2ad90ff" })
-        .submit({ aptos })
-        .then((resp) => resp[0]),
-    ];
-    const supported = await Promise.all(emojis);
-    expect(supported).toEqual([true, true, true, true, false, true, true, true, false, false]);
+    const testIsSupported = async (hex: HexInput, expected: boolean) => {
+      const view = new EmojicoinDotFun.IsASupportedEmoji({ moduleAddress, hex_bytes: hex });
+      expect(await view.submit({ aptos })).toEqual([expected]);
+    };
+
+    await testIsSupported("e298afefb88f", true);
+    await testIsSupported(new Uint8Array([240, 159, 167, 159]), true);
+    await testIsSupported("f09faa80", true);
+    await testIsSupported("f09fa693", true);
+    await testIsSupported("f09fa611", false);
+    await testIsSupported("f09fa691", true);
+    await testIsSupported("f09f8f9fefb88f", true);
+    await testIsSupported("e2ad90", true);
+    await testIsSupported("e2ad90ff", false);
+    await testIsSupported("00e2ad90ff", false);
   });
 });
