fc5d8872b16cdde602ed68aa91ad9e2f6dedc2cc
xbtmatt <xbtchunky@gmail.com>
2024-05-15 12:10:56 -0700

    Fix build errors due to lack of index file in home/components
    Fix build errors from import change

--- src/typescript/frontend/src/components/pages/index.ts
+++ src/typescript/frontend/src/components/pages/index.ts
@@ -1,6 +1,5 @@
 export * from "./not-found";
 export * from "./connect-wallet";
 export * from "./emojicoin";
-export * from "./home/components";
 export * from "./launch-emojicoin";
 export * from "./pools/components";
