6964e59e5100f93d06a1ec9a7e50b069b3b3d909
matt <xbtmatt@gmail.com>
2024-05-19 23:53:16 -0700

    Rename `api` to `sdk`
    Add `.gitmodules`
    
    Update meta files like `package.json`, `.eslintrc.js`, `.prettierignore` etc

---
+++ src/typescript/sdk/.prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+  bracketSpacing: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  singleQuote: false,
+  arrowParens: "always",
+  printWidth: 100,
+};
+
+module.exports = config;
