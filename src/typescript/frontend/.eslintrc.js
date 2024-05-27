22af026c97bca47aca1cbee2cd9811e88c4301e4
matt <xbtmatt@gmail.com>
2024-05-26 21:49:26 -0700

    Remove storybook, remove it from package.json and pnpm-lock.yaml
    Add full-clean command to turbo repo for faster clean builds

--- src/typescript/frontend/.eslintrc.js
+++ src/typescript/frontend/.eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "plugin:storybook/recommended",
   ],
   globals: {
     JSX: true,
--- src/typescript/frontend/src/components/loader/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader as StyledLoader } from "components";
-
-export default {
-  title: "Components/Loader",
-};
-
-export const Loader: React.FC = () => {
-  return <StyledLoader />;
-};
