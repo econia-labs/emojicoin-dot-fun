009b9b695da1f7fa82eb6ce4b2b6f710e4ffe3b7
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-05-26 22:29:41 -0700

    [ECO-1738] Remove storybook, remove it from `package.json` and `pnpm-lock.yaml` (#81)
    Co-authored-by: matt <xbtmatt@gmail.com>

--- src/typescript/frontend/src/components/error-boundary/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ErrorBoundaryFallback as StyledErrorBoundaryFallback } from "components";
-
-export default {
-  title: "Components/ErrorBoundaryFallback",
-};
-
-export const ErrorBoundaryFallback: React.FC = () => {
-  return <StyledErrorBoundaryFallback error={new Error("Error")} resetError={() => null} />;
-};
