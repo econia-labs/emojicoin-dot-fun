0cd040dae1c912c6a5064655a0e669aa6ee7dc84
matt <xbtmatt@gmail.com>
2024-05-28 10:48:03 -0700

    Test build debug in prod

--- src/typescript/frontend/next.config.mjs
+++ src/typescript/frontend/next.config.mjs
@@ -1,45 +1,49 @@
 // @ts-check
 
-const isDebug = process.env.BUILD_DEBUG === 'true';
+const isDebug = process.env.BUILD_DEBUG === "true";
 // Turbo must be off for the below.
 const styledComponentConfigOptions = {
-    displayName: true,
-    ssr: true,
-    fileName: true,
-    minify: false,
+  displayName: true,
+  ssr: true,
+  fileName: true,
+  minify: false,
+};
+if (isDebug) {
+  console.log("Using build debug.");
 }
 const debugConfigOptions = {
-    productionBrowserSourceMaps: true,
-    outputFileTracing: true,
-    swcMinify: false,
-    cleanDistDir: true,
-    experimental: {
-        serverMinification: false,
-        serverSourceMaps: true,
-    },
-    // styledComponents: isDebug ? { ...styledComponentConfigOptions } : true,
-}
+  productionBrowserSourceMaps: true,
+  outputFileTracing: true,
+  swcMinify: false,
+  cleanDistDir: true,
+  experimental: {
+    serverMinification: false,
+    serverSourceMaps: true,
+  },
+  // styledComponents: isDebug ? { ...styledComponentConfigOptions } : true,
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    crossOrigin: 'use-credentials',
-    typescript: {
-        tsconfigPath: 'tsconfig.json',
-    },
-    compiler: {
-        styledComponents: true
-    },
-    ...(isDebug ? debugConfigOptions : {}),
-    transpilePackages: ['@/sdk'],
-    redirects: async () => {
-        return [
-            {
-                source: '/emojicoin',
-                destination: '/emojicoin/0',
-                permanent: true,
-            },
-        ];
-    }
-}
+  crossOrigin: "use-credentials",
+  typescript: {
+    tsconfigPath: "tsconfig.json",
+  },
+  compiler: {
+    styledComponents: true,
+    removeConsole: false,
+  },
+  ...(isDebug ? debugConfigOptions : {}),
+  transpilePackages: ["@/sdk"],
+  redirects: async () => {
+    return [
+      {
+        source: "/emojicoin",
+        destination: "/emojicoin/0",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
