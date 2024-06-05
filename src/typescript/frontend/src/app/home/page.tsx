a98197eadc2e04c84925145e88ae26ad53aa0849
matt <xbtmatt@gmail.com>
2024-06-05 15:03:04 -0700

    Set revalidate to being short for home page

--- src/typescript/frontend/src/app/home/page.tsx
+++ src/typescript/frontend/src/app/home/page.tsx
@@ -1,6 +1,9 @@
 import ClientHomePage from "components/pages/home/ClientHomePage";
 import fetchMarketData from "lib/queries/initial/market-data";
 
+export const revalidate = process.env.SHORT_REVALIDATE === "true" ? 10 : 3600;
+export const dynamic = "auto";
+
 export default async function HomePage() {
   const data = await fetchMarketData();
 
--- src/typescript/sdk/tests/utils/aptos-client.ts
+++ src/typescript/sdk/tests/utils/aptos-client.ts
@@ -5,11 +5,11 @@ export function getAptosClient(additionalConfig?: Partial<AptosConfig>): {
   aptos: Aptos;
   config: AptosConfig;
 } {
-  const networkRaw = process.env.APTOS_NETWORK;
+  const networkRaw = process.env.NEXT_PUBLIC_APTOS_NETWORK;
   const network = networkRaw ? NetworkToNetworkName[networkRaw] : Network.LOCAL;
   if (!network) {
     throw new Error(
-      `Unknown network, confirm the APTOS_NETWORK environment variable is valid: ${networkRaw}`
+      `Unknown network, confirm the NEXT_PUBLIC_APTOS_NETWORK environment variable is valid: ${networkRaw}`
     );
   }
   const config = new AptosConfig({
