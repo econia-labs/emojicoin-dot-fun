b846eb6c2dbfc050964d944b2a7784c96425226b
matt <xbtmatt@gmail.com>
2024-06-05 01:54:04 -0700

    Automatically add events to the event state store when submitting a tx

--- src/typescript/frontend/src/components/pages/emojicoin/components/trade-emojicoin/components/SwapButton.tsx
+++ src/typescript/frontend/src/components/pages/emojicoin/components/trade-emojicoin/components/SwapButton.tsx
@@ -5,10 +5,6 @@ import { Swap } from "@sdk/emojicoin_dot_fun/emojicoin-dot-fun";
 import { useAptos } from "context/wallet-context/AptosContextProvider";
 import { INTEGRATOR_ADDRESS, INTEGRATOR_FEE_RATE_BPS } from "lib/env";
 import { toCoinTypes } from "@sdk/markets/utils";
-import { useEventStore } from "context/store-context";
-// import { isUserTransactionResponse } from "@aptos-labs/ts-sdk";
-// import { getEvents } from "@sdk/emojicoin_dot_fun";
-import { storeEvents } from "@store/event-utils";
 
 export const SwapButton = ({
   inputAmount,
@@ -21,8 +17,6 @@ export const SwapButton = ({
 }) => {
   const { t } = translationFunction();
   const { aptos, account, submit } = useAptos();
-  // const { addSwapEvent, addStateEvent, addPeriodicStateEvent } = useEventStore((state) => state);
-  const store = useEventStore((state) => state);
 
   const handleClick = async () => {
     if (!account) {
@@ -40,22 +34,7 @@ export const SwapButton = ({
         integratorFeeRateBps: INTEGRATOR_FEE_RATE_BPS,
         typeTags: [emojicoin, emojicoinLP],
       });
-    await submit(builderLambda).then((res) => {
-      storeEvents(store, res);
-      // if (res?.response && isUserTransactionResponse(res.response)) {
-      //   const events = getEvents(res.response);
-      //   events.stateEvents.forEach((state) => {
-      //     addStateEvent(state);
-      //   });
-      //   events.periodicStateEvents.forEach((periodicState) => {
-      //     addPeriodicStateEvent(periodicState);
-      //   });
-      //   events.swapEvents.forEach((swap) => {
-      //     addSwapEvent(swap);
-      //   });
-      // }
-      // return null;
-    });
+    await submit(builderLambda);
   };
 
   return (
