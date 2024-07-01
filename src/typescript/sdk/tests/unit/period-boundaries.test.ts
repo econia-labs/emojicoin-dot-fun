df0db5736197538ddc38d2c1229430d976552e0f
matt <xbtmatt@gmail.com>
2024-06-30 18:40:52 -0700

    Fix unit test linker error

--- src/typescript/sdk/tests/unit/period-boundaries.test.ts
+++ src/typescript/sdk/tests/unit/period-boundaries.test.ts
@@ -1,4 +1,4 @@
-import { CandlestickResolution, getPeriodBoundary } from "../../src";
+import { CandlestickResolution, getPeriodStartTime } from "../../src";
 import { SAMPLE_STATE_EVENT, SAMPLE_SWAP_EVENT } from "../utils/sample-data";
 
 const swap = SAMPLE_SWAP_EVENT;
@@ -19,24 +19,24 @@ const { PERIOD_1D } = CandlestickResolution;
 describe("tests period boundaries", () => {
   it("calculates the first period boundary ever correctly for a swap event", () => {
     swap.time = epochInMicroseconds;
-    expect(getPeriodBoundary(swap, PERIOD_1D) === 0n * BigInt(PERIOD_1D)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_4H) === 0n * BigInt(PERIOD_4H)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_1H) === 0n * BigInt(PERIOD_1H)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1D) === 0n * BigInt(PERIOD_1D)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_4H) === 0n * BigInt(PERIOD_4H)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1H) === 0n * BigInt(PERIOD_1H)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
   });
 
   it("calculates the first period boundary ever correctly for a state event", () => {
     state.lastSwap.time = epochInMicroseconds;
-    expect(getPeriodBoundary(state, PERIOD_1D) === 0n * BigInt(PERIOD_1D)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_4H) === 0n * BigInt(PERIOD_4H)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_1H) === 0n * BigInt(PERIOD_1H)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1D) === 0n * BigInt(PERIOD_1D)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_4H) === 0n * BigInt(PERIOD_4H)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1H) === 0n * BigInt(PERIOD_1H)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
   });
 
   it("calculates various period boundaries correctly for a swap event", () => {
@@ -46,39 +46,39 @@ describe("tests period boundaries", () => {
 
     // Put it right before the next period boundary.
     swap.time -= 1n;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     swap.time += 1n;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     swap.time += 1n;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     swap.time += 60n * microsecondFactor;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 2n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 2n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     // Add 3 minutes.
     swap.time += 3n * 60n * microsecondFactor;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 5n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 1n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 5n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 1n * BigInt(PERIOD_5M)).toEqual(true);
 
     // Add 10 minutes.
     swap.time += 10n * 60n * microsecondFactor;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 15n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 3n * BigInt(PERIOD_5M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_15M) === 1n * BigInt(PERIOD_15M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 15n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 3n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_15M) === 1n * BigInt(PERIOD_15M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
 
     // Subtract 1 second.
     swap.time -= 1n * microsecondFactor;
-    expect(getPeriodBoundary(swap, PERIOD_1M) === 14n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_5M) === 2n * BigInt(PERIOD_5M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
-    expect(getPeriodBoundary(swap, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_1M) === 14n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_5M) === 2n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
+    expect(getPeriodStartTime(swap, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
   });
 
   it("calculates various period boundaries correctly for a state event", () => {
@@ -88,38 +88,38 @@ describe("tests period boundaries", () => {
 
     // Put it right before the next period boundary.
     state.lastSwap.time -= 1n;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 0n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     state.lastSwap.time += 1n;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     state.lastSwap.time += 1n;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 1n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     state.lastSwap.time += 60n * microsecondFactor;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 2n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 2n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 0n * BigInt(PERIOD_5M)).toEqual(true);
 
     // Add 3 minutes.
     state.lastSwap.time += 3n * 60n * microsecondFactor;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 5n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 1n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 5n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 1n * BigInt(PERIOD_5M)).toEqual(true);
 
     // Add 10 minutes.
     state.lastSwap.time += 10n * 60n * microsecondFactor;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 15n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 3n * BigInt(PERIOD_5M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_15M) === 1n * BigInt(PERIOD_15M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 15n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 3n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_15M) === 1n * BigInt(PERIOD_15M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
 
     // Subtract 1 second.
     state.lastSwap.time -= 1n * microsecondFactor;
-    expect(getPeriodBoundary(state, PERIOD_1M) === 14n * BigInt(PERIOD_1M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_5M) === 2n * BigInt(PERIOD_5M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
-    expect(getPeriodBoundary(state, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_1M) === 14n * BigInt(PERIOD_1M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_5M) === 2n * BigInt(PERIOD_5M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_15M) === 0n * BigInt(PERIOD_15M)).toEqual(true);
+    expect(getPeriodStartTime(state, PERIOD_30M) === 0n * BigInt(PERIOD_30M)).toEqual(true);
   });
 });
