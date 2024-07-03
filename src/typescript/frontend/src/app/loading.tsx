3effacdf9530597832ee44b9b384a4184d289fcb
matt <xbtmatt@gmail.com>
2024-07-03 12:26:16 -0700

    Format

--- src/typescript/frontend/src/app/loading.tsx
+++ src/typescript/frontend/src/app/loading.tsx
@@ -5,7 +5,5 @@ import React from "react";
 import LoadingComponent from "components/loading";
 
 export default function Loading() {
-    return (
-        <LoadingComponent />
-    );
+  return <LoadingComponent />;
 }
--- src/typescript/frontend/src/components/loading.tsx
+++ src/typescript/frontend/src/components/loading.tsx
@@ -3,17 +3,23 @@ import { useScramble } from "use-scramble";
 import { Text } from "./text";
 
 export default function Loading() {
-    const { ref } = useScramble({
-        text: `Loading...`,
-        overdrive: false,
-        speed: 0.5,
-    });
-    return (
-        <div style={{
-            height: "100%",
-            width: "100%",
-            display: "grid",
-            placeItems: "center",
-        }}><Text textScale="pixelHeading3" color="econiaBlue" textTransform="uppercase" ref={ref}>Loading...</Text></div>
-    );
+  const { ref } = useScramble({
+    text: `Loading...`,
+    overdrive: false,
+    speed: 0.5,
+  });
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <Text textScale="pixelHeading3" color="econiaBlue" textTransform="uppercase" ref={ref}>
+        Loading...
+      </Text>
+    </div>
+  );
 }
