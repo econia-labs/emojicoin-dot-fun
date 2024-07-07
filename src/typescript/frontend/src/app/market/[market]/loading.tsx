3e8343346047ce358d730b5b8b799013db7be297
matt <xbtmatt@gmail.com>
2024-07-07 13:39:37 -0700

    Update the market loading indicator to use the last response emoji for registering a market

--- src/typescript/frontend/src/app/market/[market]/loading.tsx
+++ src/typescript/frontend/src/app/market/[market]/loading.tsx
@@ -3,7 +3,22 @@
 import React from "react";
 
 import LoadingComponent from "components/loading";
+import { useAptos } from "context/wallet-context/AptosContextProvider";
+import { type EntryFunctionPayloadResponse, isUserTransactionResponse } from "@aptos-labs/ts-sdk";
+import AnimatedStatusIndicator from "components/pages/launch-emojicoin/animated-status-indicator";
 
 export default function Loading() {
-  return <LoadingComponent />;
+  const { status, lastResponse } = useAptos();
+  const { response } = lastResponse ?? {};
+  const isRegisterMarket =
+    response &&
+    isUserTransactionResponse(response) &&
+    response.success &&
+    (response.payload as EntryFunctionPayloadResponse).function.endsWith("::register_market");
+
+  return (status === "pending" || status === "success") && isRegisterMarket ? (
+    <AnimatedStatusIndicator />
+  ) : (
+    <LoadingComponent />
+  );
 }
