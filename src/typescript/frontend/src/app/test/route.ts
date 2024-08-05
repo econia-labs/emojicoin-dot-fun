52151b54ee856bb259bb810a5d550d144c2ad055
Bogdan Crisan <bogdan@crbl.studio>
2024-08-05 13:54:52 +0200

    Add playwright with minimal test

---
+++ src/typescript/frontend/src/app/test/route.ts
@@ -0,0 +1,10 @@
+import { getAptos } from "lib/utils/aptos-client";
+import { NextResponse } from "next/server";
+
+export const revalidate = 2;
+
+export async function GET() {
+  const aptos = getAptos();
+  const version = await aptos.getLedgerInfo().then(res => res.ledger_version);
+  return new NextResponse(version.toString());
+}
