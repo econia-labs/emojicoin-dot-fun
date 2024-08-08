0c8a3278d1ae0d67d9b58b70b86d90ef2dde7c22
Bogdan Crisan <bogdan@crbl.studio>
2024-08-08 14:53:54 +0200

    Implement functionality for the provide liquidity button

--- src/typescript/frontend/src/components/pages/emojicoin/components/trade-emojicoin/LiquidityButton.tsx
+++ src/typescript/frontend/src/components/pages/emojicoin/components/trade-emojicoin/LiquidityButton.tsx
@@ -5,6 +5,7 @@ import Button from "components/button";
 import { translationFunction } from "context/language-context";
 import { isInBondingCurve } from "utils/bonding-curve";
 import { AnimatedProgressBar } from "./AnimatedProgressBar";
+import { Link } from "components";
 
 export const LiquidityButton = (props: GridProps) => {
   const { t } = translationFunction();
@@ -14,7 +15,9 @@ export const LiquidityButton = (props: GridProps) => {
       {!isInBondingCurve(props.data) ? (
         <StyledContentHeader>
           <Flex width="100%" justifyContent="center">
-            <Button scale="lg">{t("Provide liquidity")}</Button>
+            <Link href={`/pools?pool=${props.data.emojis.reduce((p, c) => `${p}${c.emoji}`, "")}`}>
+              <Button scale="lg">{t("Provide liquidity")}</Button>
+            </Link>
           </Flex>
         </StyledContentHeader>
       ) : (
