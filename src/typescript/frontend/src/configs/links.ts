b003a640cfe33ba08cd799f60c95d58bb6059672
Bogdan Crisan <bogdan@crbl.studio>
2024-08-28 12:20:49 +0200

    [ECO-2069] Add TOS and configurable links (#211)
    Co-authored-by: alnoki <43892045+alnoki@users.noreply.github.com>

--- src/typescript/frontend/src/configs/links.ts
+++
@@ -1,7 +0,0 @@
-export const LINKS = {
-  x: "https://x.com/EconiaLabs",
-  github: "https://github.com/econia-labs/emojicoin-dot-fun",
-  econia_labs: "https://econialabs.com/",
-  discord: "https://discord.gg/econia",
-  telegram: "https://discord.gg/econia",
-};
