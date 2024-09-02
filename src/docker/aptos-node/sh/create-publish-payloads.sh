4f539e5df79cc1c89c52387b3ac254c0b873a6d8
matt <xbtmatt@gmail.com>
2024-09-01 17:19:27 -0700

    Initial try of using state, currently broken. I believe it's an issue with processor architecture

---
+++ src/docker/aptos-node/sh/create-publish-payloads.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+root_dir=$(git rev-parse --show-toplevel)
+docker_dir="$root_dir/src/docker"
+move_dir="$root_dir/src/move"
+
+json_output_dir="$docker_dir/aptos-node/json"
+emojicoin_json_path="$json_output_dir/publish-emojicoin_dot_fun.json"
+rewards_json_path="$json_output_dir/publish-rewards.json"
+
+source $docker_dir/.env
+
+address="$EMOJICOIN_MODULE_ADDRESS"
+aptos move build-publish-payload \
+    --assume-yes \
+    --private-key $PUBLISHER_PK \
+    --encoding hex \
+    --named-addresses emojicoin_dot_fun=$address \
+    --override-size-check \
+    --included-artifacts none \
+    --package-dir $move_dir/emojicoin_dot_fun/ \
+    --json-output-file $emojicoin_json_path &
+
+aptos move build-publish-payload \
+    --assume-yes \
+    --private-key $PUBLISHER_PK \
+    --encoding hex \
+    --named-addresses rewards=$address,integrator=$address,emojicoin_dot_fun=$address \
+    --override-size-check \
+    --included-artifacts none \
+    --package-dir $move_dir/rewards/ \
+    --json-output-file $rewards_json_path
