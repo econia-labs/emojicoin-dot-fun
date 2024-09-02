97c711857aff414676e53a7f5b88599254678535
matt <xbtmatt@gmail.com>
2024-09-02 15:16:12 -0700

    Rename to `emojicoin-node` and update tag to push

--- src/docker/emojicoin-aptos-node/sh/create-publish-payloads.sh
+++ src/docker/emojicoin-aptos-node/sh/create-publish-payloads.sh
@@ -4,7 +4,7 @@ root_dir=$(git rev-parse --show-toplevel)
 docker_dir="$root_dir/src/docker"
 move_dir="$root_dir/src/move"
 
-json_output_dir="$docker_dir/aptos-node/json"
+json_output_dir="$docker_dir/emojicoin-aptos-node/json"
 emojicoin_json_path="$json_output_dir/publish-emojicoin_dot_fun.json"
 rewards_json_path="$json_output_dir/publish-rewards.json"
 
