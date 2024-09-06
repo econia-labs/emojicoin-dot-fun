b6e255e860534fdd7beed493e20f3e8fab0a4b53
Bogdan Crisan <bogdan@crbl.studio>
2024-09-06 14:06:38 +0200

    Address matt's comments

--- src/typescript/frontend/src/lib/hooks/queries/use-num-markets.ts
+++ src/typescript/frontend/src/lib/hooks/queries/use-num-markets.ts
@@ -1,6 +1,7 @@
 import { RegistryView } from "@sdk/emojicoin_dot_fun/emojicoin-dot-fun";
 import { useQuery } from "@tanstack/react-query";
 import { getAptos } from "lib/utils/aptos-client";
+import { useEventStore } from "context/state-store-context";
 
 async function getNumMarkets(): Promise<number> {
   const aptos = getAptos();
@@ -8,12 +9,13 @@ async function getNumMarkets(): Promise<number> {
 }
 
 export const useNumMarkets = () => {
+  const numMarkets = useEventStore((s) => s.symbols.size);
   const res = useQuery({
-    queryKey: ["num-markets"],
+    queryKey: ["num-markets", numMarkets],
     queryFn: () => {
       return getNumMarkets();
     },
-    staleTime: 30000,
+    staleTime: 15000,
   });
 
   return res;
