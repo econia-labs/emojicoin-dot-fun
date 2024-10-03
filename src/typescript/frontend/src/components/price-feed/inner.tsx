c267af953b7f47def49830277fee174678cca350
Bogdan Crisan <bogdan@crbl.studio>
2024-10-03 13:57:44 +0200

    Update price feed styling

--- src/typescript/frontend/src/components/price-feed/inner.tsx
+++ src/typescript/frontend/src/components/price-feed/inner.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import Planet from "@icons/Planet";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useAnimationControls } from "framer-motion";
+import { useEffect, useRef } from "react";
 import type { PriceFeedData } from "./types";
+import Link from "next/link";
 
 const Item = ({ emoji, change }: { emoji: string, change: number }) => {
   return (
-    <div className="h-[40px] pixel-heading-3 whitespace-nowrap">
-      <span>{emoji}:</span>
-      <span className={change >= 0 ? "text-green" : "text-pink"}>{change >= 0 && "+"}{change}%</span>
-    </div>
+    <Link href={`/market/${emoji}`} className={`font-pixelar whitespace-nowrap border-[1px] border-solid ${change >= 0 ? "border-green" : "border-pink"} rounded-full px-3 py-[2px] select-none`} draggable={false}>
+      <span className="text-xl mr-[9px]">{emoji}:</span>
+      <span className={`text-2xl ${change >= 0 ? "text-green" : "text-pink"}`}>{change >= 0 ? "+" : "-"} {Math.abs(change)}%</span>
+    </Link>
   );
 };
 
@@ -18,26 +18,28 @@ export const PriceFeedInner = ({ data }: {data: PriceFeedData}) => {
   const carouselRef = useRef<HTMLDivElement>(null);
   carouselRef.current?.clientWidth;
 
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    controls.start((_) => ({
+      transform: `translateX(-${(carouselRef.current?.clientWidth ?? 0 / 5 * 4)}px)`,
+    }));
+  }, [carouselRef]);
+
   const getRow = (i: number) =>
-    data!.map((itemData, j) => <><Item key={`item::${i}::${j}`} emoji={itemData.emoji} change={itemData.change} /><Planet /></>)
+    data!.map((itemData, j) => <Item key={`item::${i}::${j}`} emoji={itemData.emoji} change={itemData.change} />)
 
   return <div className="w-full">
       <div className="overflow-hidden w-full flex-row">
         <div className="flex">
-          <motion.div className="flex gap-[16px] animate-carousel" ref={carouselRef}
-            initial={{
-              transform: "translateX(0)",
-            }}
-            animate={{
-              transform: `translateX(-${(carouselRef.current?.clientWidth ?? 0 / 5 * 4)}px)`,
-            }}
+          <motion.div className="flex gap-[22px] animate-carousel" ref={carouselRef}
+            animate={controls}
             transition={{
               duration:  ((carouselRef.current?.clientWidth ?? 0 / 5 * 4)) / (4527.83 / 88.407),
               repeat: Infinity,
               repeatType: "loop",
               ease: []
             }}>
-            <Planet />
             {[1,2,3,4,5].map(getRow)}
           </motion.div>
         </div>
