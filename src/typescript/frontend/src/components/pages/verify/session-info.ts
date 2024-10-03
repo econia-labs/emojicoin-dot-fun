30f0008a568898eaccdfb9672c84bb823e568e1a
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-10-03 13:35:18 -0700

    Remove more incompatible imports

--- src/typescript/frontend/src/components/pages/verify/session-info.ts
+++ src/typescript/frontend/src/components/pages/verify/session-info.ts
@@ -1,7 +1,6 @@
 // cspell:word emojicoindotfun
 
-import { UNIT_OF_TIME_MULTIPLIERS, UnitOfTime } from "@sdk/utils/misc";
-
+// One week.
+export const COOKIE_LENGTH = 1000 * 60 * 60 * 24 * 7;
 export const COOKIE_FOR_HASHED_ADDRESS = "emojicoindotfun-account-hashed-address";
 export const COOKIE_FOR_ACCOUNT_ADDRESS = "emojicoindotfun-account-address";
-export const COOKIE_LENGTH = UNIT_OF_TIME_MULTIPLIERS[UnitOfTime.Weeks] * 1;
