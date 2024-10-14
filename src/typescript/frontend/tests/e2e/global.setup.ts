4c693806ef068418f315a224ec485f657959dcc9
Bogdan Crisan <bogdan@crbl.studio>
2024-10-14 18:51:51 +0200

    Fix ci

--- src/typescript/frontend/tests/e2e/global.setup.ts
+++ src/typescript/frontend/tests/e2e/global.setup.ts
@@ -3,7 +3,7 @@ import { DockerTestHarness } from "../../../sdk/src/utils/test/docker/docker-tes
 
 setup("setup", async ({}) => {
   setup.setTimeout(240000);
-  const startDockerServices = process.env.APTOS_NETWORK === "local";
+  const startDockerServices = process.env.APTOS_NETWORK === "docker";
   if (startDockerServices) {
     await DockerTestHarness.run(true);
   }
