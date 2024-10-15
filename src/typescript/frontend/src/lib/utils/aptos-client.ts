e9664df49d0497d50a41fba5e0542652fee7280e
Bogdan Crisan <bogdan@crbl.studio>
2024-10-15 14:12:02 +0200

    Revert to local aptos network for ci

--- src/typescript/frontend/src/lib/utils/aptos-client.ts
+++ src/typescript/frontend/src/lib/utils/aptos-client.ts
@@ -1,20 +1,24 @@
 /* eslint-disable import/no-unused-modules */ // Used in the frontend repo.
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { NetworkToFaucetAPI, NetworkToIndexerAPI, NetworkToNodeAPI } from "@aptos-labs/ts-sdk";
 import { Aptos, AptosConfig, NetworkToNetworkName } from "@aptos-labs/ts-sdk";
 import { APTOS_NETWORK } from "lib/env";
 
+const toDockerUrl = (url: string) => url.replace("127.0.0.1", "host.docker.internal");
+
 // Get an Aptos config based off of the network environment variables.
 export const getAptosConfig = (): AptosConfig => {
-  // Check if it's a valid network.
   const networkString = APTOS_NETWORK;
-  console.log({ networkString });
-  if ((networkString as string) === "docker") {
-    return new AptosConfig({
-      network: NetworkToNetworkName["custom"],
-      fullnode: NetworkToNodeAPI["local"].replace("127.0.0.1", "host.docker.internal"),
-      indexer: NetworkToIndexerAPI["local"].replace("127.0.0.1", "host.docker.internal"),
-      faucet: NetworkToFaucetAPI["local"].replace("127.0.0.1", "host.docker.internal"),
-    });
+  if (networkString === "local" && typeof window === "undefined") {
+    const fs = require("node:fs");
+    if (fs.existsSync("/.dockerenv")) {
+      return new AptosConfig({
+        network: NetworkToNetworkName["local"],
+        fullnode: toDockerUrl(NetworkToNodeAPI["local"]),
+        indexer: toDockerUrl(NetworkToIndexerAPI["local"]),
+        faucet: toDockerUrl(NetworkToFaucetAPI["local"]),
+      });
+    }
   }
   const network = NetworkToNetworkName[networkString ?? APTOS_NETWORK];
   const fullnode = NetworkToNodeAPI[network];
@@ -29,6 +33,5 @@ export const getAptosConfig = (): AptosConfig => {
 // Get an Aptos client based off of the network environment variables.
 export const getAptos = (): Aptos => {
   const cfg = getAptosConfig();
-  console.log({ cfg });
   return new Aptos(cfg);
 };
--- src/typescript/frontend/tests/e2e/global.setup.ts
+++ src/typescript/frontend/tests/e2e/global.setup.ts
@@ -3,7 +3,7 @@ import { DockerTestHarness } from "../../../sdk/src/utils/test/docker/docker-tes
 
 setup("setup", async ({}) => {
   setup.setTimeout(240000);
-  const startDockerServices = process.env.APTOS_NETWORK === "docker";
+  const startDockerServices = process.env.APTOS_NETWORK === "local";
   if (startDockerServices) {
     await DockerTestHarness.run(true);
   }
