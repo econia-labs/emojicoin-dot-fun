21c2661dbfffea4fb00e8f863df777babcb3c795
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-10-16 12:58:30 -0700

    Make the setup timeout longer

--- src/typescript/frontend/tests/e2e/global.setup.ts
+++ src/typescript/frontend/tests/e2e/global.setup.ts
@@ -2,7 +2,8 @@ import { test as setup } from "@playwright/test";
 import { DockerTestHarness } from "../../../sdk/src/utils/test/docker/docker-test-harness";
 
 setup("setup the Docker containers", async ({}) => {
-  setup.setTimeout(240000);
+  // Ten minute timeout.
+  setup.setTimeout(600_000);
   const startDockerServices = process.env.APTOS_NETWORK === "local";
   if (startDockerServices) {
     await DockerTestHarness.run(true);
