17c9881871dbf0d4a09ff2551a54dfeee8cbdd90
Bogdan Crisan <bogdan@crbl.studio>
2024-10-21 11:26:37 +0200

    Fix path

--- src/typescript/sdk/tests/e2e/queries/price-feed/price-feed.test.ts
+++ src/typescript/sdk/tests/e2e/queries/price-feed/price-feed.test.ts
@@ -1,21 +1,24 @@
 import { getDbConnection } from "../../helpers";
 import { fetchPriceFeed } from "../../../../src/indexer-v2/queries";
+import path from "path";
 
 // We need a long timeout because the test must wait for the 1-minute period to expire.
 jest.setTimeout(750000);
+
+const pathRoot = path.join(__dirname, "./");
 
 describe("queries price_feed and returns accurate price feed data", () => {
   it("checks price feed results generated from artificial data", async () => {
     const db = getDbConnection();
 
     // Insert a swap 25 hours ago at price 500
-    await db.file("./insert_past_day_swap.sql");
+    await db.file(`${pathRoot}insert_past_day_swap.sql`);
 
     // Insert a fresh swap at price 750
-    await db.file("./insert_current_day_swap.sql");
+    await db.file(`${pathRoot}insert_current_day_swap.sql`);
 
     // Update market_latest_state_event accordingly
-    await db.file("./insert_market_state.sql");
+    await db.file(`${pathRoot}insert_market_state.sql`);
 
     const priceFeed = await fetchPriceFeed({});
     expect(priceFeed[0].marketID).toEqual(777701);
