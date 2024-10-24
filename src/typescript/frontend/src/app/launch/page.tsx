b810a8a988034a218c8e96edaa9f4bdc942222b2
Bogdan Crisan <bogdan@crbl.studio>
2024-10-24 10:29:31 +0200

    Update meta description

--- src/typescript/frontend/src/app/launch/page.tsx
+++ src/typescript/frontend/src/app/launch/page.tsx
@@ -2,9 +2,16 @@ import { REVALIDATION_TIME } from "lib/server-env";
 import ClientLaunchEmojicoinPage from "../../components/pages/launch-emojicoin/ClientLaunchEmojicoinPage";
 import { isUserGeoblocked } from "utils/geolocation";
 import { headers } from "next/headers";
+import { type Metadata } from "next";
+import { SYMBOL_EMOJI_DATA } from "@sdk/emoji_data";
 
 export const revalidate = REVALIDATION_TIME;
 export const dynamic = "force-static";
+
+export const metadata: Metadata = {
+  title: "launch",
+  description: `Launch your own emojicoins using emojicoin.fun ${SYMBOL_EMOJI_DATA.byName("rocket")}`,
+};
 
 export default async function LaunchEmojicoinPage() {
   const geoblocked = await isUserGeoblocked(headers().get("x-real-ip"));
--- src/typescript/frontend/src/configs/meta.ts
+++ src/typescript/frontend/src/configs/meta.ts
@@ -1,7 +1,8 @@
+import { SYMBOL_EMOJI_DATA } from "@sdk/emoji_data";
 import { type Metadata } from "next";
 
 export const DEFAULT_TITLE = "emojicoin.fun";
-export const DEFAULT_DESCRIPTION = "Emojicoin joy starts here";
+export const DEFAULT_DESCRIPTION = `Give your wallet the personality it deserves ${SYMBOL_EMOJI_DATA.byName("zany face")}${SYMBOL_EMOJI_DATA.byName("sparkles")}`;
 export const OG_IMAGES = "/social-preview.png";
 export const OG_TYPE = "website";
 export const TWITTER_CARD = "summary";
@@ -27,7 +28,10 @@ export const getDefaultMetadata = (): Metadata => {
     alternates: {
       canonical: "/",
     },
-    title: DEFAULT_TITLE,
+    title: {
+      default: DEFAULT_TITLE,
+      template: `%s | ${DEFAULT_TITLE}`,
+    },
     description: DEFAULT_DESCRIPTION,
     keywords: "aptos, tokens, emoji, emojicoins",
     openGraph: {
