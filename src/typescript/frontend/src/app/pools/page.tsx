5e625ed184355c341f2710aec24ac7d69ff0e87e
Bogdan Crisan <bogdan@crbl.studio>
2024-10-28 22:15:04 +0100

    [ECO-2331] Update meta description (#311)
    Co-authored-by: alnoki <43892045+alnoki@users.noreply.github.com>

--- src/typescript/frontend/src/app/pools/page.tsx
+++ src/typescript/frontend/src/app/pools/page.tsx
@@ -5,9 +5,16 @@ import { isUserGeoblocked } from "utils/geolocation";
 import { getPoolData } from "./api/getPoolDataQuery";
 import { SortMarketsBy } from "@sdk/indexer-v2/types/common";
 import { symbolBytesToEmojis } from "@sdk/emoji_data/utils";
+import { type Metadata } from "next";
+import { emoji } from "utils";
 
 export const revalidate = REVALIDATION_TIME;
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "pools",
+  description: `Provide ${emoji("water wave")}liquidity${emoji("water wave")} and earn APR using your emojis !`,
+};
 
 export default async function PoolsPage({ searchParams }: { searchParams: { pool: string } }) {
   const geoblocked = await isUserGeoblocked(headers().get("x-real-ip"));
