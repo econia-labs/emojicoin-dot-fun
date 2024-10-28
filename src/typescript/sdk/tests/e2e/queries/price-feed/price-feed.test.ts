e1110db1fa98fcd03b48b9fa4ca16388d0568a93
Bogdan Crisan <bogdan@crbl.studio>
2024-10-28 14:28:25 +0100

    Add tests for new price feed logic

--- src/typescript/sdk/tests/e2e/queries/price-feed/price-feed.test.ts
+++ src/typescript/sdk/tests/e2e/queries/price-feed/price-feed.test.ts
@@ -12,17 +12,30 @@ describe("queries price_feed and returns accurate price feed data", () => {
     const db = getDbConnection();
 
     // Insert a swap 25 hours ago at price 500
-    await db.file(`${pathRoot}insert_past_day_swap.sql`);
+    await db.file(`${pathRoot}test_1_insert_past_day_swap.sql`);
 
     // Insert a fresh swap at price 750
-    await db.file(`${pathRoot}insert_current_day_swap.sql`);
+    await db.file(`${pathRoot}test_1_insert_current_day_swap.sql`);
 
     // Update market_latest_state_event accordingly
-    await db.file(`${pathRoot}insert_market_state.sql`);
+    await db.file(`${pathRoot}test_1_insert_market_state.sql`);
+
+    // Insert a swap 10 hours ago at price 1000
+    await db.file(`${pathRoot}test_2_insert_earlier_swap.sql`);
+
+    // Insert a fresh swap at price 250
+    await db.file(`${pathRoot}test_2_insert_later_swap.sql`);
+
+    // Update market_latest_state_event accordingly
+    await db.file(`${pathRoot}test_2_insert_market_state.sql`);
 
     const priceFeed = await fetchPriceFeed({});
     expect(priceFeed[0].marketID).toEqual(777701);
     expect(priceFeed[0].openPrice).toEqual(500n);
     expect(priceFeed[0].closePrice).toEqual(750n);
+
+    expect(priceFeed[1].marketID).toEqual(777702);
+    expect(priceFeed[1].openPrice).toEqual(1000n);
+    expect(priceFeed[1].closePrice).toEqual(250n);
   });
 });
