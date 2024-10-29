0e3e07ff068d007bb7e3b78959ad75856b66fcb6
Bogdan Crisan <bogdan@crbl.studio>
2024-10-29 11:28:23 +0100

    Revert env changes

--- src/typescript/frontend/playwright.config.ts
+++ src/typescript/frontend/playwright.config.ts
@@ -1,9 +1,4 @@
 import { defineConfig, devices } from "@playwright/test";
-import dotenv from "dotenv";
-import path from "path";
-
-// Read from ".env" file.
-dotenv.config({ path: path.resolve(__dirname, "../ci.env") });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
