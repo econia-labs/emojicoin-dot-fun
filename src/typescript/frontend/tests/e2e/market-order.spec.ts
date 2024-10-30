48c1c441d47e5758f0261aba4eeefa8b6974a01a
Bogdan Crisan <bogdan@crbl.studio>
2024-10-30 12:18:42 +0100

    [ECO-2153] Add sorting e2e tests (#310)

---
+++ src/typescript/frontend/tests/e2e/market-order.spec.ts
@@ -0,0 +1,84 @@
+import { test, expect } from "@playwright/test";
+import { EmojicoinClient } from "../../../sdk/src/client/emojicoin-client";
+import { getFundedAccount } from "../../../sdk/src/utils/test/test-accounts";
+import { ONE_APT_BIGINT, sleep, SYMBOL_EMOJI_DATA } from "../../../sdk/src";
+
+test("check sorting order", async ({ page }) => {
+  const user = getFundedAccount("777");
+  const rat = SYMBOL_EMOJI_DATA.byName("rat")!.emoji;
+  const emojis = ["cat", "dog", "eagle", "sauropod"];
+  const markets = emojis.map(e => [rat, SYMBOL_EMOJI_DATA.byName(e)!.emoji])
+
+  const client = new EmojicoinClient();
+
+  // Register markets.
+  // They all start with rat to simplify the search.
+  for (let i = 0; i < markets.length; i++) {
+    await client.register(user, markets[i]).then(res => res.handle);
+    const amount = 1n * ONE_APT_BIGINT / 100n * BigInt(10 ** (markets.length - i));
+    await client.buy(user, markets[i], amount).then(res => res.handle);
+  }
+
+  await page.goto("/home");
+
+  // Click the search field.
+  const search = page.getByTestId("emoji-input");
+  expect(search).toBeVisible();
+  await sleep(2000);
+  await search.click();
+
+  // Expect the emoji picker to be visible.
+  const picker = page.getByTestId("picker");
+  expect(picker).toBeVisible();
+
+  // Search for "rat" in the emoji picker search field.
+  const emojiSearch = page.getByPlaceholder("Search");
+  expect(emojiSearch).toBeVisible();
+  await emojiSearch.fill("rat");
+
+  // Search for the rat market.
+  await picker.getByLabel(rat).click();
+
+  // Expect markets.length results, since that's how many we registered.
+  let marketGridItems = page.getByTestId("market-grid-item");
+  expect(marketGridItems).toHaveCount(markets.length);
+
+  // Expect the sorting button to be visible.
+  const filters = page.getByText(/{Sort/);
+  expect(filters).toBeVisible();
+
+  // Click the sorting button.
+  await filters.click();
+
+  // Expect the sort by daily volume button to be visible.
+  const dailyVolume = page.locator('#emoji-grid-header').getByText('24h Volume');
+  expect(dailyVolume).toBeVisible();
+
+  // Sort by daily volume.
+  await dailyVolume.click();
+
+  const names = emojis.map(e => `rat,${e}`);
+  const patterns = names.map(e => new RegExp(e));
+
+  // Expect the markets to be in order of daily volume.
+  marketGridItems = page.locator("#emoji-grid a").getByTitle(/RAT,/, { exact: true });
+  expect(marketGridItems).toHaveText(patterns);
+
+  // Click the sorting button.
+  await filters.click();
+
+  // Expect the sort by bump order button to be visible.
+  const bumpOrder = page.locator('#emoji-grid-header').getByText('Bump Order');
+  expect(bumpOrder).toBeVisible();
+
+  // Sort by bump order.
+  await bumpOrder.click();
+
+  await page.screenshot();
+
+  // Expect the markets to be in bump order.
+  marketGridItems = page.locator("#emoji-grid a").getByTitle(/RAT,/, { exact: true });
+  expect(marketGridItems).toHaveText(patterns.reverse());
+
+  await page.screenshot();
+});
--- src/typescript/frontend/tests/e2e/search.spec.ts
+++ src/typescript/frontend/tests/e2e/search.spec.ts
@@ -1,23 +1,48 @@
 import { test, expect } from "@playwright/test";
 import { EmojicoinClient } from "../../../sdk/src/client/emojicoin-client";
 import { getFundedAccount } from "../../../sdk/src/utils/test/test-accounts";
-import { SYMBOL_EMOJI_DATA } from "../../../sdk/src";
+import { sleep, SYMBOL_EMOJI_DATA } from "../../../sdk/src";
 
 test("check search results", async ({ page }) => {
   const user = getFundedAccount("666");
-  const symbols = [SYMBOL_EMOJI_DATA.byName("cat")!.emoji, SYMBOL_EMOJI_DATA.byName("cat")!.emoji];
+  const cat = SYMBOL_EMOJI_DATA.byName("cat")!.emoji;
+  const symbols = [cat,cat];
+
   const client = new EmojicoinClient();
   await client.register(user, symbols).then((res) => res.handle);
 
   await page.goto("/home");
 
+  // Click the search field.
   const search = page.getByTestId("emoji-input");
   expect(search).toBeVisible();
-  await search.fill(symbols.join(""));
+  await search.click();
 
+  // Expect the emoji picker to be visible.
+  const picker = page.getByTestId("picker");
+  expect(picker).toBeVisible();
+
+  // Search for "cat" in the emoji picker search field.
+  const emojiSearch = page.getByPlaceholder("Search");
+  expect(emojiSearch).toBeVisible();
+  await emojiSearch.fill("cat");
+
+  // Expect the "cat" emoji to be visible in the search results.
+  let emojiSearchCatButton = picker.getByLabel(cat).first();
+  expect(emojiSearchCatButton).toBeVisible();
+
+  // Search for the cat,cat market.
+  await emojiSearchCatButton.click({force: true});
+
+  emojiSearchCatButton = picker.getByLabel(cat).first();
+  expect(emojiSearchCatButton).toBeVisible();
+  await emojiSearchCatButton.click({force: true});
+
+  // Click on the cat,cat market.
   const marketCard = page.getByText("cat,cat", { exact: true });
   expect(marketCard).toBeVisible();
   await marketCard.click();
 
+  // Expect to be redirected to the cat,cat market.
   await expect(page).toHaveURL(/.*cat;cat/);
 });
