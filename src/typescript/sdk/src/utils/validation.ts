8dfcf07208f6af96c867d66618114df36520e705
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-10-30 20:33:53 -0700

    Update doc comments, lint/format

--- src/typescript/sdk/src/utils/validation.ts
+++ src/typescript/sdk/src/utils/validation.ts
@@ -1,6 +1,9 @@
-// Remove all useless leading zeros.
-//
-// In the case of 0.[0-9]+, the leading zero will not be removed.
+/**
+ * Removes all useless leading zeros.
+ * In the case of 0.[0-9]+, the leading zero will not be removed.
+ * @param {string} input - The string to process
+ * @returns {string} The processed string
+ */
 export const trimLeadingZeros = (input: string) => {
   // Replace all leading zeros with one zero
   input = input.replace(/^0+/, "0"); // The regex matches all leading 0s.
@@ -12,9 +15,12 @@ export const trimLeadingZeros = (input: string) => {
   return input;
 };
 
-// Remove all leading zeros and transform "," in ".".
-//
-// .[0-9]+ will be replaced with 0.[0-9]+
+/**
+ * Removes all leading zeros and transforms "," into ".".
+ * .[0-9]+ will be replaced with 0.[0-9]+
+ * @param {string} input - The string to process
+ * @returns {string} The processed string
+ */
 export const sanitizeNumber = (input: string) => {
   input = trimLeadingZeros(input.replace(/,/, "."));
   if (input.startsWith(".")) {
@@ -23,37 +29,34 @@ export const sanitizeNumber = (input: string) => {
   return input;
 };
 
-// Return true if input is a number in construction.
-//
-// This is used to allow the input of invalid numbers, if they are what needs
-// to be inputted to create a valid number.
-//
-// For example, in order to input 0.001, you first need to input 0, then 0. which
-// is an invalid number, but we do not want to forbid inputting that since it
-// will prevent the user from inputting 0.001.
-//
-// We define a number in construction as a number that leads to the construction
-// of a valid number.
-//
-// A number in construction can be:
-// - 0.1
-// - 0
-// - 0.000
-// - .0
-// - 0.
-//
-// It cannot be:
-// - 0.0.1
-//
-// Regex explanation:
-// n digits, then maybe (a dot, then maybe (m digits))
+/**
+ * Checks if the input is a number in construction.
+ *
+ * This facilitates using temporarily invalid numbers that will eventually be valid- aka, they are
+ * numbers in construction.
+ *
+ * For example, to input 0.001, you need to input "0", then "0." (invalid),
+ * which should be allowed to reach "0.001".
+ *
+ * Valid examples:
+ * - 0.1
+ * - 0
+ * - 0.000
+ * - .0
+ * - 0.
+ *
+ * Invalid examples:
+ * - 0.0.1
+ *
+ * @param {string} input - The string to test
+ * @returns {boolean} True if the input is a number in construction
+ */
 export const isNumberInConstruction = (input: string) => /^[0-9]*(\.([0-9]*)?)?$/.test(input);
 
-// Return the number of decimals that input has.
-//
-// If there is a dot, we count the number of elements past the dot, else we
-// return 0.
-//
-// The regex tests if there is a dot in the string.
-export const numberOfDecimals = (input: string) =>
+/**
+ * Counts the number of digits after the decimal point in a string number.
+ * @param {string} input - The numeric string to analyze (e.g., "123.456")
+ * @returns {number} The count of digits after the decimal point (e.g., "123.456" returns 3)
+ */
+export const countDigitsAfterDecimal = (input: string) =>
   /\./.test(input) ? input.split(".")[1].length : 0;
