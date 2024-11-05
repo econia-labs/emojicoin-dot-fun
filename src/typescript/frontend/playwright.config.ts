68bbdc5228e457de9db1f814c48fb93aba9713ce
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-04 16:25:03 -0800

    Try trace retain on failure

--- src/typescript/frontend/playwright.config.ts
+++ src/typescript/frontend/playwright.config.ts
@@ -9,8 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -24,7 +23,7 @@ export default defineConfig({
     baseURL: "http://127.0.0.1:3001",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
 
     launchOptions: {
       env: {
