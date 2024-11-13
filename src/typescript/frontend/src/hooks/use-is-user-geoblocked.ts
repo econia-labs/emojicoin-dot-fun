de79587fba38843a72badcd1506f909fa0587127
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-13 10:32:02 -0800

    Add 7 days stale time, return stale data while revalidating stale data

--- src/typescript/frontend/src/hooks/use-is-user-geoblocked.ts
+++ src/typescript/frontend/src/hooks/use-is-user-geoblocked.ts
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import { MS_IN_ONE_DAY } from "components/charts/const";
 import { isUserGeoblocked } from "utils/geolocation";
+
+const SEVEN_DAYS_MS = 7 * MS_IN_ONE_DAY;
 
 const useIsUserGeoblocked = () => {
   const { data } = useQuery({
     queryKey: ["geoblocked"],
     queryFn: () => isUserGeoblocked(),
-    staleTime: Infinity,
+    staleTime: SEVEN_DAYS_MS,
+    placeholderData: (prev) => prev,
   });
 
   // Assume the user is geoblocked if the response hasn't completed yet.
