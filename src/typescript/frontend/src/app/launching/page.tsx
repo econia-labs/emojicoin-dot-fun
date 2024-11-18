778b67ddb0c9197e58ae69741bd94be408f7dcd3
Bogdan Crisan <bogdan@crbl.studio>
2024-11-18 22:21:34 +0100

    Add pre launcch page

---
+++ src/typescript/frontend/src/app/launching/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useMemo } from "react";
+import MatrixRain from "../maintenance/matrix";
+import { useScramble } from "use-scramble";
+import { Text } from "components";
+import { getRandomChatEmoji } from "@sdk/emoji_data";
+
+export default function LaunchingPage() {
+  const catchPhrase = useMemo(() => {
+    const phrases = ["launching soon", "get ready"];
+    const index = Math.floor(Math.random() * phrases.length);
+    const emoji = getRandomChatEmoji().emoji;
+    return `${emoji} ${phrases[index]} ${emoji}`;
+  }, []);
+  const { ref } = useScramble({ text: `{ ${catchPhrase} }` });
+  return (
+    <div className="relative">
+      <MatrixRain />
+      <Text
+        ref={ref}
+        textScale="pixelHeading1"
+        color="econiaBlue"
+        textTransform="uppercase"
+        className="fixed left-0 right-0 top-0 bottom-0 bg-black"
+        style={{
+          boxShadow: "0px 0px 30px 30px black",
+          margin: "auto auto",
+          width: "fit-content",
+          height: "fit-content",
+        }}
+      />
+    </div>
+  );
+}
