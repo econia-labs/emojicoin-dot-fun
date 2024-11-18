b027339030ce8a51109ff1ca13e1c40a9da4b0a9
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-18 13:47:57 -0800

    Change font size to be dynamic based on width

--- src/typescript/frontend/src/app/launching/page.tsx
+++ src/typescript/frontend/src/app/launching/page.tsx
@@ -22,12 +22,14 @@ export default function LaunchingPage() {
         textScale="pixelHeading2"
         color="econiaBlue"
         textTransform="uppercase"
-        className="fixed left-0 right-0 top-0 bottom-0 bg-black"
+        className="fixed left-0 right-0 top-0 bottom-0 bg-black font-pixelar"
         style={{
           boxShadow: "0px 0px 30px 30px black",
           margin: "auto auto",
           width: "fit-content",
           height: "fit-content",
+          fontSize: "7vw",
+          lineHeight: "9vw",
         }}
       />
     </div>
