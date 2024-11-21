3409e738e3c11be5f895c4dbcd163276cc106aab
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-20 18:46:27 -0800

    Re-order return in aptos-client.ts

--- src/typescript/sdk/src/utils/aptos-client.ts
+++ src/typescript/sdk/src/utils/aptos-client.ts
@@ -17,19 +17,12 @@ const toDockerUrl = (url: string) => url.replace("127.0.0.1", "host.docker.inter
 
 export function getAptosClient(additionalConfig?: Partial<AptosConfig>): Aptos {
   const network = APTOS_NETWORK;
-  let config = new AptosConfig({
-    network,
-    ...additionalConfig,
-    clientConfig: {
-      ...APTOS_CONFIG,
-      ...additionalConfig?.clientConfig,
-    },
-  });
+
   if (network === "local" && typeof window === "undefined") {
     /* eslint-disable-next-line @typescript-eslint/no-var-requires */
     const fs = require("node:fs");
     if (fs.existsSync("/.dockerenv")) {
-      config = new AptosConfig({
+      const config = new AptosConfig({
         network: NetworkToNetworkName["local"],
         fullnode: toDockerUrl(NetworkToNodeAPI["local"]),
         indexer: toDockerUrl(NetworkToIndexerAPI["local"]),
@@ -39,7 +32,17 @@ export function getAptosClient(additionalConfig?: Partial<AptosConfig>): Aptos {
           ...additionalConfig?.clientConfig,
         },
       });
+      return new Aptos(config);
     }
   }
+
+  const config = new AptosConfig({
+    network,
+    ...additionalConfig,
+    clientConfig: {
+      ...APTOS_CONFIG,
+      ...additionalConfig?.clientConfig,
+    },
+  });
   return new Aptos(config);
 }
