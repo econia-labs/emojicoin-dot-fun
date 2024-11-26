f46216c266eece17cf96e3672fb6601de81cd4d3
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-26 14:10:20 -0800

    Add scripts folder

---
+++ src/typescript/scripts/.eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  ignorePatterns: ["dist/**", "node_modules/**", ".eslintrc.js"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["tsconfig.json", "tests/tsconfig.json"],
+    ecmaVersion: "latest",
+    sourceType: "module",
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  plugins: ["@typescript-eslint", "unused-imports", "import"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "no-console": [
+      "warn",
+      {
+        allow: ["warn", "error", "debug", "clear", "trace"],
+      },
+    ],
+    "@typescript-eslint/lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    "@typescript-eslint/no-throw-literal": "error",
+    "import/extensions": "off",
+    "import/no-commonjs": ["error", { allowRequire: false, allowPrimitiveModules: false }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+        optionalDependencies: true,
+        peerDependencies: true,
+      },
+    ],
+    "import/no-useless-path-segments": ["error", { noUselessIndex: true }],
+    "max-classes-per-file": ["error", 10],
+    "import/prefer-default-export": "off",
+    "object-curly-newline": "off",
+    "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error", { functions: false, classes: false }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "unused-imports/no-unused-imports": "error",
+    "import/no-unused-modules": [
+      "warn",
+      {
+        missingExports: true,
+        unusedExports: true,
+        ignoreExports: ["tests/**/*", "**/index.ts", "src/types/server-only.d.ts"],
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
