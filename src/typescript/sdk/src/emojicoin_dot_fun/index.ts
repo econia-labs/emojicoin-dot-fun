dc876bc403d7b0a641d35bf2c0f18a8ff49fdfe6
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-26 10:36:07 -0800

    [ECO-2492] Calculate the swap price client-side (#412)

--- src/typescript/sdk/src/emojicoin_dot_fun/index.ts
+++ src/typescript/sdk/src/emojicoin_dot_fun/index.ts
@@ -2,3 +2,4 @@ export * as AptosFramework from "./aptos-framework";
 export * as EmojicoinDotFun from "./emojicoin-dot-fun";
 export * from "./types";
 export * from "./utils";
+export * from "./calculate-swap-price";
--- src/typescript/sdk/tests/pre-test.ts
+++ src/typescript/sdk/tests/pre-test.ts
@@ -1,7 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 import WebSocket from "ws";
 import { DockerTestHarness } from "../src/utils/test/docker/docker-test-harness";
-import { type ContainerName } from "../src/utils/test/docker/logs";
+
+// process.env.NO_TEST_SETUP => to skip the docker container test setup, like for unit tests.
+// process.env.FILTER_TEST_LOGS => quiet mode, don't output logs that print to the console a lot.
 
 export default async function preTest() {
   // @ts-expect-error Using `globalThis` as any for a polyfill for `WebSocket` in node.js.
@@ -12,19 +14,16 @@ export default async function preTest() {
   if (setupTest && startDockerServices) {
     // Print an empty line to separate `Determining test suites to run...` from the logs.
     console.debug();
-    const noLogs: Array<ContainerName> = [];
-    // Only show more meaningful test logs by default.
-    if (!process.env.VERBOSE_TEST_LOGS) {
-      noLogs.push(...(["broker", "processor", "frontend", "postgres"] as Array<ContainerName>));
-    }
-    // @ts-expect-error Using `globalThis` as any.
-    globalThis.__DOCKER_LOGS_FILTER__ = noLogs;
     // --------------------------------------------------------------------------------------
     //                             Start the docker containers.
     // --------------------------------------------------------------------------------------
     // Start the Docker test harness without the frontend container.
-    await DockerTestHarness.run({ frontend: false });
-
-    // The docker container start-up script publishes the package on-chain.
+    await DockerTestHarness.run({
+      frontend: false,
+      filterLogsFrom:
+        process.env.FILTER_TEST_LOGS === "true"
+          ? ["broker", "processor", "frontend", "postgres"]
+          : [],
+    });
   }
 }
