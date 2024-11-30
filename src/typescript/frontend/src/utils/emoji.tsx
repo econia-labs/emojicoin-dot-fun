fb3e0aa6c129d95d66bec39e839f9f75bbf28b45
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-11-30 00:33:46 -0800

    Clean up `emojit.sx` component

--- src/typescript/frontend/src/utils/emoji.tsx
+++ src/typescript/frontend/src/utils/emoji.tsx
@@ -1,4 +1,5 @@
 import { type AnyEmojiData, getEmojisInString } from "@sdk/index";
+import { cn } from "lib/utils/class-name";
 import { type DetailedHTMLProps, type HTMLAttributes } from "react";
 
 import * as React from "react";
@@ -22,28 +23,21 @@ export const Emoji = ({
 }: Omit<DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>, "children"> & {
   emojis: AnyEmojiData[] | string;
 }) => {
-  let data: React.ReactNode[] = [];
-  if (typeof emojis === "string") {
-    const emojisInString = getEmojisInString(emojis);
-    data = emojisInString.map((e, i) => (
-      <span
-        className="text-[1em] font-noto-color-emoji"
-        style={{ fontVariantEmoji: "emoji" }}
-        key={`${emojisInString[i]}-${i}`}
-      >
-        {e}
-      </span>
-    ));
-  } else {
-    data = emojis.map((e, i) => (
-      <span
-        className="text-[1em] font-noto-color-emoji"
-        style={{ fontVariantEmoji: "emoji" }}
-        key={`${emojis[i].emoji}-${i}`}
-      >
-        {e.emoji}
-      </span>
-    ));
-  }
-  return <span {...props}>{data}</span>;
+  const data = React.useMemo(
+    () =>
+      typeof emojis === "string"
+        ? getEmojisInString(emojis).join("")
+        : emojis.map((e) => e.emoji).join(""),
+    [emojis]
+  );
+
+  return (
+    <span
+      {...props}
+      className={cn(props.className, "text-[1em] font-noto-color-emoji")}
+      style={{ fontVariantEmoji: "emoji" }}
+    >
+      {data}
+    </span>
+  );
 };
