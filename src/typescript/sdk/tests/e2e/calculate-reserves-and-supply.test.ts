57d5ab9c8eb65615ec84e92668ea7881fa8a26e1
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-04 12:31:42 -0800

    Add tests

--- src/typescript/sdk/tests/e2e/calculate-reserves-and-supply.test.ts
+++ src/typescript/sdk/tests/e2e/calculate-reserves-and-supply.test.ts
@@ -14,12 +14,15 @@ import { EmojicoinClient } from "../../src/client/emojicoin-client";
 import { getCoinBalanceFromChanges } from "../../src/utils/parse-changes-for-balances";
 import { EXACT_TRANSITION_INPUT_AMOUNT } from "../../src/utils/test";
 import { isInBondingCurve } from "../../src/utils/bonding-curve";
+import { UserTransactionResponse } from "@aptos-labs/ts-sdk";
+import { SwapEventModel } from "../../src/indexer-v2/types";
+import { number } from "@noble/hashes/_assert";
 
 jest.setTimeout(30000);
 
 describe("tests the calculation functions for circulating supply and real reserves", () => {
-  const registrants = getFundedAccounts("074", "075", "076");
-  const marketSymbols: SymbolEmoji[][] = [["🎁"], ["🪨"], ["🏝️"]];
+  const registrants = getFundedAccounts("074", "075", "076", "077", "078", "079");
+  const marketSymbols: SymbolEmoji[][] = [["🤌"], ["🤌🏻"], ["🤌🏼"], ["🤌🏽"], ["🤌🏾"], ["🤌🏿"]];
   const emojicoin = new EmojicoinClient({ integratorFeeRateBPs: 0 });
 
   beforeAll(async () => {
@@ -35,11 +38,13 @@ describe("tests the calculation functions for circulating supply and real reserv
   it("checks the circulating supply after a buy and a sell", async () => {
     const idx = 0;
     const [swapper, symbolEmojis] = [registrants[idx], marketSymbols[idx]];
+    const buyAmount = ONE_APT_BIGINT;
     const coinAddress = getMarketAddress(symbolEmojis);
     const coinType = toCoinTypes(coinAddress).emojicoin;
+    const userAddress = swapper.accountAddress;
 
     const { supplyAfterBuy, userBalance } = await emojicoin
-      .buy(swapper, symbolEmojis, ONE_APT_BIGINT)
+      .buy(swapper, symbolEmojis, buyAmount)
       .then(async ({ response, swap }) => {
         const circulatingSupplyFromFetch = await fetchCirculatingSupply(
           symbolEmojis,
@@ -48,11 +53,7 @@ describe("tests the calculation functions for circulating supply and real reserv
         const circulatingSupplyFromTxnResponse = calculateCirculatingSupply(swap.model.state);
         expect(circulatingSupplyFromFetch).toBeDefined();
         expect(circulatingSupplyFromFetch).toEqual(circulatingSupplyFromTxnResponse);
-        const balance = getCoinBalanceFromChanges({
-          response,
-          userAddress: swapper.accountAddress,
-          coinType,
-        });
+        const balance = getCoinBalanceFromChanges({ response, userAddress, coinType });
         expect(balance).toBeDefined();
         return {
           supplyAfterBuy: circulatingSupplyFromFetch!,
@@ -101,13 +102,56 @@ describe("tests the calculation functions for circulating supply and real reserv
     });
   });
 
-  it("checks the real reserves after a buy and a sell", async () => {
+  it("checks the real reserves after a user buys & sells post-bonding curve", async () => {
     const idx = 2;
     const [swapper, symbolEmojis] = [registrants[idx], marketSymbols[idx]];
+
+    const buyAmountQuote = EXACT_TRANSITION_INPUT_AMOUNT + ONE_APT_BIGINT;
+    const sellAmountEmojicoin = ONE_APT_BIGINT * 2n;
+    const userAddress = swapper.accountAddress;
     const coinAddress = getMarketAddress(symbolEmojis);
     const coinType = toCoinTypes(coinAddress).emojicoin;
 
+    await emojicoin
+      .buy(swapper, symbolEmojis, buyAmountQuote)
+      .then(({ swap }) => {
+        fetchRealReserves(symbolEmojis)
+          .then((res) => res!)
+          .then(({ base, quote }) => {
+            // Expect the base reserves to simply equal the supply minus what the user just bought.
+            expect(base).toEqual(EMOJICOIN_SUPPLY - swap.model.swap.netProceeds);
+            // The quote reserves are equal to the amount of APT the user just exchanged for base.
+            expect(quote).toEqual(EXACT_TRANSITION_INPUT_AMOUNT + ONE_APT_BIGINT);
+            expect(isInBondingCurve(swap.model.state)).toBe(false);
+          });
+      })
+      .then(() => emojicoin.sell(swapper, symbolEmojis, sellAmountEmojicoin))
+      .then(({ response, swap }) => {
+        fetchRealReserves(symbolEmojis)
+          .then((res) => res!)
+          .then(({ base, quote }) => {
+            const userBalance = getCoinBalanceFromChanges({ response, userAddress, coinType })!;
+            expect(userBalance).toBeDefined();
+            const previousBaseReserves = EMOJICOIN_SUPPLY - userBalance;
+            expect(sellAmountEmojicoin).toEqual(swap.model.swap.baseVolume);
+            // The base reserves now are the previous reserves plus the amount of base sold.
+            expect(base).toEqual(previousBaseReserves + sellAmountEmojicoin);
+            const { netProceeds } = swap.model.swap;
+            // The quote reserves now are the previous reserves minus the quote volume/net proceeds.
+            expect(netProceeds).toEqual(swap.model.swap.quoteVolume);
+            expect(quote).toEqual(EXACT_TRANSITION_INPUT_AMOUNT + ONE_APT_BIGINT - netProceeds);
+          });
+      });
+  });
+
+  it("checks the real reserves after a buy and a sell", async () => {
+    const idx = 3;
+    const [swapper, symbolEmojis] = [registrants[idx], marketSymbols[idx]];
     const buyAmount = ONE_APT_BIGINT;
+    const userAddress = swapper.accountAddress;
+    const coinAddress = getMarketAddress(symbolEmojis);
+    const coinType = toCoinTypes(coinAddress).emojicoin;
+
     const { userBalance, baseAfterBuy } = await emojicoin
       .buy(swapper, symbolEmojis, buyAmount)
       .then(async ({ response, swap }) => {
@@ -117,16 +161,12 @@ describe("tests the calculation functions for circulating supply and real reserv
         expect(realReservesFromFetch).toBeDefined();
         expect(realReservesFromFetch).toEqual(realReservesFromTxnResponse);
         const { base, quote } = realReservesFromFetch!;
-        const balance = getCoinBalanceFromChanges({
-          response,
-          userAddress: swapper.accountAddress,
-          coinType,
-        })!;
+        const balance = getCoinBalanceFromChanges({ response, userAddress, coinType })!;
+        expect(balance).toBeDefined();
         // The base reserves are just the total emojicoin supply minus what the user just received.
         expect(base).toEqual(EMOJICOIN_SUPPLY - balance);
         // The quote reserves are equal to however much APT has been deposited to the bonding curve.
         expect(quote).toEqual(buyAmount);
-        expect(balance).toBeDefined();
         return {
           baseAfterBuy: base,
           userBalance: balance!,
@@ -152,6 +192,71 @@ describe("tests the calculation functions for circulating supply and real reserv
       // The new quote reserves are equal to the original buy amount minus the net proceeds returned
       // from the swap event.
       expect(quote).toEqual(buyAmount - swap.event.netProceeds);
+    });
+  });
+
+  const verifyReservesWithBuyBuyBuySell = async ({
+    idx,
+    amounts,
+  }: {
+    idx: number;
+    amounts: bigint[];
+  }) => {
+    const [swapper, symbolEmojis] = [registrants[idx], marketSymbols[idx]];
+
+    const userAddress = swapper.accountAddress;
+    const coinAddress = getMarketAddress(symbolEmojis);
+    const coinType = toCoinTypes(coinAddress).emojicoin;
+
+    // Verify the real reserves equals the total supply - circulating supply.
+    const verifyCSAndReserves = (response: UserTransactionResponse, swap: SwapEventModel) => {
+      const realReserves = calculateRealReserves(swap.state);
+      const circulatingSupply = calculateCirculatingSupply(swap.state);
+      expect(realReserves).toEqual(EMOJICOIN_SUPPLY - circulatingSupply);
+      const userBalance = getCoinBalanceFromChanges({ response, userAddress, coinType });
+      expect(userBalance).toEqual(circulatingSupply);
+    };
+
+    // buy before bonding curve, buy to bonding curve, buy after bonding curve, then sell
+    // verify all are expected
+    await emojicoin
+      .buy(swapper, symbolEmojis, amounts[0])
+      .then(({ swap, response }) => {
+        verifyCSAndReserves(response, swap.model);
+        return emojicoin.buy(swapper, symbolEmojis, amounts[1]);
+      })
+      .then(({ swap, response }) => {
+        verifyCSAndReserves(response, swap.model);
+        return emojicoin.buy(swapper, symbolEmojis, amounts[2]);
+      })
+      .then(({ swap, response }) => {
+        verifyCSAndReserves(response, swap.model);
+        return emojicoin.sell(swapper, symbolEmojis, amounts[3]);
+      })
+      .then(({ swap, response }) => verifyCSAndReserves(response, swap.model));
+  };
+
+  it("verifies the real reserves are total supply - circulating supply", async () => {
+    verifyReservesWithBuyBuyBuySell({
+      idx: 4,
+      amounts: [
+        EXACT_TRANSITION_INPUT_AMOUNT / 2n, // Buy in the bonding curve only.
+        EXACT_TRANSITION_INPUT_AMOUNT / 2n, // Buy exactly to the state transition.
+        ONE_APT_BIGINT, // Buy post bonding curve.
+        ONE_APT_BIGINT * 2n, // Sell lower than what the top bonding curve price would be at.
+      ],
+    });
+  });
+
+  it("verifies the real reserves are total supply - circulating supply, huge amounts", async () => {
+    verifyReservesWithBuyBuyBuySell({
+      idx: 5,
+      amounts: [
+        (EXACT_TRANSITION_INPUT_AMOUNT * 3n) / 4n, // Buy in the bonding curve only.
+        EXACT_TRANSITION_INPUT_AMOUNT / 4n, // Buy exactly to the state transition.
+        ONE_APT_BIGINT * 100n, // Buy post bonding curve.
+        ONE_APT_BIGINT * 200n, // Sell lower than what the top bonding curve price would be at.
+      ],
     });
   });
 });
