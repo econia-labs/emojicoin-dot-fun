abbf5de0eb54e2c7ba981347cc0d048379a0824a
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-04 18:15:09 -0800

    Remove flaky volume test and associated code
    Bump version for rc

--- src/typescript/sdk/tests/e2e/helpers/misc.ts
+++ src/typescript/sdk/tests/e2e/helpers/misc.ts
@@ -1,51 +1,7 @@
-import { type AccountAddressInput, type UserTransactionResponse } from "@aptos-labs/ts-sdk";
-import {
-  BASIS_POINTS_PER_UNIT,
-  getEvents,
-  getMarketResourceFromWriteSet,
-  INTEGRATOR_FEE_RATE_BPS,
-  Period,
-  periodEnumToRawDuration,
-  rawPeriodToEnum,
-  type Types,
-} from "../../../src";
+import { BASIS_POINTS_PER_UNIT, INTEGRATOR_FEE_RATE_BPS } from "../../../src";
 import postgres from "postgres";
 import Big from "big.js";
 import { EXACT_TRANSITION_INPUT_AMOUNT } from "../../utils";
-
-export const getTrackerFromWriteSet = <T extends Period>(
-  res: UserTransactionResponse,
-  marketAddress: AccountAddressInput,
-  period: T
-) => {
-  const market = getMarketResourceFromWriteSet(res, marketAddress);
-  const tracker = market?.periodicStateTrackers.find((t) => rawPeriodToEnum(t.period) === period);
-  return tracker;
-};
-
-/**
- * Get the periodic state tracker's period expiry time by converting the start time to
- * milliseconds and adding 1 minute, then converting that to a Date object.
- *
- * @param tracker The periodic state tracker to get the period expiry for.
- * @param period The period to get the expiry for.
- * @returns The period expiry time as a Date object.
- */
-export const getPeriodExpiryDate = <T extends Period>(
-  tracker: Types["PeriodicStateTracker"],
-  period: T
-): Date => {
-  // Convert from micro to milliseconds. This value is never fractional, per the smart contract.
-  const millisecondStartTime = Number(tracker.startTime / 1000n);
-  const periodDuration = periodEnumToRawDuration(period);
-  const periodDurationMilliseconds = periodDuration.valueOf() / 1000;
-  return new Date(millisecondStartTime + periodDurationMilliseconds);
-};
-
-export const getOneMinutePeriodicStateEvents = (res: UserTransactionResponse) =>
-  getEvents(res).periodicStateEvents.filter(
-    (e) => rawPeriodToEnum(e.periodicStateMetadata.period) === Period.Period1M
-  );
 
 export const getDbConnection = () => {
   return postgres(process.env.DB_URL!);
