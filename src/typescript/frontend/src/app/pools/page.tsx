0f3ce2282e19eb3f4d15dcc2002828c5ff88d820
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-06 22:44:24 -0800

    Add proper search params to the pools page

--- src/typescript/frontend/src/app/pools/page.tsx
+++ src/typescript/frontend/src/app/pools/page.tsx
@@ -1,9 +1,10 @@
 import ClientPoolsPage, { type PoolsData } from "components/pages/pools/ClientPoolsPage";
 import { getPoolData } from "./api/getPoolDataQuery";
-import { SortMarketsBy } from "@sdk/indexer-v2/types/common";
 import { symbolBytesToEmojis } from "@sdk/emoji_data/utils";
 import { type Metadata } from "next";
 import { emoji, parseJSON } from "utils";
+import { getValidSortByForPoolsPage } from "@sdk/indexer-v2/queries/query-params";
+import { safeParsePageWithDefault, handleEmptySearchBytes } from "lib/routes/home-page-params";
 
 export const revalidate = 2;
 
@@ -12,16 +13,31 @@ export const metadata: Metadata = {
   description: `Provide ${emoji("water wave")}liquidity${emoji("water wave")} and earn APR using your emojis !`,
 };
 
-export default async function PoolsPage({ searchParams }: { searchParams: { pool: string } }) {
+type PoolsSearchParams = {
+  page: string | null;
+  sortby: string | null;
+  orderby: string | null;
+  searchBytes: string | null;
+  pool: string | null;
+  account: string | null;
+};
+
+export default async function PoolsPage({ searchParams }: { searchParams: PoolsSearchParams }) {
+  const page = safeParsePageWithDefault(searchParams.page);
+  const sortBy = getValidSortByForPoolsPage(searchParams.sortby);
+  const orderBy = searchParams.orderby ?? "desc";
+  const q = handleEmptySearchBytes(searchParams.searchBytes);
+  const searchEmojis = q ? symbolBytesToEmojis(q).emojis.map((e) => e.emoji) : undefined;
+
+  if (orderBy !== "asc" && orderBy !== "desc") {
+    throw new Error("Invalid params");
+  }
+
+  // The liquidity `provider`, aka the account to search for in the user liquidity pools.
+  const provider = searchParams.account;
+
   const initialData: PoolsData[] = parseJSON(
-    await getPoolData(
-      1,
-      SortMarketsBy.AllTimeVolume,
-      "desc",
-      searchParams.pool
-        ? symbolBytesToEmojis(searchParams.pool).emojis.map((e) => e.emoji)
-        : undefined
-    )
+    await getPoolData(page, sortBy, orderBy, searchEmojis, provider ?? undefined)
   );
 
   return <ClientPoolsPage initialData={initialData} />;
