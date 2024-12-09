b87cf16ea5ca6f619257a92acb00c562e3117b61
Bogdan Crisan <bogdan@crbl.studio>
2024-12-09 12:35:53 +0100

    Fix spelling error

--- src/typescript/frontend/src/components/pages/metadata/MetadataPage.tsx
+++ src/typescript/frontend/src/components/pages/metadata/MetadataPage.tsx
@@ -7,8 +7,7 @@ type MetadataField = { name: string; placeholder: string };
 
 const DEFAULT_FIELDS: [MetadataField, string][] = [
   [{ name: "CTO link", placeholder: "https://..." }, ""],
-  // cspell:word moto
-  [{ name: "Moto", placeholder: "The best shoe market ever!" }, ""],
+  [{ name: "Motto", placeholder: "The best shoe market ever!" }, ""],
 ];
 
 const BUTTON_CLASSNAME = "text-white px-4 border-ec-blue border-solid border-[3px] rounded-xl";
