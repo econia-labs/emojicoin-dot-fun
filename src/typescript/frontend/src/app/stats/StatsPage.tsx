2f059c3cc9756a215fe0d5af85bfdc6044da4e6a
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-12 13:48:44 -0800

    Test

--- src/typescript/frontend/src/app/stats/StatsPage.tsx
+++ src/typescript/frontend/src/app/stats/StatsPage.tsx
@@ -1,10 +1,14 @@
 import { type DatabaseModels } from "@sdk/indexer-v2/types";
 import { LoadingSkeleton } from "./LoadingSkeleton";
-import { MarketPreviewCarousel } from "./MarketPreview";
+import { StatsCarousel } from "./MarketPreview";
 
 export interface HomePageProps {
   numMarkets: Promise<number>;
-  priceFeed: Promise<DatabaseModels["price_feed"][]>;
+  priceFeedData: Promise<DatabaseModels["price_feed"][]>;
+  marketCapData: Promise<DatabaseModels["market_state"][]>;
+  allTimeVolumeData: Promise<DatabaseModels["market_state"][]>;
+  latestPricesData: Promise<DatabaseModels["market_state"][]>;
+  tvlData: Promise<DatabaseModels["market_state"][]>;
 }
 
 export default function StatsPageComponent(props: HomePageProps) {
@@ -21,11 +25,56 @@ export default function StatsPageComponent(props: HomePageProps) {
             />
           </div>
         </div>
-        <div className="flex flex-row w-fit">
-          <LoadingSkeleton
-            promised={props.priceFeed.then((res) => (
-              <MarketPreviewCarousel markets={res} />
-            ))}
+        <div className="flex flex-col">
+          <StatsCarousel
+            elements={[
+              props.numMarkets.then((numMarkets) => <div>{numMarkets}</div>),
+              props.priceFeedData.then((priceFeedData) => (
+                <div className="flex flex-row m-auto text-white">
+                  {priceFeedData.map((v) => (
+                    <div key={`priceFeedData-${v.market.symbolData.symbol}`} className="text-white text-lg">
+                      {v.market.symbolData.symbol}
+                    </div>
+                  ))}
+                </div>
+              )),
+              props.marketCapData.then((marketCapData) => (
+                <div className="flex flex-row m-auto text-white">
+                  {marketCapData.map((v) => (
+                    <div key={`marketCapData-${v.market.symbolData.symbol}`} className="text-white text-lg">
+                      {v.market.symbolData.symbol}
+                    </div>
+                  ))}
+                </div>
+              )),
+              props.allTimeVolumeData.then((allTimeVolumeData) => (
+                <div className="flex flex-row m-auto text-white">
+                  {allTimeVolumeData.map((v) => (
+                    <div key={`allTimeVolumeData-${v.market.symbolData.symbol}`} className="text-white text-lg">
+                      {v.market.symbolData.symbol}
+                    </div>
+                  ))}
+                </div>
+              )),
+              props.latestPricesData.then((latestPricesData) => (
+                <div className="flex flex-row m-auto text-white">
+                  {latestPricesData.map((v) => (
+                    <div key={`latestPricesData-${v.market.symbolData.symbol}`} className="text-white text-lg">
+                      {v.market.symbolData.symbol}
+                    </div>
+                  ))}
+                </div>
+              )),
+              props.tvlData.then((tvlData) => (
+                <div className="flex flex-row m-auto text-white">
+                  {tvlData.map((v) => (
+                    <div key={`tvlData-${v.market.symbolData.symbol}`} className="text-white text-lg">
+                      {v.market.symbolData.symbol}
+                    </div>
+                  ))}
+                </div>
+              )),
+            ]}
           />
         </div>
       </div>
