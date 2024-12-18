f63ae63947c215d636835f75bae0be02baf3cf3d
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-17 20:22:24 -0800

    Remove unused packages and tailwind config stuff

--- src/typescript/frontend/tailwind.config.js
+++ src/typescript/frontend/tailwind.config.js
@@ -8,56 +8,6 @@ delete colors.coolGray;
 delete colors.blueGray;
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-const shadCnStyles = {
-  borderRadius: {
-    lg: "var(--radius)",
-    md: "calc(var(--radius) - 2px)",
-    sm: "calc(var(--radius) - 4px)",
-  },
-  colors: {
-    background: "hsl(var(--background))",
-    foreground: "hsl(var(--foreground))",
-    card: {
-      DEFAULT: "hsl(var(--card))",
-      foreground: "hsl(var(--card-foreground))",
-    },
-    popover: {
-      DEFAULT: "hsl(var(--popover))",
-      foreground: "hsl(var(--popover-foreground))",
-    },
-    primary: {
-      DEFAULT: "hsl(var(--primary))",
-      foreground: "hsl(var(--primary-foreground))",
-    },
-    secondary: {
-      DEFAULT: "hsl(var(--secondary))",
-      foreground: "hsl(var(--secondary-foreground))",
-    },
-    muted: {
-      DEFAULT: "hsl(var(--muted))",
-      foreground: "hsl(var(--muted-foreground))",
-    },
-    accent: {
-      DEFAULT: "hsl(var(--accent))",
-      foreground: "hsl(var(--accent-foreground))",
-    },
-    destructive: {
-      DEFAULT: "hsl(var(--destructive))",
-      foreground: "hsl(var(--destructive-foreground))",
-    },
-    border: "hsl(var(--border))",
-    input: "hsl(var(--input))",
-    ring: "hsl(var(--ring))",
-    chart: {
-      1: "hsl(var(--chart-1))",
-      2: "hsl(var(--chart-2))",
-      3: "hsl(var(--chart-3))",
-      4: "hsl(var(--chart-4))",
-      5: "hsl(var(--chart-5))",
-    },
-  },
-};
-
 module.exports = {
   darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
@@ -92,7 +42,6 @@ module.exports = {
         white: "0 0 2px #FFFFFF",
         gray: "0 0 2px #717386",
       },
-      ...shadCnStyles,
     },
     colors: {
       ...colors,
@@ -272,6 +221,5 @@ module.exports = {
       };
       addUtilities(newUtilities, ["responsive"]);
     },
-    require("tailwindcss-animate"),
   ],
 };
