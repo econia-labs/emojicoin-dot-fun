af2e7b9190c182ff58a2b392dfea48dc7db77379
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-18 19:53:28 -0800

    Add the shadcn select dropdown with proper styling and functionality

--- src/typescript/frontend/src/app/stats/StatsPage.tsx
+++ src/typescript/frontend/src/app/stats/StatsPage.tsx
@@ -1,5 +1,15 @@
+"use client";
+
 import { type DatabaseModels } from "@sdk/indexer-v2/types";
 import { ClientTable } from "./ClientTable";
+import { useState } from "react";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "components/ui/Select";
 
 export interface StatsPageProps {
   numMarkets: number;
@@ -12,24 +22,45 @@ export interface StatsPageProps {
 }
 
 export default function StatsPageComponent(props: StatsPageProps) {
+  const [tab, setTab] = useState<"global-stats" | "per-market-stats">("per-market-stats");
+
   return (
     <>
-      <div className="flex flex-col mb-[31px] opacity-[.95] w-full">
-        <div className="flex flex-row m-auto">
-          <span>{"num markets:"}</span>
-          <div>{props.numMarkets}</div>
-          <span>{"global stats"}</span>
-          <div>{"global stats go here or something"}</div>
+      <div className="flex flex-col mb-[31px] w-full text-white">
+        <div className="flex w-fit text-white m-auto font-forma-bold mb-4">
+          <Select onValueChange={(v: string) => setTab(v as "global-stats" | "per-market-stats")}>
+            <SelectTrigger className="">
+              <SelectValue defaultValue={tab} placeholder="Select a view" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem
+                className="text-white font-forma-bold hover:bg-light-gray"
+                value="global-stats"
+              >
+                Global stats
+              </SelectItem>
+              <SelectItem
+                className="text-white font-forma-bold hover:bg-light-gray"
+                value="per-market-stats"
+              >
+                Per market stats
+              </SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-        <ClientTable
-          price={props.latestPricesData}
-          allTimeVolume={props.allTimeVolumeData}
-          priceDelta={props.priceFeedData}
-          dailyVolume={props.dailyVolumeData}
-          lastAvgExecutionPrice={props.latestPricesData}
-          tvl={props.tvlData}
-          marketCap={props.marketCapData}
-        />
+        {tab === "global-stats" ? (
+          <></>
+        ) : (
+          <ClientTable
+            price={props.latestPricesData}
+            allTimeVolume={props.allTimeVolumeData}
+            priceDelta={props.priceFeedData}
+            dailyVolume={props.dailyVolumeData}
+            lastAvgExecutionPrice={props.latestPricesData}
+            tvl={props.tvlData}
+            marketCap={props.marketCapData}
+          />
+        )}
       </div>
     </>
   );
--- src/typescript/frontend/src/app/stats/TableData.tsx
+++ src/typescript/frontend/src/app/stats/TableData.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { toNominal } from "lib/utils/decimals";
 import { calculateCurvePrice, calculateCirculatingSupply } from "@sdk/markets";
 import { MiniBondingCurveProgress } from "./MiniBondingCurveProgress";
@@ -29,48 +29,30 @@ export const TableData = <T extends DatabaseModels["price_feed"] | DatabaseModel
   reversed: boolean;
   priceDeltas: DatabaseModels["price_feed"][];
 }) => {
+  const getCN = useCallback(
+    (col: Column) => (col === k ? " bg-opacity-[0.04] bg-ec-blue" : ""),
+    [k]
+  );
   const { cells, tableData } = useMemo(() => {
     const deltaByMarket = new Map(
       priceDeltas.map(({ deltaPercentage, market }) => [market.symbolData.symbol, deltaPercentage])
     );
     const bigNumberFormatter = new Intl.NumberFormat("en-US", { maximumFractionDigits: 2 });
     const fmt = (n: number) => bigNumberFormatter.format(n);
-    const getCN = (col: Column) => (col === k ? "bg-opacity-[0.04] bg-ec-blue" : "");
     return {
       cells: {
-        delta: {
-          cn: getCN(Column.PriceDelta),
-          getValue: (v: T) => deltaByMarket.get(v.market.symbolData.symbol)?.toFixed(4) ?? "-",
-        },
-        price: {
-          cn: getCN(Column.Price),
-          getValue: (v: T) => calculateCurvePrice(v.state).toFixed(8),
-        },
-        allTimeVolume: {
-          cn: getCN(Column.AllTimeVolume),
-          getValue: (v: T) => fmt(toNominal(v.state.cumulativeStats.quoteVolume)),
-        },
-        dailyVolume: {
-          cn: getCN(Column.DailyVolume),
-          getValue: (v: T) => fmt(toNominal(v.dailyVolume)),
-        },
-        lastAvgPrice: {
-          cn: getCN(Column.LastAvgExecutionPrice),
-          getValue: (v: T) => toNominalPrice(v.lastSwap.avgExecutionPriceQ64).toFixed(8),
-        },
-        tvl: {
-          cn: getCN(Column.Tvl),
-          getValue: (v: T) => fmt(toNominal(v.state.instantaneousStats.totalValueLocked)),
-        },
-        marketCap: {
-          cn: getCN(Column.MarketCap),
-          getValue: (v: T) => fmt(toNominal(v.state.instantaneousStats.marketCap)),
-        },
+        delta: (v: T) => deltaByMarket.get(v.market.symbolData.symbol)?.toFixed(4) ?? "-",
+        price: (v: T) => calculateCurvePrice(v.state).toFixed(8),
+        allTimeVolume: (v: T) => fmt(toNominal(v.state.cumulativeStats.quoteVolume)),
+        dailyVolume: (v: T) => fmt(toNominal(v.dailyVolume)),
+        tvl: (v: T) => fmt(toNominal(v.state.instantaneousStats.totalValueLocked)),
+        lastAvgPrice: (v: T) => toNominalPrice(v.lastSwap.avgExecutionPriceQ64).toFixed(8),
+        marketCap: (v: T) => fmt(toNominal(v.state.instantaneousStats.marketCap)),
         getCirculatingSupply: (v: T) => fmt(toNominal(calculateCirculatingSupply(v.state))),
       },
       tableData: reversed ? data.toReversed() : data,
     };
-  }, [k, reversed, priceDeltas, data]);
+  }, [reversed, priceDeltas, data]);
 
   return tableData.map((row, i) => (
     <tr
@@ -86,20 +68,18 @@ export const TableData = <T extends DatabaseModels["price_feed"] | DatabaseModel
           {row.market.symbolData.symbol}
         </a>
       </td>
-      <td className={cells.delta.cn}>{cells.delta.getValue(row)}</td>
+      <td className={getCN(Column.PriceDelta)}>{cells.delta(row)}</td>
       <td>{row.market.marketID.toString()}</td>
-      <td className={cells.price.cn}>{cells.price.getValue(row)}</td>
-      <td className={cells.allTimeVolume.cn}>{cells.allTimeVolume.getValue(row)}</td>
-      <td className={cells.dailyVolume.cn}>{cells.dailyVolume.getValue(row)}</td>
-      <td className={cells.tvl.cn}>{cells.tvl.getValue(row)}</td>
-      <td className={cells.lastAvgPrice.cn}>
-        {
-          <ExplorerLink className="hover:underline" value={row.transaction.version} type="txn">
-            {cells.lastAvgPrice.getValue(row)}
-          </ExplorerLink>
-        }
+      <td className={getCN(Column.Price)}>{cells.price(row)}</td>
+      <td className={getCN(Column.AllTimeVolume)}>{cells.allTimeVolume(row)}</td>
+      <td className={getCN(Column.DailyVolume)}>{cells.dailyVolume(row)}</td>
+      <td className={getCN(Column.Tvl)}>{cells.tvl(row)}</td>
+      <td className={getCN(Column.LastAvgExecutionPrice)}>
+        <ExplorerLink className="hover:underline" value={row.transaction.version} type="txn">
+          {cells.lastAvgPrice(row)}
+        </ExplorerLink>
       </td>
-      <td className={cells.marketCap.cn}>{cells.marketCap.getValue(row)}</td>
+      <td className={getCN(Column.MarketCap)}>{cells.marketCap(row)}</td>
       <td>{cells.getCirculatingSupply(row)}</td>
       <td>
         <MiniBondingCurveProgress state={row.state} />
