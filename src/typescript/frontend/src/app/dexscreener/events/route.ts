2e1233099e7cc2226858ee278b0b0cd4b43f12ee
Matt <90358481+xbtmatt@users.noreply.github.com>
2024-12-20 09:01:40 -0800

    [ECO-2615] Fix the dexscreener endpoints to return floored numbers, set caching policies (#473)

--- src/typescript/frontend/src/app/dexscreener/events/route.ts
+++ src/typescript/frontend/src/app/dexscreener/events/route.ts
@@ -88,7 +88,27 @@ import { calculateCurvePrice, calculateRealReserves } from "@sdk/markets";
 import { toCoinDecimalString } from "../../../lib/utils/decimals";
 import { DECIMALS } from "@sdk/const";
 import { symbolEmojisToPairId } from "../util";
-import { compareBigInt } from "@econia-labs/emojicoin-sdk";
+import { compareBigInt, type Flatten } from "@econia-labs/emojicoin-sdk";
+import { type XOR } from "@sdk/utils/utility-types";
+
+export type Asset0In1Out = {
+  asset0In: number | string;
+  asset1Out: number | string;
+};
+
+export type Asset1In0Out = {
+  asset0Out: number | string;
+  asset1In: number | string;
+};
+
+export type AssetInOut = XOR<Asset0In1Out, Asset1In0Out>;
+
+export type DexscreenerReserves = {
+  reserves: {
+    asset0: number | string;
+    asset1: number | string;
+  };
+};
 
 /**
  * - `txnId` is a transaction identifier such as a transaction hash
@@ -128,24 +148,19 @@ import { compareBigInt } from "@econia-labs/emojicoin-sdk";
  * - The Indexer automatically handles calculations for USD pricing (`priceUsd` as opposed to
  * `priceNative`)
  */
-export interface SwapEvent {
-  eventType: "swap";
-  txnId: string;
-  txnIndex: number;
-  eventIndex: number;
-  maker: string;
-  pairId: string;
-  asset0In?: number | string;
-  asset1In?: number | string;
-  asset0Out?: number | string;
-  asset1Out?: number | string;
-  priceNative: number | string;
-  reserves?: {
-    asset0: number | string;
-    asset1: number | string;
-  };
-  metadata?: Record<string, string>;
-}
+export type SwapEvent = Flatten<
+  {
+    eventType: "swap";
+    txnId: string;
+    txnIndex: number;
+    eventIndex: number;
+    maker: string;
+    pairId: string;
+    priceNative: number | string;
+    metadata?: Record<string, string>;
+  } & AssetInOut &
+    DexscreenerReserves
+>;
 
 /**
  * - `txnId` is a transaction identifier such as a transaction hash
@@ -167,21 +182,19 @@ export interface SwapEvent {
  * - `metadata` includes any optional auxiliary info not covered in the default schema and not
  * required in most cases
  */
-interface JoinExitEvent {
-  eventType: "join" | "exit";
-  txnId: string;
-  txnIndex: number;
-  eventIndex: number;
-  maker: string;
-  pairId: string;
-  amount0: number | string;
-  amount1: number | string;
-  reserves?: {
-    asset0: number | string;
-    asset1: number | string;
-  };
-  metadata?: Record<string, string>;
-}
+type JoinExitEvent = Flatten<
+  {
+    eventType: "join" | "exit";
+    txnId: string;
+    txnIndex: number;
+    eventIndex: number;
+    maker: string;
+    pairId: string;
+    amount0: number | string;
+    amount1: number | string;
+    metadata?: Record<string, string>;
+  } & DexscreenerReserves
+>;
 
 type BlockInfo = { block: Block };
 type Event = (SwapEvent | JoinExitEvent) & BlockInfo;
@@ -189,27 +202,18 @@ type Event = (SwapEvent | JoinExitEvent) & BlockInfo;
 interface EventsResponse {
   events: Event[];
 }
-
 function toDexscreenerSwapEvent(event: ReturnType<typeof toSwapEventModel>): SwapEvent & BlockInfo {
-  let assetInOut;
-
-  if (event.swap.isSell) {
-    // We are selling to APT
-    assetInOut = {
-      asset0In: toCoinDecimalString(event.swap.inputAmount, DECIMALS),
-      asset0Out: 0,
-      asset1In: 0,
-      asset1Out: toCoinDecimalString(event.swap.baseVolume, DECIMALS),
-    };
-  } else {
-    // We are buying with APT
-    assetInOut = {
-      asset0In: 0,
-      asset0Out: toCoinDecimalString(event.swap.quoteVolume, DECIMALS),
-      asset1In: toCoinDecimalString(event.swap.inputAmount, DECIMALS),
-      asset1Out: 0,
-    };
-  }
+  // Base / quote is emojicoin / APT.
+  // Thus asset0 / asset1 is always base volume / quote volume.
+  const assetInOut = event.swap.isSell
+    ? {
+        asset0In: toCoinDecimalString(event.swap.baseVolume, DECIMALS),
+        asset1Out: toCoinDecimalString(event.swap.quoteVolume, DECIMALS),
+      }
+    : {
+        asset0Out: toCoinDecimalString(event.swap.baseVolume, DECIMALS),
+        asset1In: toCoinDecimalString(event.swap.quoteVolume, DECIMALS),
+      };
 
   const { base, quote } = calculateRealReserves(event.state);
   const reserves = {
@@ -224,7 +228,7 @@ function toDexscreenerSwapEvent(event: ReturnType<typeof toSwapEventModel>): Swa
   return {
     block: {
       blockNumber: Number(event.blockAndEvent.blockNumber),
-      blockTimestamp: event.transaction.timestamp.getTime() / 1000,
+      blockTimestamp: Math.floor(event.transaction.timestamp.getTime() / 1000),
     },
     eventType: "swap",
     txnId: event.transaction.version.toString(),
@@ -236,11 +240,8 @@ function toDexscreenerSwapEvent(event: ReturnType<typeof toSwapEventModel>): Swa
     pairId: symbolEmojisToPairId(event.market.symbolEmojis),
 
     ...assetInOut,
-
-    asset0In: event.swap.inputAmount.toString(),
-    asset1Out: event.swap.quoteVolume.toString(),
     priceNative,
-    ...reserves,
+    reserves,
   };
 }
 
@@ -258,7 +259,7 @@ function toDexscreenerJoinExitEvent(
   return {
     block: {
       blockNumber: Number(event.blockAndEvent.blockNumber),
-      blockTimestamp: event.transaction.timestamp.getTime() / 1000,
+      blockTimestamp: Math.floor(event.transaction.timestamp.getTime() / 1000),
     },
     eventType: event.liquidity.liquidityProvided ? "join" : "exit",
 
@@ -290,7 +291,14 @@ async function getEventsByVersion(fromBlock: number, toBlock: number): Promise<E
     );
 }
 
-// NextJS JSON response handler
+// Don't cache this request, because we could inadvertently cache data that is immediately invalid
+// in the case where the `toBlock` is larger than the present block. Although this shouldn't happen,
+// it's not worth caching these queries anyway, because they should only be called once or twice
+// with the same query parameters.
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 /**
  * We treat our versions as "blocks" because it's faster to implement given our current architecture
  * This requires dexscreener to have relatively large `fromBlock - toBlock` ranges to keep up
