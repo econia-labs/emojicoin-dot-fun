48f44733a3b7dc2166daa335d6808a927ea80b00
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-03 18:26:29 -0800

    [ECO-1588] Add minimal JSON stats page to frontend (#466)

--- src/typescript/frontend/src/components/link/component.tsx
+++ src/typescript/frontend/src/components/link/component.tsx
@@ -2,38 +2,6 @@ import React from "react";
 import { StyledLink, RouterLink } from "./styled";
 import { EXTERNAL_LINK_PROPS } from "./const";
 import { type LinkProps } from "./types";
-import { toExplorerLink } from "lib/utils/explorer-link";
-import { type AnyNumberString } from "@sdk-types";
-
-export const ExplorerLink = ({
-  value,
-  type,
-  network,
-  children,
-  style,
-  className,
-}: {
-  value: AnyNumberString;
-  type: "acc" | "account" | "transaction" | "version" | "txn";
-  children: React.ReactNode;
-  network?: string;
-  style?: React.CSSProperties;
-  className?: string;
-}) => {
-  const href = toExplorerLink({ value, linkType: type, network });
-  return (
-    <a
-      style={style}
-      className={className + " explorer-link"}
-      href={href}
-      {...EXTERNAL_LINK_PROPS}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {children}
-    </a>
-  );
-};
 
 const Link: React.FC<LinkProps> = ({ external, href, underline = false, target, ...props }) => {
   const internalProps = external ? EXTERNAL_LINK_PROPS : {};
