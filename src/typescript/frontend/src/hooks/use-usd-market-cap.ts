fa9d4e00129405e02ed6b224b19855af547abff9
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-06 12:22:45 -0800

    Make doc comment an actual jsdoc comment

--- src/typescript/frontend/src/hooks/use-usd-market-cap.ts
+++ src/typescript/frontend/src/hooks/use-usd-market-cap.ts
@@ -1,8 +1,11 @@
 import { useAptPrice } from "context/AptPrice";
 import { toNominal } from "lib/utils/decimals";
 
-// Returns market cap in USD.
-// If the APT price isn't available, undefined will be returned.
+/**
+ * Returns the market cap in USD.
+ *
+ * If the APT price isn't available, it will return `undefined`.
+ */
 export function useUsdMarketCap(marketCapInOctas: bigint): number | undefined {
   const aptPrice = useAptPrice();
   return aptPrice ? toNominal(marketCapInOctas) * aptPrice : undefined;
