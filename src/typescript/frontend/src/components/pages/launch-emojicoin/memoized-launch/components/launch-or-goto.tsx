23c043a21c3ecb433d3f4f7bd7875652726af096
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-27 19:11:00 -0800

    Undo link variable thing

--- src/typescript/frontend/src/components/pages/launch-emojicoin/memoized-launch/components/launch-or-goto.tsx
+++ src/typescript/frontend/src/components/pages/launch-emojicoin/memoized-launch/components/launch-or-goto.tsx
@@ -23,23 +23,20 @@ export const LaunchButtonOrGoToMarketLink = ({
     overflow: true,
   };
 
-  const link = (
-    <Link
-      className="font-pixelar text-lg uppercase text-ec-blue"
-      href={path.join(ROUTES.market, emojis.join(""))}
-    >
-      <Button scale="lg" scrambleProps={scrambleProps}>
-        {t("Go to emojicoin market")}
-      </Button>
-    </Link>
-  );
   return (
     <>
       {/* Force displaying the "Go to emojicoin market" link if the market is already registered,
           regardless of whether or not the user is connected. */}
       <ButtonWithConnectWalletFallback forceDisplayChildren={registered}>
         {registered ? (
-          link
+          <Link
+            className="font-pixelar text-lg uppercase text-ec-blue"
+            href={path.join(ROUTES.market, emojis.join(""))}
+          >
+            <Button scale="lg" scrambleProps={scrambleProps}>
+              {t("Go to emojicoin market")}
+            </Button>
+          </Link>
         ) : (
           <Button
             disabled={invalid || typeof registered === "undefined"}
