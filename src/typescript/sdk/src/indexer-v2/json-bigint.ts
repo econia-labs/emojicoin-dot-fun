d02254591e8fc7c6163d5c2b9be0fa733c0bbfb7
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-30 15:27:48 -0800

    Add a fallback parsing function in case the designated one fails

--- src/typescript/sdk/src/indexer-v2/json-bigint.ts
+++ src/typescript/sdk/src/indexer-v2/json-bigint.ts
@@ -12,6 +12,26 @@ const JSON_BIGINT = parse({
   constructorAction: "ignore",
 });
 
+/**
+ * In case a field doesn't match up with its proper parsing function, fall back to parsing it with
+ * a primitive JSON parse.
+ */
+const tryWithFallbackParse = (parser: (v: any) => any) => (v: any) => {
+  try {
+    // Try the passed in parsing function.
+    return parser(v);
+  } catch {
+    // Otherwise, as a last resort, stringify and re-parse the JSON data and then return the value
+    // after parsing it, with no assumptions about what the parsing function should be and what data
+    // type it should return.
+    const fieldName = "substitute_json_field_name";
+    const stringified = JSON_BIGINT.stringify({
+      [fieldName]: v,
+    });
+    const parsed = JSON_BIGINT.parse(stringified);
+    return parsed[fieldName];
+  }
+};
 const parseFloat = (v: any) => Big(v).toString();
 const parseBigInt = (v: any) => BigInt(v);
 const parseInteger = (v: any) => Number(v);
@@ -34,8 +54,14 @@ const converter = new Map<AnyColumnName, (value: any) => any>([
  */
 export const parseJSONWithBigInts = <T>(msg: string): T => {
   return JSON_BIGINT.parse(msg, (key, value) => {
-    const fn = converter.get(key as AnyColumnName) ?? parseDefault;
-    return fn(value);
+    try {
+      const fn = converter.get(key as AnyColumnName) ?? parseDefault;
+      // Curry the retrieved parsing function to add a fallback parsing function.
+      const fnWithFallback = tryWithFallbackParse(fn);
+      return fnWithFallback(value);
+    } catch {
+      console.error(`Failed to parse ${key}: ${value} as a ${converter.get(key as AnyColumnName)}`);
+    }
   });
 };
 
