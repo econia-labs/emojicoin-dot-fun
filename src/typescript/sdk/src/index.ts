4059434066c133d531721a2e391c423f4e82298e
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-31 15:23:17 -0800

    Remove unused import resolution for indexer-v2

--- src/typescript/sdk/src/index.ts
+++ src/typescript/sdk/src/index.ts
@@ -4,6 +4,3 @@ export * from "./emojicoin_dot_fun";
 export * from "./markets";
 export * from "./types";
 export * from "./utils";
-
-// Explicitly resolve the duplicate export from "./indexer-v2" and "./types".
-export { type EventName } from "./types";
