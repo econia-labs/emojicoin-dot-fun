fc80c2909c80fe3bb72e0f853137cbe1bac9fb43
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-31 14:44:46 -0800

    Add the client and indexer-v2 into the package bundle in barrel file to facilitate simple auto-completion

--- src/typescript/sdk/src/index.ts
+++ src/typescript/sdk/src/index.ts
@@ -1,6 +1,11 @@
 export * from "./const";
+export * from "./client";
 export * from "./emoji_data";
 export * from "./emojicoin_dot_fun";
+export * from "./indexer-v2";
 export * from "./markets";
 export * from "./types";
 export * from "./utils";
+
+// Explicitly resolve the duplicate export from "./indexer-v2" and "./types".
+export { type EventName } from "./types";
