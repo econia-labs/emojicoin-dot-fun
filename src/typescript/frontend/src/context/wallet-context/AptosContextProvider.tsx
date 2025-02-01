1872d75f376b67e26b45c4f664a6bc7fda4c1fe4
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-01-31 20:09:51 -0800

    Clean up the AptosContextProvider and add the ability to set the transaction builder before submitting a txn

--- src/typescript/frontend/src/context/wallet-context/AptosContextProvider.tsx
+++ src/typescript/frontend/src/context/wallet-context/AptosContextProvider.tsx
@@ -1,27 +1,21 @@
 import {
-  AccountAddress,
   APTOS_COIN,
   AptosApiError,
   isUserTransactionResponse,
-  parseTypeTag,
-  TypeTag,
   type Aptos,
   type PendingTransactionResponse,
   type UserTransactionResponse,
 } from "@aptos-labs/ts-sdk";
-import { type NetworkInfo, useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import {
   createContext,
-  type Dispatch,
   type PropsWithChildren,
-  type SetStateAction,
   useCallback,
   useContext,
   useMemo,
   useState,
 } from "react";
 import { toast } from "react-toastify";
-
 import { type EntryFunctionTransactionBuilder } from "@sdk/emojicoin_dot_fun/payload-builders";
 import {
   checkNetworkAndToast,
@@ -33,15 +27,15 @@ import { type TypeTagInput } from "@sdk/emojicoin_dot_fun";
 import { DEFAULT_TOAST_CONFIG } from "const";
 import { sleep } from "@sdk/utils";
 import { useWalletBalance } from "lib/hooks/queries/use-wallet-balance";
-import {
-  getAptBalanceFromChanges,
-  getCoinBalanceFromChanges,
-} from "@sdk/utils/parse-changes-for-balances";
-import { getEventsAsProcessorModelsFromResponse } from "@sdk/indexer-v2/mini-processor";
-import { emoji } from "utils";
 import useIsUserGeoblocked from "@hooks/use-is-user-geoblocked";
 import { getAptosClient } from "@sdk/utils/aptos-client";
 import { useNameResolver } from "@hooks/use-name-resolver";
+import {
+  copyAddressHelper,
+  getFlattenedEventModelsFromResponse,
+  setBalancesFromWriteset,
+  setCoinTypeHelper,
+} from "./utils";
 
 type WalletContextState = ReturnType<typeof useWallet>;
 export type SubmissionResponse = Promise<{
@@ -61,8 +55,10 @@ export type EntryFunctionNames =
 
 export type AptosContextState = {
   aptos: Aptos;
-  submit: (builderFn: () => Promise<EntryFunctionTransactionBuilder>) => SubmissionResponse;
-  signThenSubmit: (builderFn: () => Promise<EntryFunctionTransactionBuilder>) => SubmissionResponse;
+  submit: () => SubmissionResponse;
+  signThenSubmit: () => SubmissionResponse;
+  transactionBuilder?: EntryFunctionTransactionBuilder;
+  setTransactionBuilder: (builder?: EntryFunctionTransactionBuilder) => void;
   account: WalletContextState["account"];
   copyAddress: () => void;
   status: TransactionStatus;
@@ -77,14 +73,6 @@ export type AptosContextState = {
   forceRefetch(coinType: TrackedCoinType): void;
   refetchIfStale(coinType: TrackedCoinType): void;
   setBalance(coinType: TrackedCoinType, n: bigint): void;
-  handleTransactionSubmission({
-    functionName,
-    res,
-  }: {
-    functionName: EntryFunctionNames;
-    res: PendingTransactionResponse;
-  }): SubmissionResponse;
-  linearSubmit: (builder: EntryFunctionTransactionBuilder) => SubmissionResponse;
 };
 
 export const AptosContext = createContext<AptosContextState | undefined>(undefined);
@@ -97,11 +85,12 @@ export function AptosContextProvider({ children }: PropsWithChildren) {
     submitTransaction,
     signTransaction,
   } = useWallet();
-  const pushEventFromClient = useEventStore((state) => state.pushEventFromClient);
   const [status, setStatus] = useState<TransactionStatus>("idle");
   const [lastResponse, setLastResponse] = useState<ResponseType>(null);
+  const pushEventsFromClient = useEventStore((s) => s.pushEventsFromClient);
   const [lastResponseStoredAt, setLastResponseStoredAt] = useState(-1);
-  const [emojicoinType, setEmojicoinType] = useState<string | undefined>();
+  const [emojicoinType, setEmojicoinType] = useState<string>();
+  const [transactionBuilder, setTransactionBuilder] = useState<EntryFunctionTransactionBuilder>();
   const geoblocked = useIsUserGeoblocked();
   // We could check `account?.ansName` here but it would require conditional hook logic, plus not all wallets provide it
   // so it's not really worth the extra effort and complexity.
@@ -120,78 +109,25 @@ export function AptosContextProvider({ children }: PropsWithChildren) {
     return getAptosClient();
   }, [network]);
 
-  const {
-    balance: aptBalance,
-    setBalance: setAptBalance,
-    isFetching: isFetchingAptBalance,
-    forceRefetch: forceRefetchAptBalance,
-    refetchIfStale: refetchAptBalanceIfStale,
-  } = useWalletBalance({
-    aptos,
-    accountAddress: account?.address,
-    coinType: APTOS_COIN,
-  });
+  const aptHelper = useWalletBalance({ aptos, account, coinType: APTOS_COIN });
+  const emojicoinHelper = useWalletBalance({ aptos, account, coinType: emojicoin });
+  const emojicoinLPHelper = useWalletBalance({ aptos, account, coinType: emojicoinLP });
 
-  const {
-    balance: emojicoinBalance,
-    setBalance: setEmojicoinBalance,
-    isFetching: isFetchingEmojicoinBalance,
-    forceRefetch: forceRefetchEmojicoinBalance,
-    refetchIfStale: refetchEmojicoinBalanceIfStale,
-  } = useWalletBalance({
-    aptos,
-    accountAddress: account?.address,
-    coinType: emojicoin,
-  });
-
-  const {
-    balance: emojicoinLPBalance,
-    setBalance: setEmojicoinLPBalance,
-    isFetching: isFetchingEmojicoinLPBalance,
-    forceRefetch: forceRefetchEmojicoinLPBalance,
-    refetchIfStale: refetchEmojicoinLPBalanceIfStale,
-  } = useWalletBalance({
-    aptos,
-    accountAddress: account?.address,
-    coinType: emojicoinLP,
-  });
-
-  const copyAddress = useCallback(async () => {
-    if (!account?.address) return;
-    try {
-      await navigator.clipboard.writeText(account.address);
-      toast.success(`Copied address to clipboard! ${emoji("clipboard")}`, {
-        pauseOnFocusLoss: false,
-        autoClose: 2000,
-      });
-    } catch {
-      toast.error(`Failed to copy address to clipboard. ${emoji("downcast face with sweat")}`, {
-        pauseOnFocusLoss: false,
-        autoClose: 2000,
-      });
-    }
-  }, [account?.address]);
+  const copyAddress = useCallback(async () => copyAddressHelper(account), [account]);
 
   const parseChangesAndSetBalances = useCallback(
     (response: UserTransactionResponse) => {
-      const userAddress = AccountAddress.from(response.sender);
-      // Return if the account is not connected or the sender of the transaction is not the currently connected account.
-      if (!account || !userAddress.equals(AccountAddress.from(account.address))) return;
-
-      const newAptBalance = getAptBalanceFromChanges(response, userAddress);
-      const newEmojicoinBalance = emojicoin
-        ? getCoinBalanceFromChanges({ response, userAddress, coinType: parseTypeTag(emojicoin) })
-        : undefined;
-      const newEmojicoinLPBalance = emojicoinLP
-        ? getCoinBalanceFromChanges({ response, userAddress, coinType: parseTypeTag(emojicoinLP) })
-        : undefined;
-      // Update the user's balance if the coins are present in the write set changes.
-      if (typeof newAptBalance !== "undefined") setAptBalance(newAptBalance);
-      if (typeof newEmojicoinBalance !== "undefined") setEmojicoinBalance(newEmojicoinBalance);
-      if (typeof newEmojicoinLPBalance !== "undefined")
-        setEmojicoinLPBalance(newEmojicoinLPBalance);
+      setBalancesFromWriteset({
+        response,
+        account,
+        emojicoin,
+        emojicoinLP,
+        setAptBalance: aptHelper.setBalance,
+        setEmojicoinBalance: emojicoinHelper.setBalance,
+        setEmojicoinLPBalance: emojicoinLPHelper.setBalance,
+      });
     },
-    [account, emojicoin, emojicoinLP, setAptBalance, setEmojicoinBalance, setEmojicoinLPBalance]
+    [account, emojicoin, emojicoinLP, aptHelper, emojicoinHelper, emojicoinLPHelper]
   );
 
   const handleTransactionSubmission = useCallback(
@@ -246,138 +182,103 @@ export function AptosContextProvider({ children }: PropsWithChildren) {
       }
       // Store any relevant events in the state event store for all components to see.
       if (response && isUserTransactionResponse(response)) {
-        const models = getEventsAsProcessorModelsFromResponse(response);
-        const flattenedEvents = [
-          ...models.marketRegistrationEvents,
-          ...models.periodicStateEvents,
-          ...models.swapEvents,
-          ...models.chatEvents,
-          ...models.liquidityEvents,
-          ...models.marketLatestStateEvents,
-        ];
-        flattenedEvents.forEach((e) => pushEventFromClient(e, true));
+        const flattenedEvents = getFlattenedEventModelsFromResponse(response);
+        pushEventsFromClient(flattenedEvents, true);
         parseChangesAndSetBalances(response);
       }
 
       return { response, error };
     },
-    [pushEventFromClient, parseChangesAndSetBalances, aptos, network]
+    [pushEventsFromClient, parseChangesAndSetBalances, aptos, network]
   );
 
-  const linearSubmit = useCallback(
-    async (builder: EntryFunctionTransactionBuilder) => {
-      if (geoblocked) return null;
-      if (checkNetworkAndToast(network, true)) {
-        setStatus("prompt");
-        const input = builder.payloadBuilder.toInputPayload();
-        const { functionName, res } = await adapterSignAndSubmitTxn(input).then((res) => ({
-          functionName: builder.payloadBuilder.functionName as EntryFunctionNames,
-          res,
-        }));
+  const submit = useCallback(async () => {
+    if (geoblocked || !transactionBuilder) return null;
+    if (checkNetworkAndToast(network, true)) {
+      setStatus("prompt");
+      const input = transactionBuilder.payloadBuilder.toInputPayload();
+      const { functionName, res } = await adapterSignAndSubmitTxn(input).then((res) => ({
+        functionName: transactionBuilder.payloadBuilder.functionName as EntryFunctionNames,
+        res,
+      }));
 
-        return await handleTransactionSubmission({ functionName, res });
-      }
-      return null;
-    },
-    [network, handleTransactionSubmission, adapterSignAndSubmitTxn, geoblocked]
-  );
-
-  const submit = useCallback(
-    async (builderFn: () => Promise<EntryFunctionTransactionBuilder>) => {
-      if (geoblocked) return null;
-      if (checkNetworkAndToast(network, true)) {
-        const builder = await builderFn();
-        setStatus("prompt");
-        const input = builder.payloadBuilder.toInputPayload();
-        const { functionName, res } = await adapterSignAndSubmitTxn(input).then((res) => ({
-          functionName: builder.payloadBuilder.functionName as EntryFunctionNames,
-          res,
-        }));
-
-        return await handleTransactionSubmission({ functionName, res });
-      }
-      return null;
-    },
-    [network, handleTransactionSubmission, adapterSignAndSubmitTxn, geoblocked]
-  );
+      return await handleTransactionSubmission({ functionName, res });
+    }
+    return null;
+  }, [
+    network,
+    transactionBuilder,
+    handleTransactionSubmission,
+    adapterSignAndSubmitTxn,
+    geoblocked,
+  ]);
 
   // To manually enforce explicit gas options, we can use this transaction submission flow.
   // Note that you need to pass the options to the builder, not here. It's possible to do it here, but it's
   // unnecessary to support that and I'm not gonna write the code for it.
-  const signThenSubmit = useCallback(
-    async (builderFn: () => Promise<EntryFunctionTransactionBuilder>) => {
-      if (geoblocked) return null;
-      if (checkNetworkAndToast(network, true)) {
-        const builder = await builderFn();
-        setStatus("prompt");
-        const senderAuthenticator = await signTransaction(builder.rawTransactionInput);
-        const { functionName, res } = await submitTransaction({
-          transaction: builder.rawTransactionInput,
-          senderAuthenticator,
-        }).then((res) => ({
-          functionName: builder.payloadBuilder.functionName as EntryFunctionNames,
-          res,
-        }));
+  const signThenSubmit = useCallback(async () => {
+    if (geoblocked || !transactionBuilder) return null;
+    if (checkNetworkAndToast(network, true)) {
+      setStatus("prompt");
+      const senderAuthenticator = await signTransaction(transactionBuilder.rawTransactionInput);
+      const { functionName, res } = await submitTransaction({
+        transaction: transactionBuilder.rawTransactionInput,
+        senderAuthenticator,
+      }).then((res) => ({
+        functionName: transactionBuilder.payloadBuilder.functionName as EntryFunctionNames,
+        res,
+      }));
 
-        return await handleTransactionSubmission({ functionName, res });
-      }
-      return null;
-    },
-    [network, handleTransactionSubmission, signTransaction, submitTransaction, geoblocked]
-  );
-
-  const setCoinTypeHelper = (
-    set: Dispatch<SetStateAction<string | undefined>>,
-    type?: TypeTagInput
-  ) => {
-    if (!type) set(undefined);
-    else if (typeof type === "string") {
-      set(type);
-    } else if (type instanceof TypeTag) {
-      set(type.toString());
-    } else {
-      throw new Error(`Invalid type: ${type}`);
+      return await handleTransactionSubmission({ functionName, res });
     }
-  };
+    return null;
+  }, [
+    network,
+    transactionBuilder,
+    handleTransactionSubmission,
+    signTransaction,
+    submitTransaction,
+    geoblocked,
+  ]);
 
   const value: AptosContextState = {
     aptos,
     account,
     submit,
-    handleTransactionSubmission,
-    linearSubmit,
     signThenSubmit,
+    transactionBuilder,
+    setTransactionBuilder,
     copyAddress,
     status,
     lastResponse,
     lastResponseStoredAt,
-    aptBalance,
-    emojicoinBalance,
-    emojicoinLPBalance,
+    aptBalance: aptHelper.balance,
+    emojicoinBalance: emojicoinHelper.balance,
+    emojicoinLPBalance: emojicoinLPHelper.balance,
     addressName,
     setEmojicoinType: (type?: TypeTagInput) => setCoinTypeHelper(setEmojicoinType, type),
     setBalance: (coinType: TrackedCoinType, n: bigint) => {
-      if (coinType === "apt") setAptBalance(n);
-      else if (coinType === "emojicoin") setEmojicoinBalance(n);
-      else if (coinType === "emojicoinLP") setEmojicoinLPBalance(n);
+      if (coinType === "apt") aptHelper.setBalance(n);
+      else if (coinType === "emojicoin") emojicoinHelper.setBalance(n);
+      else if (coinType === "emojicoinLP") emojicoinLPHelper.setBalance(n);
       else throw new Error(`Invalid coin type: ${coinType}`);
     },
     isFetching: (coinType: TrackedCoinType) => {
-      if (coinType === "apt") return isFetchingAptBalance;
-      else if (coinType === "emojicoin") return isFetchingEmojicoinBalance;
-      else if (coinType === "emojicoinLP") return isFetchingEmojicoinLPBalance;
+      if (coinType === "apt") return aptHelper.isFetching;
+      else if (coinType === "emojicoin") return emojicoinHelper.isFetching;
+      else if (coinType === "emojicoinLP") return emojicoinLPHelper.isFetching;
       else throw new Error(`Invalid coin type: ${coinType}`);
     },
     forceRefetch: (coinType: TrackedCoinType) => {
-      if (coinType === "apt") forceRefetchAptBalance();
-      else if (coinType === "emojicoin") forceRefetchEmojicoinBalance();
-      else if (coinType === "emojicoinLP") forceRefetchEmojicoinLPBalance();
+      if (coinType === "apt") aptHelper.forceRefetch();
+      else if (coinType === "emojicoin") emojicoinHelper.forceRefetch();
+      else if (coinType === "emojicoinLP") emojicoinLPHelper.forceRefetch();
       else throw new Error(`Invalid coin type: ${coinType}`);
     },
     refetchIfStale: (coinType: TrackedCoinType) => {
-      if (coinType === "apt") refetchAptBalanceIfStale();
-      else if (coinType === "emojicoin") refetchEmojicoinBalanceIfStale();
-      else if (coinType === "emojicoinLP") refetchEmojicoinLPBalanceIfStale();
+      if (coinType === "apt") aptHelper.refetchIfStale();
+      else if (coinType === "emojicoin") emojicoinHelper.refetchIfStale();
+      else if (coinType === "emojicoinLP") emojicoinLPHelper.refetchIfStale();
       else throw new Error(`Invalid coin type: ${coinType}`);
     },
   };
