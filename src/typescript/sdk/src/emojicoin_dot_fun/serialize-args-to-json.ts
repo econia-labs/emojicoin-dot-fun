0316eae4c446b97babb950fb5d4cfcb909556dcc
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-03 13:11:42 -0800

    Fix the issue with FixedBytes and types, simplify things

--- src/typescript/sdk/src/emojicoin_dot_fun/serialize-args-to-json.ts
+++ src/typescript/sdk/src/emojicoin_dot_fun/serialize-args-to-json.ts
@@ -1,43 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
-  Bool,
-  U8,
-  U16,
-  U32,
-  U64,
-  U128,
-  U256,
-  MoveString,
   AccountAddress,
   MoveOption,
-  type EntryFunctionArgumentTypes,
   MoveVector,
+  FixedBytes,
+  type EntryFunctionArgumentTypes,
 } from "@aptos-labs/ts-sdk";
-import { normalizeHex } from "../utils";
 
 export type Primitive = boolean | number | string | bigint | undefined | null;
 export type AnyPrimitive = Primitive | Array<AnyPrimitive> | Uint8Array;
 
-type EntryFunctionOptionArg = MoveOption<
-  Bool | U8 | U16 | U32 | U64 | U128 | U256 | MoveString | AccountAddress
->;
-
-const isSingleValue = (
-  v: any
-): v is Bool | U8 | U16 | U32 | U64 | U128 | U256 | MoveString | AccountAddress =>
-  v instanceof Bool ||
-  v instanceof U8 ||
-  v instanceof U16 ||
-  v instanceof U32 ||
-  v instanceof U64 ||
-  v instanceof U128 ||
-  v instanceof U256 ||
-  v instanceof MoveString ||
-  v instanceof AccountAddress;
-
-const serializeToJSON = (
-  value: EntryFunctionArgumentTypes | EntryFunctionOptionArg
-): AnyPrimitive => {
+const serializeToJSON = (value: EntryFunctionArgumentTypes): AnyPrimitive => {
   if (value instanceof MoveVector) {
     return value.values.map((v) => serializeToJSON(v));
   }
@@ -48,20 +21,18 @@ const serializeToJSON = (
     }
     return [serializeToJSON(inner)];
   }
-  // A single serializable value.
-  if (isSingleValue(value)) {
-    if (value instanceof AccountAddress) {
-      return value.toString();
-    }
-    return typeof value.value === "bigint" ? value.value.toString() : value.value;
+  // This may not be properly handled, but it should generally never be used for JSON-serializable
+  // purposes anyway. Return the hex bytes of FixedBytes if it appears.
+  if (value instanceof FixedBytes) {
+    return value.bcsToHex().toString();
   }
-  // Fixed bytes. Treat them like a normal Move vector, although this may be incorrect, but they
-  // shouldn't be used for this purpose anyway.
-  return normalizeHex(value.value);
+  if (value instanceof AccountAddress) {
+    return value.toString();
+  }
+  return typeof value.value === "bigint" ? value.value.toString() : value.value;
 };
 
-export const serializeArgsToJSON = (
-  args: Record<string, EntryFunctionArgumentTypes | EntryFunctionOptionArg>
-) => Object.values(args).map((v) => serializeToJSON(v));
+export const serializeArgsToJSON = (args: Record<string, EntryFunctionArgumentTypes>) =>
+  Object.values(args).map((v) => serializeToJSON(v));
 
 export default serializeArgsToJSON;
