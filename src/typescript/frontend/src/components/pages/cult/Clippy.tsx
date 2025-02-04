892d45b1dba27b3327dc222e3d84bcec9c3e0b84
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-04 12:48:32 -0800

    [ECO-2760] `/cult` page: fix fonts, add examples, misc updates (#551)

--- src/typescript/frontend/src/components/pages/cult/Clippy.tsx
+++ src/typescript/frontend/src/components/pages/cult/Clippy.tsx
@@ -3,6 +3,19 @@ import clippy, { type Agent } from "clippyts";
 import "./clippy.css";
 
 const ANIMATIONS_LOOP = ["Greeting", "Processing", "IdleAtom", "Congratulate"];
+const ANIMATION_INTERVAL = 11000;
+
+const FOUR_SECONDS = 4 * 1000;
+/**
+ * Each "word" adds 200ms:
+ * @see {@link https://github.com/pi0/clippyjs/blob/d88943d529410114c9cea7f01e05de40254cd914/lib/balloon.js#L9/**}
+ */
+const WORD_SPEAK_TIME = 200;
+/**
+ * Add a flat amount of time at the end by adding spaces, each space counts as a "word" due to the regex used:
+ * @see {@link https://github.com/pi-1/clippyjs/blob/d88943d529410114c9cea7f01e05de40254cd914/lib/balloon.js#L140}
+ */
+const FILLER_TO_LENGTHEN_DURATION = " ".repeat(FOUR_SECONDS / WORD_SPEAK_TIME);
 
 export const Clippy = ({ monologue }: { monologue: string[] }) => {
   const clippyRef = useRef<Agent>();
@@ -11,9 +24,11 @@ export const Clippy = ({ monologue }: { monologue: string[] }) => {
   const renderNextText = useCallback(() => {
     monologueIndex.current = monologueIndex.current + 1;
     if (monologueIndex.current === monologue.length) monologueIndex.current = 0;
-    //Play random animation from array
+    // Play random animation from array.
     clippyRef.current?.play(ANIMATIONS_LOOP[Math.floor(Math.random() * ANIMATIONS_LOOP.length)]);
-    clippyRef.current?.speak(monologue[monologueIndex.current], false);
+
+    const newMonologue = monologue[monologueIndex.current] + FILLER_TO_LENGTHEN_DURATION;
+    clippyRef.current?.speak(newMonologue, false);
   }, [monologue]);
 
   useEffect(() => {
@@ -28,7 +43,7 @@ export const Clippy = ({ monologue }: { monologue: string[] }) => {
           renderNextText();
           interval = setInterval(() => {
             renderNextText();
-          }, 6000);
+          }, ANIMATION_INTERVAL);
         }
       },
     });
@@ -36,7 +51,7 @@ export const Clippy = ({ monologue }: { monologue: string[] }) => {
     return () => {
       clearInterval(interval);
       clippyRef.current?.hide(true, () => null);
-      //No function to destroy it. We can destroy by classname
+      // No function to destroy it. We can destroy by class name.
       document.querySelectorAll(".clippy, .clippy-balloon").forEach((item) => item.remove());
       clippyRef.current = undefined;
     };
