f2fc400eb195619a00c9edcf5a6530eac314ad99
alnoki <43892045+alnoki@users.noreply.github.com>
2025-02-04 16:52:16 -0800

    [ECO-2768] Merge `main` to `production` up through #555 for cold upgrade revival (#558)
    Co-authored-by: Matt <90358481+xbtmatt@users.noreply.github.com>

--- src/typescript/frontend/src/app/cult/page.tsx
+++ src/typescript/frontend/src/app/cult/page.tsx
@@ -1,5 +1,7 @@
 import { type Metadata } from "next";
 import CultClientPage from "components/pages/cult/CultClientPage";
+import LaunchingPage from "app/launching/page";
+import { VERCEL_TARGET_ENV } from "@sdk/const";
 
 export const dynamic = "force-static";
 
@@ -8,6 +10,11 @@ export const metadata: Metadata = {
   description: `Explore the emojicoin cult`,
 };
 
-export default function LaunchEmojicoinPage() {
-  return <CultClientPage />;
+export default function CultPage() {
+  // Temporarily disable the page in non-development/non-preview modes and show the Launching Soon page instead.
+  return VERCEL_TARGET_ENV === "development" || VERCEL_TARGET_ENV === "preview" ? (
+    <CultClientPage />
+  ) : (
+    <LaunchingPage />
+  );
 }
