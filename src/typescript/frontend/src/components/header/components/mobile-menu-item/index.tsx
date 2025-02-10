185adf5de60c8cef34590c8a1799c6540aae0b33
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-09 20:31:32 -0800

    Fix class name concatenation

--- src/typescript/frontend/src/components/header/components/mobile-menu-item/index.tsx
+++ src/typescript/frontend/src/components/header/components/mobile-menu-item/index.tsx
@@ -8,6 +8,7 @@ import Arrow from "components/svg/icons/Arrow";
 
 import { type MobileMenuItemProps } from "./types";
 import { StyledItemWrapper } from "./styled";
+import { cn } from "lib/utils/class-name";
 
 const MobileMenuItem: React.FC<MobileMenuItemProps> = ({
   withIcon,
@@ -26,7 +27,7 @@ const MobileMenuItem: React.FC<MobileMenuItemProps> = ({
 
   return (
     <StyledItemWrapper onMouseOver={replay} onClick={onClick} borderBottom={borderBottom}>
-      <div className={`${withIcon?.className} ${pill?.className}`}>
+      <div className={cn(withIcon?.className, pill?.className)}>
         {withIcon?.icon}
         <Text
           textScale={withIcon ? "pixelHeading4" : "pixelHeading3"}
