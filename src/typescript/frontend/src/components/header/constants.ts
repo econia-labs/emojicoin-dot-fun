ab25293665f6d56c98035c33a05953fe5ed9f0f0
Bogdan Crisan <bogdan@crbl.studio>
2025-02-10 05:41:49 +0100

    [ECO-2588] Add market page banner and navigation link to arena (#512)
    Co-authored-by: Matt <90358481+xbtmatt@users.noreply.github.com>

--- src/typescript/frontend/src/components/header/constants.ts
+++ src/typescript/frontend/src/components/header/constants.ts
@@ -1,6 +1,7 @@
 import { ROUTES } from "router/routes";
 
 export const NAVIGATE_LINKS = [
+  { title: "arena", path: ROUTES.arena },
   { title: "pools", path: ROUTES.pools },
   { title: "launch", path: ROUTES.launch },
   { title: "cult", path: ROUTES.cult },
