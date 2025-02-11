b05ceb995b7b8863c9af38a4bd4d9ac0ba79acc5
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-10 18:45:46 -0800

    [ECO-2793] Merge `main` to `production`; `/cult`, page, misc changes/fixes (#556 - #577) (#578)
    Co-authored-by: alnoki <43892045+alnoki@users.noreply.github.com>
    Co-authored-by: 0xForkh <104137388+0xForkh@users.noreply.github.com>

---
+++ src/typescript/frontend/src/components/pages/cult/WinDesktopItem.tsx
@@ -0,0 +1,60 @@
+// cspell:word textfile
+// cspell:word mycomputer
+// cspell:word recyclebin
+// cspell:word desktopapp
+
+import Text from "components/text";
+import Image from "next/image";
+import { type FC } from "react";
+
+export enum WinIcons {
+  TEXT_FILE = "images/textfile.svg",
+  MY_COMPUTER = "images/mycomputer.svg",
+  RECYCLE_BIN = "images/recyclebin.svg",
+  DESKTOP_APP = "images/desktopapp.svg",
+}
+
+interface Props {
+  icon: WinIcons;
+  label: string;
+  onClick: () => void;
+}
+
+export const WinDesktopItem: FC<Props> = ({ icon, label, onClick }) => {
+  return (
+    <div
+      className="flex flex-col gap-3 cursor-pointer items-center w-[150px]"
+      onClick={() => onClick()}
+    >
+      <Image unoptimized width={64} height={64} src={icon} alt="desktop icon" />
+      <div className="w-[200px]">
+        <Text className="px-1" textAlign="center" textScale={"pixelBodySmall"} fontSize={"30px"}>
+          {label}
+        </Text>
+      </div>
+    </div>
+  );
+};
+
+export const CommunityCreationWinDesktopItem: FC<Props & { pos: { x: number; y: number } }> = ({
+  pos,
+  ...rest
+}) => {
+  return (
+    <>
+      {/* useMatchBreakpoints seems to be unreliable. Need to duplicate this component and rely on tailwind for the breakpoints */}
+      <div className={"mobile-sm:block mobile-lg:hidden"}>
+        <WinDesktopItem {...rest} />
+      </div>
+      <div
+        className={"mobile-sm:hidden mobile-lg:block absolute"}
+        style={{
+          left: pos.x,
+          top: pos.y,
+        }}
+      >
+        <WinDesktopItem {...rest} />
+      </div>
+    </>
+  );
+};
