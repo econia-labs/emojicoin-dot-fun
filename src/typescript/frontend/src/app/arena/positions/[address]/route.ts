26119e8a9e5c0186239dd093353daf3030d6c6e3
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-11 20:59:24 -0800

    Fix arena leaderboard history with arena info rpc function, use new view

--- src/typescript/frontend/src/app/arena/positions/[address]/route.ts
+++ src/typescript/frontend/src/app/arena/positions/[address]/route.ts
@@ -1,27 +1,19 @@
-// cspell:word timespan
-
 import { fetchArenaLeaderboardHistoryWithArenaInfo } from "@/queries/arena";
 import { AccountAddress } from "@aptos-labs/ts-sdk";
+import { safeParsePageWithDefault } from "lib/routes/home-page-params";
 import { cookies } from "next/headers";
 import { type NextRequest } from "next/server";
 import { stringifyJSON } from "utils";
 
-/* eslint-disable-next-line import/no-unused-modules */
+const ROWS_RETURNED = 25;
+
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ address: string }> }
 ) {
   cookies();
   const address = (await params).address;
-  let skip: number;
-  try {
-    skip = Number(request.nextUrl.searchParams.get("skip") ?? "0");
-  } catch {
-    return new Response("Invalid skip.", { status: 400 });
-  }
-  if (Number.isNaN(skip) || skip < 0 || skip != Math.round(skip)) {
-    return new Response("Invalid skip.", { status: 400 });
-  }
+  const page = safeParsePageWithDefault(request.nextUrl.searchParams.get("page"));
 
   if (!AccountAddress.isValid({ input: address, strict: true }).valid) {
     return new Response("Invalid address.", { status: 400 });
@@ -29,8 +21,8 @@ export async function GET(
 
   const position = await fetchArenaLeaderboardHistoryWithArenaInfo({
     user: address,
-    skip,
-    amount: 25,
+    page,
+    pageSize: ROWS_RETURNED,
   });
 
   return new Response(stringifyJSON(position ?? undefined));
