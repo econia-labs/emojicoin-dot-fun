394c4db9d527b4ef53fbf8505004bbb9edcec1d2
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-13 12:07:42 -0800

    [ECO-2811] Bump processor submodule; add/separate SDK test commands for arena; change skip to page; add arena routes to `ROUTES` (#585)

--- src/typescript/sdk/src/indexer-v2/types/json-types.ts
+++ src/typescript/sdk/src/indexer-v2/types/json-types.ts
@@ -328,7 +328,6 @@ type ArenaLeaderboardHistoryWithArenaInfoData = {
   emojicoin_1_balance: Uint64String;
   exited: boolean;
   last_exit_0: boolean | null;
-
   emojicoin_0_market_address: AccountAddressString;
   emojicoin_1_market_address: AccountAddressString;
   emojicoin_0_market_id: Uint64String;
@@ -414,6 +413,7 @@ export enum TableName {
   ArenaLeaderboard = "arena_leaderboard",
   // The table for a user's historic arena pnl.
   ArenaLeaderboardHistory = "arena_leaderboard_history",
+  // A user's leaderboard history combined with the arena info.
   ArenaLeaderboardHistoryWithArenaInfo = "arena_leaderboard_history_with_arena_info",
 }
 
@@ -513,7 +513,7 @@ export type DatabaseJsonType = {
 
   [TableName.ArenaLeaderboard]: ArenaLeaderboardData;
   [TableName.ArenaLeaderboardHistory]: ArenaLeaderboardHistoryData;
-  [TableName.ArenaLeaderboardHistoryWithArenaInfo]: ArenaLeaderboardHistoryWithArenaInfoData;
+  [TableName.ArenaLeaderboardHistoryWithArenaInfo]: Flatten<ArenaLeaderboardHistoryWithArenaInfoData>;
   [DatabaseRpc.UserPools]: Flatten<
     TransactionMetadata &
       MarketAndStateMetadata &
