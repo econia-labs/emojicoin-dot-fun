3fb11ed6a46b4463ceec7f602a1b9dc0d57a267f
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-15 21:54:40 -0800

    Add multiple series to the chart with proper styles

--- src/typescript/frontend/src/components/charts/types.ts
+++ src/typescript/frontend/src/components/charts/types.ts
@@ -1,7 +1,9 @@
 import { type SymbolEmojiData } from "@sdk/emoji_data";
+import { type ClassValue } from "clsx";
 
 export type ChartContainerProps = {
   symbol: string;
   emojis: SymbolEmojiData[];
   secondarySymbol?: string;
+  className?: ClassValue;
 };
