900f1ad22c37b83b875c61e6f12de7d5c094e5f0
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-19 15:57:43 -0800

    Remove duplicated `fetchSpecificMarkets` query

--- src/typescript/sdk/src/indexer-v2/queries/app/stats.ts
+++ src/typescript/sdk/src/indexer-v2/queries/app/stats.ts
@@ -2,46 +2,9 @@ if (process.env.NODE_ENV !== "test") {
   require("server-only");
 }
 
-import { type SymbolEmoji } from "../../../emoji_data";
 import { compareNumber } from "../../../utils";
-import { TableName, toMarketStateModel } from "../../types";
-import { postgrest } from "../client";
-
-/**
- * Based off this working postgrest syntax:
- * .eq('symbol_emojis.eq.{"👀","👀"}');
- *
- * @param emojis ["👀", "👀"]
- * @returns symbol_emojis.eq.{"👀","👀"}
- */
-const toEqClause = (emojis: SymbolEmoji[]) => `symbol_emojis.eq.{"${emojis.join('","')}"}`;
-
-/**
- * Map an array of symbol emojis representing the separated symbol emojis for a market symbol to the
- * proper `eq.(...)` clause in postgrest.
- *
- * @param symbols [["👀","👀"], ["💀"], ["🎲"]]
- * @returns symbol_emojis.eq.{"👀","👀"},symbol_emojis.eq.{"💀"},symbol_emojis.eq.{"🎲"}
- */
-const joinEqClauses = (symbols: SymbolEmoji[][]) => symbols.map(toEqClause).join(",");
 
 /* eslint-disable import/no-unused-modules */
-/**
- * NOTE: These markets are returned *unsorted*.
- *
- * @see {@link toEqClause}
- * @see {@link joinEqClauses}
- * @param symbols the market symbols to filter on
- * @returns the *unsorted* market state data for the market symbols passed in
- */
-export const fetchSpecificMarkets = async (symbols: SymbolEmoji[][]) =>
-  await postgrest
-    .from(TableName.MarketState)
-    .select("*")
-    .or(joinEqClauses(symbols))
-    .then((res) => res.data ?? [])
-    .then((res) => res.map(toMarketStateModel));
-
 /**
  * Given an exhaustive list of price deltas for all markets, sort them based on query params.
  */
