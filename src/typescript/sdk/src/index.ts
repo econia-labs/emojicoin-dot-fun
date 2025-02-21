22b51f94d4a3bbaf4f33fe7049369dc8b517cafc
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-21 14:35:41 -0800

    Remove duplicate `EventName` type and export indexer types by default in the SDK

--- src/typescript/sdk/src/index.ts
+++ src/typescript/sdk/src/index.ts
@@ -2,6 +2,7 @@ export * from "./const";
 export * from "./broker-v2";
 export * from "./emoji_data";
 export * from "./emojicoin_dot_fun";
+export * from "./indexer-v2/types";
 export * from "./markets";
 export * from "./types";
 export * from "./utils";
