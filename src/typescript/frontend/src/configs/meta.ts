0860c78ed02b4b1033a4ca2edda038e31e5b3317
Hasan <hasanshafiq@vinncorp.com>
2025-02-24 22:15:32 +0500

    Changes in meta and favico

--- src/typescript/frontend/src/configs/meta.ts
+++ src/typescript/frontend/src/configs/meta.ts
@@ -1,7 +1,7 @@
 import { type Metadata } from "next";
 import { emoji } from "utils";
 
-export const DEFAULT_TITLE = "emojicoin.fun";
+export const DEFAULT_TITLE = "Values.co";
 export const DEFAULT_DESCRIPTION = `Give your wallet the personality it deserves ${emoji("zany face")}${emoji("sparkles")}`;
 export const OG_IMAGES = "/social-preview.png";
 export const OG_TYPE = "website";
