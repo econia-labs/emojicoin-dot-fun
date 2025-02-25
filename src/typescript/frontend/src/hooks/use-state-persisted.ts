e1b7f18febe2cb223cf9b3fe8b2356a73bb0ea8f
0xForkHunter <0xforkhunter@protonmail.com>
2025-02-25 12:46:16 +0700

    improve doc for use-state-persisted

--- src/typescript/frontend/src/hooks/use-state-persisted.ts
+++ src/typescript/frontend/src/hooks/use-state-persisted.ts
@@ -1,7 +1,22 @@
 import { useEffect, useState } from "react";
 
-// hook "useLocalStorage" from package "react-use" doesn't seem to provide the correct previous value when you set a state by using a callback function.
-// This custom hook behaves exactly like useState from React, but it persists the state in the local/session storage.
+/**
+ * A custom React hook that behaves like useState but persists the state in localStorage/sessionStorage.
+ * Note: The "useLocalStorage" hook from "react-use" package doesn't provide the correct previous value
+ * when setting state using a callback function. This hook fixes that issue.
+ *
+ * Warning: This shouldn't be used to share values between components. If two separate components
+ * use the same key, updating one state will not update the other.
+ *
+ * @template T The type of the state value
+ * @param {string} key The key under which the value will be stored in storage
+ * @param {T} initialValue The initial value to use if no value exists in storage
+ * @param {"localStorage" | "sessionStorage"} [storageType="localStorage"] The type of storage to use
+ * @returns {[T, React.Dispatch<React.SetStateAction<T>>, () => void]} A tuple containing:
+ *   - The current state value
+ *   - A function to update the state
+ *   - A function to clear the stored value
+ */
 function useStatePersisted<T>(
   key: string,
   initialValue: T,
