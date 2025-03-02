94db52ed324f58669b6069a794de7e3843caa022
0xForkHunter <0xforkhunter@protonmail.com>
2025-03-02 10:36:23 +0700

    add params validation with zod. Replace fetch with Axios

--- src/typescript/frontend/src/components/pages/wallet/useSwapEventsQuery.ts
+++ src/typescript/frontend/src/components/pages/wallet/useSwapEventsQuery.ts
@@ -1,22 +1,28 @@
-import { type fetchSenderSwapEvents } from "@/queries/market";
+import { type fetchSwapEvents } from "@/queries/market";
 import { type AnyNumberString } from "@sdk-types";
 import { LIMIT } from "@sdk/indexer-v2/const";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { parseJSON } from "utils";
+import { axiosInstance } from "utils/axios";
 
-export type SwapEvent = Awaited<ReturnType<typeof fetchSenderSwapEvents>>[number];
+export type SwapEvent = Awaited<ReturnType<typeof fetchSwapEvents>>[number];
 
-export const useSwapEventsQuery = (address?: string, marketId?: AnyNumberString) => {
-  const query = useInfiniteQuery<SwapEvent[]>({
-    queryKey: ["fetchSwapEvents", address, marketId],
+export const useSwapEventsQuery = ({
+  sender,
+  marketId,
+}: {
+  sender?: string;
+  marketId?: AnyNumberString;
+}) => {
+  const query = useInfiniteQuery({
+    queryKey: ["fetchSwapEvents", sender, marketId],
     queryFn: ({ pageParam }) =>
-      fetch(`/wallet/${address}/swaps?page=${pageParam}${marketId ? `&market_id=${marketId}` : ""}`)
-        .then(async (res) => res.text())
-        .then((res) => parseJSON(res)),
+      axiosInstance
+        .get<SwapEvent[]>("/api/trades", { params: { marketId, sender, page: pageParam } })
+        .then((res) => res.data),
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) =>
       lastPage?.length === LIMIT ? allPages.length + 1 : undefined,
-    enabled: !!address,
+    enabled: !!sender,
   });
 
   return query;
