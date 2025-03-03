7f4976909ad39b3c18205f941bde17d92ff28ed6
0xForkHunter <0xforkhunter@protonmail.com>
2025-03-03 22:02:28 +0700

    fix build errors

--- src/typescript/frontend/src/app/api/trades/route.ts
+++ src/typescript/frontend/src/app/api/trades/route.ts
@@ -1,23 +1,6 @@
 import { fetchSwapEvents } from "@/queries/market";
-import { AccountAddress } from "@aptos-labs/ts-sdk";
-import { toOrderBy } from "@sdk/indexer-v2/const";
 import { stringifyJSON } from "utils";
-import { z } from "zod";
-
-export const GetTradesSchema = z.object({
-  sender: z
-    .string()
-    .refine((arg) => AccountAddress.isValid({ input: arg }))
-    .transform((val) => AccountAddress.from(val))
-    .optional(),
-  page: z.coerce.number().default(1),
-  limit: z.coerce.number().max(100).optional().default(100),
-  marketId: z.string().optional(),
-  orderBy: z
-    .enum(["asc", "desc"])
-    .optional()
-    .transform((o) => (o ? toOrderBy(o) : undefined)),
-});
+import { GetTradesSchema } from "./schema";
 
 export const GET = async (req: Request) => {
   const { searchParams } = new URL(req.url);
