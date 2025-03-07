15229a38574e241485a0d63a7d13ed749f6949b3
Greg Nazario <greg@gnazar.io>
2025-03-07 11:47:49 -0500

    [markets] add estimated volume in USD based on current APT price
    This should make it easier for users to value what their emojis are
    worth, ignoring that the value can change over time / isn't the exact
    value.

--- src/typescript/frontend/src/hooks/use-usd-market-cap.ts
+++ src/typescript/frontend/src/hooks/use-usd-market-cap.ts
@@ -17,3 +17,20 @@ export function useUsdMarketCap(marketCapInOctas: bigint): number | undefined {
 
   return aptInUsd;
 }
+
+/**
+ * Multiplies the value times the APT price to get the value in USD.
+ *
+ * If the APT price isn't available, it will return `undefined`.
+ */
+export function useUSDValue(marketCapInOctas: bigint): number | undefined {
+  const aptPrice = useAptPrice();
+  const aptInUsd = aptPrice ? toNominal(marketCapInOctas) * aptPrice : undefined;
+
+  // Remove decimals if market cap is over 1 million
+  if (aptInUsd && aptInUsd >= 1_000_000) {
+    return Math.floor(aptInUsd);
+  }
+
+  return aptInUsd;
+}
