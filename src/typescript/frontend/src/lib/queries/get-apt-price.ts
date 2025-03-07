0a42a4fa60211c2a72a9a62f0a298f0749cff955
0xForkHunter <0xforkhunter@protonmail.com>
2025-03-07 21:43:16 +0700

    rename marketId -> marketID and replace axios

--- src/typescript/frontend/src/lib/queries/get-apt-price.ts
+++ src/typescript/frontend/src/lib/queries/get-apt-price.ts
@@ -1,29 +1,26 @@
 "use server";
 
+import { ecFetch } from "lib/ecFetch/ecFetch";
 import { COINGECKO_API_KEY } from "lib/server-env";
 import { unstable_cache } from "next/cache";
 
 const COINGECKO_APT_ID = "aptos";
 const COINGECKO_ROOT_URL = "https://pro-api.coingecko.com/api";
 const COINGECKO_ENDPOINT = "v3/simple/price";
-const COINGECKO_PARAMS = "vs_currencies=usd&precision=4";
 
 export const getAptPrice = unstable_cache(
   async () =>
-    fetch(
-      `${COINGECKO_ROOT_URL}/${COINGECKO_ENDPOINT}` +
-        `?ids=${COINGECKO_APT_ID}&${COINGECKO_PARAMS}`,
-      {
-        headers: {
-          Accept: "application/json",
-          "x-cg-pro-api-key": COINGECKO_API_KEY,
-        },
-      }
-    )
-      .then(async (r) => {
-        if (r.ok) return r.json();
-        throw new Error(r.statusText + " " + JSON.stringify(await r.json()));
-      })
+    ecFetch(`${COINGECKO_ROOT_URL}/${COINGECKO_ENDPOINT}`, {
+      searchParams: {
+        ids: COINGECKO_APT_ID,
+        vs_currencies: "usd",
+        precision: 4,
+      },
+      headers: {
+        Accept: "application/json",
+        "x-cg-pro-api-key": COINGECKO_API_KEY,
+      },
+    })
       .then((r) => r.aptos.usd as number)
       .catch((e) => {
         console.error("Could not get APT price from CoinGecko.", e);
