398ae06d0df14853b33e64c31e507bbe66a00a7f
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-08 14:46:44 -0800

    Add check for compose file

--- .github/actions/indexer-versions/sh/check-compose-file.sh
+++ .github/actions/indexer-versions/sh/check-compose-file.sh
@@ -1,2 +1,5 @@
-echo $BROKER_VERSION
-echo $PROCESSOR_VERSION
+COMPOSE_FILE="src/docker/comose.yaml"
+COMPOSE_BROKER_VERSION=$(yq '.services.broker.image' "$COMPOSE_FILE")
+COMPOSE_PROCESSOR_VERSION=$(yq '.services.processor.image' "$COMPOSE_FILE")
+echo $COMPOSE_BROKER_VERSION
+echo $COMPOSE_PROCESSOR_VERSION
