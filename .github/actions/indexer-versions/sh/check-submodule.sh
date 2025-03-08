77921f09998d8f46446f84b82451e9978f7cea3d
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-08 15:12:07 -0800

    Add tag checker

--- .github/actions/indexer-versions/sh/check-submodule.sh
+++ .github/actions/indexer-versions/sh/check-submodule.sh
@@ -6,3 +6,11 @@ SUBMODULE_DIR="src/rust/processor"
 cd $SUBMODULE_DIR
 SUBMODULE_COMMIT=$(cd "$PROCESSOR_DIR" && git rev-parse HEAD)
 echo "Current submodule commit: $SUBMODULE_COMMIT"
+
+# Check the commit expected based on the tag.
+EXPECTED_TAG="emojicoin-process0r-v${PROCESSOR_VERSION}"
+TAG_COMMIT=$(git rev-list -n 1 $EXPECTED_TAG)
+if [ -z "$TAG_COMMIT" ]; then
+	echo "::error::Tag $EXPECTED_TAG does not exist in processor repository"
+	exit 1
+fi
