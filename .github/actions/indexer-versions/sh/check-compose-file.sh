ae805fb23f41cecd2a4487f3ef03495f4615debe
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-08 14:47:22 -0800

    Add exit on error

--- .github/actions/indexer-versions/sh/check-compose-file.sh
+++ .github/actions/indexer-versions/sh/check-compose-file.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+set -e
 COMPOSE_FILE="src/docker/comose.yaml"
 COMPOSE_BROKER_VERSION=$(yq '.services.broker.image' "$COMPOSE_FILE")
 COMPOSE_PROCESSOR_VERSION=$(yq '.services.processor.image' "$COMPOSE_FILE")
