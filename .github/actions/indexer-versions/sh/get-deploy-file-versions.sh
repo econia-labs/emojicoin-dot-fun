46be519b0e8c5bd1a66a46b03480798383751221
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-08 13:45:56 -0800

    Add deploy file checker

--- .github/actions/indexer-versions/sh/get-deploy-file-versions.sh
+++ .github/actions/indexer-versions/sh/get-deploy-file-versions.sh
@@ -1,23 +1,69 @@
 #!/bin/sh
 set -e
 
-# Ensure file exists.
-DEPLOY_FILE="src/cloud-formation/deploy-indexer-${DEPLOY_FILE_SUFFIX}.yaml"
-echo "Loading values from: ${DEPLOY_FILE}"
-if [ ! -f "${DEPLOY_FILE}" ]; then
-  echo "Error: File not found: ${DEPLOY_FILE}"
-  exit 1
+# Check if a file exists.
+check_file_exists() {
+	local file_path="$1"
+	if [ ! -f "$file_path" ]; then
+		echo "::error::File not found: $file_path"
+		exit 1
+	fi
+}
+
+# Check the `Environment` value for a deploy file.
+check_environment() {
+	local file_path="$1"
+	local expected_env="$2"
+	local actual_env=$(yq '.parameters.Environment' "$file_path")
+	if [ "$actual_env" != "$expected_env" ]; then
+		echo "::error::Environment in $file_path is '$actual_env'"
+		exit 1
+	fi
+}
+
+# Define file paths.
+DEPLOY_FILE_PREFIX="src/cloud-formation/deploy-indexer-"
+DEPLOY_FILE_FALLBACK="${DEPLOY_FILE_PREFIX}fallback.yaml"
+DEPLOY_FILE_PRODUCTION="${DEPLOY_FILE_PREFIX}production.yaml"
+
+# Check if files exist.
+check_file_exists "$DEPLOY_FILE_FALLBACK"
+check_file_exists "$DEPLOY_FILE_PRODUCTION"
+
+# Verify `Environment` values in fallback and production files.
+check_environment "$DEPLOY_FILE_PRODUCTION" "production"
+check_environment "$DEPLOY_FILE_FALLBACK" "fallback"
+
+# Create temporary files.
+TEMP_FALLBACK=$(mktemp)
+TEMP_PRODUCTION=$(mktemp)
+
+# Extract data without Environment field.
+yq 'del(.parameters.Environment)' "$DEPLOY_FILE_FALLBACK" >"$TEMP_FALLBACK"
+yq 'del(.parameters.Environment)' "$DEPLOY_FILE_PRODUCTION" >"$TEMP_PRODUCTION"
+
+# Compare the files.
+if ! diff "$TEMP_FALLBACK" "$TEMP_PRODUCTION" >/dev/null; then
+	# Clean up.
+	rm -f "$TEMP_FALLBACK" "$TEMP_PRODUCTION"
+	echo "::error::Fallback and production files mismatched"
+	exit 1
 fi
 
+# Clean up.
+rm -f "$TEMP_FALLBACK" "$TEMP_PRODUCTION"
+
 # Get binary versions.
-BROKER_VERSION=$(yq eval '.parameters.BrokerImageVersion' "${DEPLOY_FILE}")
+BROKER_VERSION=$(
+	yq eval '.parameters.BrokerImageVersion' "${DEPLOY_FILE_PRODUCTION}"
+)
 PROCESSOR_VERSION=$(
-    yq eval '.parameters.ProcessorImageVersion' "${DEPLOY_FILE}"
+	yq eval '.parameters.ProcessorImageVersion' "${DEPLOY_FILE_PRODUCTION}"
 )
 
 # Set GitHub Actions output variables.
-echo "BROKER_VERSION=${BROKER_VERSION}" >> $GITHUB_OUTPUT
-echo "PROCESSOR_VERSION=${PROCESSOR_VERSION}" >> $GITHUB_OUTPUT
-echo "Successfully loaded versions from ${DEPLOY_FILE}:"
+echo "BROKER_VERSION=${BROKER_VERSION}" >>$GITHUB_OUTPUT
+echo "PROCESSOR_VERSION=${PROCESSOR_VERSION}" >>$GITHUB_OUTPUT
+echo "Successfully checked deploy files with versions:"
 echo "  Broker Version: ${BROKER_VERSION}"
 echo "  Processor Version: ${PROCESSOR_VERSION}"
