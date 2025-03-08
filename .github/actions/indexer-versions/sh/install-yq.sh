e198604a45fa32d240ee82735fadbddefc1e62c0
alnoki <43892045+alnoki@users.noreply.github.com>
2025-03-08 13:18:45 -0800

    See if yq is on the machine already

--- .github/actions/indexer-versions/sh/install-yq.sh
+++ .github/actions/indexer-versions/sh/install-yq.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 set -e
 
-# Install jq (required dependency for the Python yq).
-sudo apt-get update
-sudo apt-get install -y jq
-
-# Install yq via pip.
-pip install yq
-
 # Verify installation.
 yq --version
