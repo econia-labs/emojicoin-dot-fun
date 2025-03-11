e703d3f521e0279835e16b5c3be79e8eda0760db
0xForkHunter <0xforkhunter@protonmail.com>
2025-03-11 20:25:14 +0700

    Use next.revalidate for fetching apt price instead of unstable_cache

--- src/typescript/frontend/src/lib/queries/get-apt-price.ts
+++ src/typescript/frontend/src/lib/queries/get-apt-price.ts
@@ -2,29 +2,27 @@
 
 import { ecFetch } from "lib/ecFetch/ecFetch";
 import { COINGECKO_API_KEY } from "lib/server-env";
-import { unstable_cache } from "next/cache";
 
 const COINGECKO_APT_ID = "aptos";
 const COINGECKO_ROOT_URL = "https://pro-api.coingecko.com/api";
 const COINGECKO_ENDPOINT = "v3/simple/price";
 
-export const getAptPrice = unstable_cache(
-  async () =>
-    ecFetch(`${COINGECKO_ROOT_URL}/${COINGECKO_ENDPOINT}`, {
-      searchParams: {
-        ids: COINGECKO_APT_ID,
-        vs_currencies: "usd",
-        precision: 4,
-      },
-      headers: {
-        "x-cg-pro-api-key": COINGECKO_API_KEY,
-      },
-    })
-      .then((r) => r.aptos.usd as number)
-      .catch((e) => {
-        console.error("Could not get APT price from CoinGecko.", e);
-        return undefined;
-      }),
-  ["apt-price"],
-  { revalidate: 10 } // Ten seconds.
-);
+export const getAptPrice = async () =>
+  ecFetch(`${COINGECKO_ROOT_URL}/${COINGECKO_ENDPOINT}`, {
+    searchParams: {
+      ids: COINGECKO_APT_ID,
+      vs_currencies: "usd",
+      precision: 4,
+    },
+    headers: {
+      "x-cg-pro-api-key": COINGECKO_API_KEY,
+    },
+    next: {
+      revalidate: 10,
+    },
+  })
+    .then((r) => r.aptos.usd as number)
+    .catch((e) => {
+      console.error("Could not get APT price from CoinGecko.", e);
+      return undefined;
+    });
