a8209bea44f7b3a3c5edc1540999a47173de9c9c
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-03-10 17:37:30 -0700

    fix typos/imports

--- src/typescript/frontend/src/lib/local-development/DisplayDebugData.tsx
+++ src/typescript/frontend/src/lib/local-development/DisplayDebugData.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import InnerDisplayDebugData from "./StoreOnClient";
+import InnerDisplayDebugData from "./inner";
 
 export const DisplayDebugData = () => {
   return <>{process.env.NODE_ENV === "development" && <InnerDisplayDebugData />}</>;
