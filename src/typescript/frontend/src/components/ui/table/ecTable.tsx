40332d64ddeab05340f31bcb7950ae477b3c722f
0xForkHunter <0xforkhunter@protonmail.com>
2025-03-12 20:20:14 +0700

    remove unused import

--- src/typescript/frontend/src/components/ui/table/ecTable.tsx
+++ src/typescript/frontend/src/components/ui/table/ecTable.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { EcTableHead } from "./ecTableHead";
 import { Table, TableHeader, TableRow } from "./table";
 import _ from "lodash";
