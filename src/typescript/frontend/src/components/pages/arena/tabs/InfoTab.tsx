67ec6a36782579290c9d88b77b0509b603e5091b
Bogdan Crisan <bogdan@crbl.studio>
2025-03-14 20:09:36 +0100

    docs(ECO-3017): Add arena documentation in the arena info tab

--- src/typescript/frontend/src/components/pages/arena/tabs/InfoTab.tsx
+++ src/typescript/frontend/src/components/pages/arena/tabs/InfoTab.tsx
@@ -1,32 +1,89 @@
+import { LINKS } from "lib/env";
+
 type Info = {
   title: string;
   paragraph: string;
 };
 const INFO: Info[] = [
   {
-    title: "How does this work?",
+    title: "What is an emojicoin.fun arena?",
     paragraph:
-      'Help text for error messages should be human-centered and guide the user to a solution. For example, if a user forgets to enter their email address, the error message could say "Enter your email address".',
+      "An arena is a daily emojicoin.fun event where two random emojicoins " +
+      "are selected to participate in an epic battle. When selected, the two " +
+      "emojicoins will be displayed on the home page, and you will be able " +
+      "to enter the arena and swap between the two. Upon entering, your APT " +
+      "will be converted to the emojicoin of your choice. Once entered, you " +
+      "can only swap between the two emojicoins in the arena. You can exit " +
+      "at any time and withdraw your emojicoins.",
   },
   {
-    title: "How does this work?",
+    title: "How long does an arena last?",
     paragraph:
-      'Help text for error messages should be human-centered and guide the user to a solution. For example, if a user forgets to enter their email address, the error message could say "Enter your email address".',
+      "An arena lasts for 24 hours. When an arena ends, the next one starts " +
+      "with two new random emojicoins.",
   },
   {
-    title: "How does this work?",
+    title: "Can I request an emojicoin to be part of an arena?",
     paragraph:
-      'Help text for error messages should be human-centered and guide the user to a solution. For example, if a user forgets to enter their email address, the error message could say "Enter your email address".',
+      "No. Emojicoins are randomly selected on chain. Nobody is able to " +
+      "chose or alter the random selection of emojicoins.",
   },
   {
-    title: "How does this work?",
+    title: "What does locking in means?",
     paragraph:
-      'Help text for error messages should be human-centered and guide the user to a solution. For example, if a user forgets to enter their email address, the error message could say "Enter your email address".',
+      "Locking in is a reward mechanism. Upon entering, you can lock in. If " +
+      "you do so, you will be matched a certain amount of APT. The exact " +
+      "amount you will get matched will be shown to you during the entering " +
+      "process. The earlier you lock in, the more APT you will be matched. " +
+      "If there are no rewards remaining, you will not be matched. When " +
+      "locking in, you can only exit after the melee ends. If you really " +
+      "wish to exit before the melee ends, you can tap out early, by paying " +
+      "back the APT amount you get matched. For example, let's say you enter " +
+      "a melee with 1 APT, and you get matched 1 APT, and 2 APT gets you " +
+      "1000 of one of the two emojicoins. After 30 minutes, you managed to " +
+      "triple your emojicoins, and you now have 3000 of them. If you wish to " +
+      "withdraw them before the melee has ended, in order to sell them for " +
+      "example, you can do so, but you'll have to pay back the 1 APT you got " +
+      "matched at the start. This can be worth it, depending on your scenario.",
   },
   {
-    title: "How does this work?",
+    title: "What is the difference between trading inside the arena and outside the arena?",
     paragraph:
-      'Help text for error messages should be human-centered and guide the user to a solution. For example, if a user forgets to enter their email address, the error message could say "Enter your email address".',
+      "Arena puts two emojicoins in the spotlight, generating a lot of " +
+      "activity for both of them. Furthermore, you can swap from one " +
+      "emojicoin to the other with 50% less fees it would cost to do that " +
+      "outside of an arena (because you only do one swap, from emojicoin A " +
+      "to emojicoin B, instead of swapping from emojicoin A to APT and then " +
+      "from APT to emojicoin B).",
+  },
+  {
+    title: "What does the price chart represent?",
+    paragraph:
+      "The price chart shows the price of the emojicoin on the left side in " +
+      "terms of the emojicoin of the right side. For example, if the price is " +
+      "3, that means that one of the emojicoins on the right can buy you " +
+      "three of the emojicoins on the left. If the price goes up, this is " +
+      "good for the emojicoin on the right. If the price goes down, this is " +
+      "good for the emojicoin on the left. In other words, the is no green " +
+      "is good or red is bad, it depends on which side you're on. The whole " +
+      "point of the arena is for you to switch sides at the best moment, " +
+      "when the price trend starts to change.",
+  },
+  {
+    title:
+      "I participated in an arena but didn't exit before the end of the " +
+      "arena. How do I exit past arenas?",
+    paragraph:
+      "To exit a past arena, you can go to the Profile tab, and click the " +
+      "exit button next to the arena you wish to exit. If the exit button is " +
+      "not present, it means you already exited that arena.",
+  },
+  {
+    title: "I don't fully understand how the arena works, what should I do?",
+    paragraph:
+      "You can join our discord" + LINKS?.discord
+        ? ` (${LINKS!.discord}) `
+        : " " + "where the developpers other members of the community can help you.",
   },
 ];
 
