3c26b9da6fd85ad2d49f507c750a6971be125818
0xForkHunter <0xforkhunter@protonmail.com>
2025-03-17 11:06:53 +0700

    remove usage of ecFetch

--- src/typescript/frontend/src/components/pages/emojicoin/components/chat/useChatEventsQuery.tsx
+++ src/typescript/frontend/src/components/pages/emojicoin/components/chat/useChatEventsQuery.tsx
@@ -2,8 +2,9 @@ import { type fetchChatEvents } from "@sdk/indexer-v2";
 import { LIMIT } from "@sdk/indexer-v2/const";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { type GetChatsSchema } from "app/api/chats/schema";
-import { ecFetch } from "lib/ecFetch/ecFetch";
 import { ROUTES } from "router/routes";
+import { parseJSON } from "utils";
+import { addSearchParams } from "utils/url-utils";
 import { type z } from "zod";
 
 export type ChatEvent = Awaited<ReturnType<typeof fetchChatEvents>>[number];
@@ -12,9 +13,12 @@ export const useChatEventsQuery = (args: z.input<typeof GetChatsSchema>) => {
   const query = useInfiniteQuery({
     queryKey: ["fetchChatEvents", args],
     queryFn: ({ pageParam }) =>
-      ecFetch<ChatEvent[]>(ROUTES.api.chats, {
-        method: "GET",
-        searchParams: { ...args, page: pageParam },
+      fetch(addSearchParams(ROUTES.api.chats, { ...args, page: pageParam })).then(async (r) => {
+        if (!r.ok) {
+          const errorText = await r.text();
+          throw new Error(`HTTP error ${r.status}: ${errorText}`);
+        }
+        return parseJSON<ChatEvent[]>(await r.text());
       }),
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) =>
