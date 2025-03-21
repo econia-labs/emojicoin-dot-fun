9682ab69912e8c886f2fbc38ea97bec470721033
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-03-20 19:32:35 -0700

    fix(ECO-3056): Fix/clean up the BackgroundEmojis component (#712)

--- src/typescript/frontend/src/context/ContentWrapper.tsx
+++ src/typescript/frontend/src/context/ContentWrapper.tsx
@@ -1,6 +1,9 @@
 export const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col max-w-[100dvw] pl-0 pr-0 h-[100dvh] overflow-x-hidden">
+    <div
+      id="content-wrapper"
+      className="flex flex-col max-w-[100dvw] pl-0 pr-0 h-[100dvh] overflow-x-hidden"
+    >
       {/* eslint-disable-next-line react/no-unknown-property */}
       <style jsx>{`
         div::-webkit-scrollbar {
