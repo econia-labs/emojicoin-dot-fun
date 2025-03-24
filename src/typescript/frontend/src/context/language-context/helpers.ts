1db022fe0cd55ab5e0587bceb2dfec67df21935b
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-03-24 02:51:59 -0700

    Remove remaining unused files

--- src/typescript/frontend/src/context/language-context/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { EN } from "configs";
-import { readLocalStorageCache } from "configs/local-storage-keys";
-
-export const getLanguageCodeFromLocalStorage = () => {
-  if (typeof window === "undefined") {
-    return EN.locale;
-  }
-  return readLocalStorageCache<string>("language") ?? EN.locale;
-};
