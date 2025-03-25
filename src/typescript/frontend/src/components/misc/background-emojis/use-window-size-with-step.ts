5072612abb16cc6c777a08579200ffb2b73e1ff9
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-03-25 00:14:25 -0700

    Update src/typescript/frontend/src/components/misc/background-emojis/use-window-size-with-step.ts

--- src/typescript/frontend/src/components/misc/background-emojis/use-window-size-with-step.ts
+++ src/typescript/frontend/src/components/misc/background-emojis/use-window-size-with-step.ts
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import useWindowSize from "react-use/lib/useWindowSize";
 
-const STEP_SIZE = 100;
+const STEP_SIZE = 222;
 
 // Round the input value down to the nearest multiple of STEP_SIZE.
 const stepFn = (value: number) => Math.floor(value / STEP_SIZE) * STEP_SIZE;
