e6ce8045c5f0feaa9c7c0aa9a193d8479824a1ca
0xForkh <0xforkhunter@protonmail.com>
2025-03-28 10:01:47 +0700

    favorite market wip

---
+++ src/typescript/frontend/src/lib/hooks/transaction-builders/use-favorite-builder.ts
@@ -0,0 +1,22 @@
+import { useAptos } from "context/wallet-context/AptosContextProvider";
+import { useMemo } from "react";
+
+import { AddFavorite, RemoveFavorite } from "@/contract-apis";
+
+import { useTransactionBuilder } from "./use-transaction-builder";
+
+export const useFavoriteTransactionBuilder = (marketAddress: `0x${string}`, isAdd: boolean) => {
+  const { account } = useAptos();
+
+  const memoizedArgs = useMemo(() => {
+    if (!account) {
+      return null;
+    }
+    return {
+      user: account.address,
+      market: marketAddress,
+    };
+  }, [account, marketAddress]);
+
+  return useTransactionBuilder(memoizedArgs, isAdd ? AddFavorite : RemoveFavorite);
+};
