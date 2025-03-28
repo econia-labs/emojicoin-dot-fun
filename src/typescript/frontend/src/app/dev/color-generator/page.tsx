9fe1c6cd095fad33f1487517d4544fabe16b010d
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-03-27 18:47:40 -0700

    Update src/typescript/frontend/src/app/dev/color-generator/page.tsx

--- src/typescript/frontend/src/app/dev/color-generator/page.tsx
+++ src/typescript/frontend/src/app/dev/color-generator/page.tsx
@@ -1,8 +1,10 @@
 import { EmojiColorGenerator } from "lib/utils/emojiColors/emoji-color-generator";
 import { redirect } from "next/navigation";
 
+import { VERCEL_TARGET_ENV } from "@/sdk/const";
+
 export default function EmojiColorPage() {
-  if (process.env.NODE_ENV === "production") {
+  if (VERCEL_TARGET_ENV === "production" || VERCEL_TARGET_ENV === "release-preview") {
     redirect("/404");
   }
   return <EmojiColorGenerator />;
