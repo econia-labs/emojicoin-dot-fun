eb92a6c71bfd8422824974c75eb93d5da747f4ff
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-02 19:35:08 -0700

    refactor(ECO-3157): Revamp user profile and history tab

--- src/typescript/sdk/src/utils/arena/escrow.ts
+++ src/typescript/sdk/src/utils/arena/escrow.ts
@@ -1,5 +1,4 @@
 import {
-  type Aptos,
   type MoveResource,
   parseTypeTag,
   type TypeTagStruct,
@@ -7,14 +6,20 @@ import {
   type WriteSetChangeWriteResource,
 } from "@aptos-labs/ts-sdk";
 
+import {
+  type ArenaInfoModel,
+  type ArenaLeaderboardHistoryWithArenaInfoModel,
+  type ArenaPositionModel,
+  maxBigInt,
+  toArenaCoinTypes,
+  toCoinTypeString,
+} from "../..";
 import type { AccountAddressString } from "../../emojicoin_dot_fun/types";
 import { type AnyNumberString, isWriteSetChangeWriteResource } from "../../types";
 import type { ArenaJsonTypes } from "../../types/arena-json-types";
 import { toEscrowResource } from "../../types/arena-types";
 import { toAccountAddressString } from "../account-address";
-import { getAccountResourcesWithInfo } from "../account-resources";
-import { getAptosClient } from "../aptos-client";
-import { STRUCT_STRINGS } from "../type-tags";
+import { type CoinTypeString, STRUCT_STRINGS } from "../type-tags";
 
 const isEscrowStruct = ({ value }: TypeTagStruct) => {
   const { address, moduleName, name } = value;
@@ -24,11 +29,10 @@ const isEscrowStruct = ({ value }: TypeTagStruct) => {
 
 export type UserEscrow = {
   version: bigint;
-  user: AccountAddressString;
+  user: `0x${string}`;
   meleeID: bigint;
   emojicoin0: bigint;
   emojicoin1: bigint;
-  matchAmount: bigint;
   coinTypes: [TypeTagStruct, TypeTagStruct, TypeTagStruct, TypeTagStruct];
 };
 
@@ -54,13 +58,15 @@ export const parseResourceForEscrow = <T extends MoveResource>({
 }): UserEscrow | null => {
   const { type } = resource;
   if (isEscrowResource(resource)) {
-    const escrow = toEscrowResource(resource);
+    const { meleeID, emojicoin0, emojicoin1 } = toEscrowResource(resource);
     const typeTag = parseTypeTag(type);
     if (typeTag.isStruct() && isEscrowStruct(typeTag)) {
       const innerTags = typeTag.value.typeArgs;
       if (innerTags.every((v) => v.isStruct()) && innerTags.length === 4) {
         return {
-          ...escrow,
+          meleeID,
+          emojicoin0,
+          emojicoin1,
           user: address,
           version: BigInt(version),
           coinTypes: innerTags as [TypeTagStruct, TypeTagStruct, TypeTagStruct, TypeTagStruct],
@@ -85,19 +91,94 @@ export const findEscrowsInTxn = ({ version, changes }: UserTransactionResponse) 
     )
     .filter((v) => !!v);
 
-/**
- * Fetch all Escrow resources the user owns.
- */
-export const fetchUserArenaEscrows = async (
-  accountAddress: AccountAddressString,
-  aptosIn: Aptos = getAptosClient()
-) =>
-  await getAccountResourcesWithInfo({
-    aptosConfig: aptosIn?.config,
-    accountAddress,
-  }).then((resources) =>
-    resources
-      .map((resource) => ({ address: accountAddress, ...resource }))
-      .map(parseResourceForEscrow)
-      .filter((v) => !!v)
-  );
+type PositionAndInfo = ArenaPositionModel & ArenaInfoModel;
+type ArenaCoinTypeStructs = [TypeTagStruct, TypeTagStruct, TypeTagStruct, TypeTagStruct];
+
+// Convert each user's current and historical positions to `UserEscrow` types.
+export const positionToUserEscrow = (
+  pos: PositionAndInfo | ArenaLeaderboardHistoryWithArenaInfoModel
+): UserEscrow => {
+  if ("version" in pos && "open" in pos) {
+    return {
+      version: pos.version,
+      user: pos.user,
+      meleeID: pos.meleeID,
+      emojicoin0: pos.emojicoin0Balance,
+      emojicoin1: pos.emojicoin1Balance,
+      coinTypes: toArenaCoinTypes({
+        symbol0: pos.emojicoin0Symbols,
+        symbol1: pos.emojicoin1Symbols,
+      }).filter((t) => t.isStruct()) as ArenaCoinTypeStructs,
+    };
+  }
+
+  return {
+    version: maxBigInt(
+      pos.arenaInfoLastTransactionVersion,
+      pos.leaderboardHistoryLastTransactionVersion
+    ),
+    user: pos.user,
+    meleeID: pos.meleeID,
+    emojicoin0: pos.emojicoin0Balance,
+    emojicoin1: pos.emojicoin1Balance,
+    coinTypes: toArenaCoinTypes({
+      symbol0: pos.emojicoin0Symbols,
+      symbol1: pos.emojicoin1Symbols,
+    }).filter((t) => t.isStruct()) as ArenaCoinTypeStructs,
+  };
+};
+
+export type UserEscrowJson = {
+  version: string;
+  user: `0x${string}`;
+  meleeID: string;
+  emojicoin0: string;
+  emojicoin1: string;
+  coinTypes: [CoinTypeString, CoinTypeString, CoinTypeString, CoinTypeString];
+};
+
+export function toUserEscrowJson({
+  version,
+  user,
+  meleeID,
+  emojicoin0,
+  emojicoin1,
+  coinTypes,
+}: UserEscrow): UserEscrowJson {
+  return {
+    version: version.toString(),
+    user: user.toString() as `0x${string}`,
+    meleeID: meleeID.toString(),
+    emojicoin0: emojicoin0.toString(),
+    emojicoin1: emojicoin1.toString(),
+    coinTypes: coinTypes.map(toCoinTypeString) as [
+      CoinTypeString,
+      CoinTypeString,
+      CoinTypeString,
+      CoinTypeString,
+    ],
+  };
+}
+
+export function fromUserEscrowJson({
+  version,
+  user,
+  meleeID,
+  emojicoin0,
+  emojicoin1,
+  coinTypes,
+}: UserEscrowJson): UserEscrow {
+  return {
+    version: BigInt(version),
+    user,
+    meleeID: BigInt(meleeID),
+    emojicoin0: BigInt(emojicoin0),
+    emojicoin1: BigInt(emojicoin1),
+    coinTypes: coinTypes.map((s) => parseTypeTag(s)).filter((t) => t.isStruct()) as [
+      TypeTagStruct,
+      TypeTagStruct,
+      TypeTagStruct,
+      TypeTagStruct,
+    ],
+  };
+}
