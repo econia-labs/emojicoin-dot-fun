a5c5962b814e8bb666405221b1b2ab94f946128b
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-04 18:49:02 -0700

    Make `useCurrentPosition` simpler

--- src/typescript/frontend/src/lib/hooks/queries/arena/use-current-position.ts
+++ src/typescript/frontend/src/lib/hooks/queries/arena/use-current-position.ts
@@ -3,16 +3,23 @@ import { useEffect, useMemo } from "react";
 import { ROUTES } from "router/routes";
 import { parseJSON } from "utils";
 
+import { useAccountAddress } from "@/hooks/use-account-address";
 import type { DatabaseJsonType } from "@/sdk/index";
 import { maxBigInt, toArenaPositionModel } from "@/sdk/index";
 import { useUserTransactionStore } from "@/store/transaction/store";
 
-export const useCurrentPositionQuery = (accountAddress?: `0x${string}`) => {
+/**
+ * @returns the position, undefined, or null. undefined means it's fetching, null means there's no
+ * connected user.
+ */
+export const useCurrentPositionQuery = () => {
+  const accountAddress = useAccountAddress();
   const latestUserTxnResponse = useUserTransactionStore((s) => s.latestResponse);
 
   const {
     data: position,
     isFetching,
+    isLoading,
     refetch,
   } = useQuery({
     queryKey: ["fetch-current-position", accountAddress ?? ""],
@@ -43,5 +50,6 @@ export const useCurrentPositionQuery = (accountAddress?: `0x${string}`) => {
   return {
     position,
     isFetching,
+    isLoading,
   };
 };
