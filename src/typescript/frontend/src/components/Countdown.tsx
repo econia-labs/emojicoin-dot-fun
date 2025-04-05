94d3cb31d1c4b183ee4192b4de074708e8bc0963
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-05 10:47:54 -0700

    Clean up countdown component

--- src/typescript/frontend/src/components/Countdown.tsx
+++ src/typescript/frontend/src/components/Countdown.tsx
@@ -1,64 +1,37 @@
 import { useMemo, useState } from "react";
 import { useInterval } from "react-use";
-import darkTheme from "theme/dark";
 
-const CountdownNumber = ({ n }: { n: string }) => (
-  <div
-    style={{
-      border: `1px solid ${darkTheme.colors.darkGray}`,
-      borderRadius: "5px",
-      background: "black",
-      width: "1.7ch",
-      textAlign: "center",
-      margin: ".1em",
-      paddingLeft: ".05em",
-      color: darkTheme.colors.econiaBlue,
-    }}
-  >
-    {n}
-  </div>
-);
+const countdownStyles =
+  "text-ec-blue border border-solid border-dark-gray rounded-[5px] w-[1.7ch] text-center m-[0.1em] pl-[0.05em]";
 
 export const Countdown = ({ startTime, duration }: { startTime: Date; duration: bigint }) => {
   const getRemaining = () =>
     Number(duration) - (new Date().getTime() / 1000 - startTime.getTime() / 1000);
   const [remaining, setRemaining] = useState<number>(getRemaining());
+
   useInterval(() => {
     setRemaining(getRemaining());
   }, 1000);
 
-  const seconds = useMemo(
-    () =>
-      Math.max(Math.floor(remaining) % 60, 0)
-        .toString()
-        .padStart(2, "0"),
-    [remaining]
-  );
-  const minutes = useMemo(
-    () =>
-      Math.max(Math.floor(remaining / 60) % 60, 0)
-        .toString()
-        .padStart(2, "0"),
-    [remaining]
-  );
-  const hours = useMemo(
-    () =>
-      Math.max(Math.floor(remaining / 60 / 60), 0)
-        .toString()
-        .padStart(2, "0"),
-    [remaining]
-  );
+  const { hours, seconds, minutes } = useMemo(() => {
+    const digits = (n: number) => Math.max(n, 0).toString().padStart(2, "0");
+    return {
+      hours: digits(Math.floor(remaining / 60 / 60)),
+      minutes: digits(Math.floor(remaining / 60) % 60),
+      seconds: digits(Math.floor(remaining) % 60),
+    };
+  }, [remaining]);
 
   return (
     <div className="text-light-gray flex flex-row pixel-clock w-min">
-      <CountdownNumber n={hours.split("")[0]} />
-      <CountdownNumber n={hours.split("")[1]} />
-      <div className="my-auto w-[1ch] text-center">:</div>
-      <CountdownNumber n={minutes.split("")[0]} />
-      <CountdownNumber n={minutes.split("")[1]} />
-      <div className="my-auto w-[1ch] text-center">:</div>
-      <CountdownNumber n={seconds.split("")[0]} />
-      <CountdownNumber n={seconds.split("")[1]} />
+      <span className={countdownStyles}>{hours.split("")[0]} </span>
+      <span className={countdownStyles}>{hours.split("")[1]} </span>
+      <span className="my-auto w-[1ch] text-center">:</span>
+      <span className={countdownStyles}>{minutes.split("")[0]} </span>
+      <span className={countdownStyles}>{minutes.split("")[1]} </span>
+      <span className="my-auto w-[1ch] text-center">:</span>
+      <span className={countdownStyles}>{seconds.split("")[0]} </span>
+      <span className={countdownStyles}>{seconds.split("")[1]} </span>
     </div>
   );
 };
