b33244130c5e00fab7ee5f31feb12b255f063e3f
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-04 18:49:36 -0700

    Make `useHistoricalPositions` simpler and sort the history by the melee ID in desc order

--- src/typescript/frontend/src/lib/hooks/queries/arena/use-historical-positions.ts
+++ src/typescript/frontend/src/lib/hooks/queries/arena/use-historical-positions.ts
@@ -3,11 +3,13 @@ import { useEffect, useMemo } from "react";
 import { ROUTES } from "router/routes";
 import { parseJSON } from "utils";
 
+import { useAccountAddress } from "@/hooks/use-account-address";
 import type { DatabaseJsonType } from "@/sdk/index";
-import { maxBigInt, toArenaLeaderboardHistoryWithArenaInfo } from "@/sdk/index";
+import { compareBigInt, maxBigInt, toArenaLeaderboardHistoryWithArenaInfo } from "@/sdk/index";
 import { useUserTransactionStore } from "@/store/transaction/store";
 
-export const useHistoricalPositionsQuery = (accountAddress?: `0x${string}`) => {
+export const useHistoricalPositionsQuery = () => {
+  const accountAddress = useAccountAddress();
   const latestUserTxnResponse = useUserTransactionStore((s) => s.latestResponse);
 
   const { data, isFetching, refetch } = useQuery({
@@ -19,7 +21,16 @@ export const useHistoricalPositionsQuery = (accountAddress?: `0x${string}`) => {
       )
         .then((res) => res.text())
         .then(parseJSON<DatabaseJsonType["arena_leaderboard_history_with_arena_info"][]>)
-        .then((res) => res.map(toArenaLeaderboardHistoryWithArenaInfo));
+        .then((res) =>
+          res
+            .map(toArenaLeaderboardHistoryWithArenaInfo)
+            .sort((a, b) => compareBigInt(a.meleeID, b.meleeID))
+            .reverse()
+        )
+        .catch((e) => {
+          console.error(e);
+          return [];
+        });
       const versions = historicalPositions.flatMap((v) => [
         v.arenaInfoLastTransactionVersion,
         v.leaderboardHistoryLastTransactionVersion,
@@ -45,7 +56,7 @@ export const useHistoricalPositionsQuery = (accountAddress?: `0x${string}`) => {
   }, [shouldRefetch, refetch]);
 
   return {
-    historicalPositions: data?.historicalPositions,
+    history: data?.historicalPositions ?? [],
     isFetching,
   };
 };
