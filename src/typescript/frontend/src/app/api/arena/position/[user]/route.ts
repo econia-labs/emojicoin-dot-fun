9d47e6d9c23fd4c0e377cf26965a21c67219a6fd
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-07 01:31:31 -0700

    Update route.ts for user position

--- src/typescript/frontend/src/app/api/arena/position/[user]/route.ts
+++ src/typescript/frontend/src/app/api/arena/position/[user]/route.ts
@@ -1,15 +1,22 @@
 // cspell:word timespan
 
 import { AccountAddress } from "@aptos-labs/ts-sdk";
+import { fetchCachedMeleeData } from "app/home/fetch-melee-data";
+import FEATURE_FLAGS from "lib/feature-flags";
 import { waitForVersionCached } from "lib/queries/latest-emojicoin-version";
 import { type NextRequest, NextResponse } from "next/server";
 
-import { fetchLatestPositionJson } from "@/queries/arena";
+import { fetchPositionWithArenaInfo } from "@/sdk/indexer-v2/queries/api/user-position/query";
+import type { UserPositionResponse } from "@/sdk/indexer-v2/queries/api/user-position/types";
 import { PositiveBigIntSchema } from "@/sdk/utils/validation/bigint";
 
 export const fetchCache = "force-no-store";
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ user: string }> }) {
+  if (!FEATURE_FLAGS.Arena) {
+    return new NextResponse("Arena isn't enabled.", { status: 503 });
+  }
+
   const searchParams = Object.fromEntries(request.nextUrl.searchParams.entries());
 
   const { user } = await params;
@@ -21,10 +28,21 @@ export async function GET(request: NextRequest, { params }: { params: Promise<{ 
   }
 
   if (!!user && !AccountAddress.isValid({ input: user, strict: true }).valid) {
-    return new Response("Invalid address.", { status: 400 });
+    return new NextResponse("Invalid address.", { status: 400 });
   }
 
-  const position = await fetchLatestPositionJson({ user });
+  const { arenaInfo, market0, market1 } = await fetchCachedMeleeData();
 
-  return NextResponse.json(position);
+  if (!arenaInfo || !market0 || !market1) {
+    return new NextResponse("Couldn't fetch current melee data in user position route.", {
+      status: 424,
+    });
+  }
+
+  const positionWithArenaInfo = await fetchPositionWithArenaInfo({
+    user: user as `0x${string}`,
+    arenaInfo,
+  });
+
+  return NextResponse.json<UserPositionResponse>(positionWithArenaInfo);
 }
