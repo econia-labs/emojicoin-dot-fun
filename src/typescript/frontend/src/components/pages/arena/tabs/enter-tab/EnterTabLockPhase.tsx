972076a410a120262da2043a87f7b13c01d80616
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-10 12:51:07 -0700

    Add interval animation brightness/scale, colors recommended by Kirsten, and hover lock tooltip if already locked in

--- src/typescript/frontend/src/components/pages/arena/tabs/enter-tab/EnterTabLockPhase.tsx
+++ src/typescript/frontend/src/components/pages/arena/tabs/enter-tab/EnterTabLockPhase.tsx
@@ -1,12 +1,13 @@
-import Big from "big.js";
 import { useEventStore } from "context/event-store-context/hooks";
 import { useAptos } from "context/wallet-context/AptosContextProvider";
 import { useCurrentPosition } from "lib/hooks/positions/use-current-position";
 import { useEnterTransactionBuilder } from "lib/hooks/transaction-builders/use-enter-builder";
+import { Lock } from "lucide-react";
 import { useMemo, useState } from "react";
 
 import Button from "@/components/button";
 import ButtonWithConnectWalletFallback from "@/components/header/wallet-button/ConnectWalletButton";
+import Popup from "@/components/popup";
 import { Switcher } from "@/components/switcher";
 import { useCurrentMeleeInfo } from "@/hooks/use-current-melee-info";
 import useRewardsRemaining from "@/hooks/use-rewards-remaining";
@@ -14,6 +15,7 @@ import { getEvents, type MarketStateModel } from "@/sdk/index";
 
 import { useArenaPhaseStore } from "../../phase/store";
 import { FormattedNominalNumber } from "../utils";
+import { MatchAmount } from "./MatchAmount";
 
 export default function EnterTabLockPhase({
   market,
@@ -23,18 +25,23 @@ export default function EnterTabLockPhase({
   amount: bigint;
 }) {
   const { position } = useCurrentPosition();
-  const [innerLock, setInnerLock] = useState<boolean>(false);
   const { account, submit } = useAptos();
   const { market0, market1 } = useCurrentMeleeInfo();
   const setPhase = useArenaPhaseStore((s) => s.setPhase);
   const setError = useArenaPhaseStore((s) => s.setError);
   const rewardsRemaining = useRewardsRemaining();
   const arenaInfo = useEventStore((s) => s.arenaInfoFromServer);
+  // Lock in by default if there are rewards remaining.
+  const [innerLock, setInnerLock] = useState<boolean>(position?.lockedIn || !!rewardsRemaining);
 
-  const lockedIn = useMemo(
-    () => innerLock || position?.lockedIn === true,
-    [innerLock, position?.lockedIn]
-  );
+  const { mustLockIn, lockedIn } = useMemo(() => {
+    const mustLockIn = position?.lockedIn === true;
+    const lockedIn = innerLock || mustLockIn;
+    return {
+      mustLockIn,
+      lockedIn,
+    };
+  }, [innerLock, position?.lockedIn]);
 
   const transactionBuilder = useEnterTransactionBuilder(
     amount,
@@ -44,51 +51,33 @@ export default function EnterTabLockPhase({
     market.market.marketAddress
   );
 
-  const matchAmount = useMemo(() => {
-    try {
-      if (!arenaInfo) {
-        return 0n;
-      }
-      if (!rewardsRemaining) {
-        return 0n;
-      }
-
-      const duration = Number(arenaInfo.duration / 1000n);
-      const remainingTime = duration - (new Date().getTime() - arenaInfo.startTime.getTime());
-      if (remainingTime < 0) return 0n;
-      const durationPercentage = Big(remainingTime / duration).mul(100);
-
-      let matchAmount = BigInt(
-        Big(amount.toString())
-          .mul(durationPercentage)
-          .div(100)
-          .mul(arenaInfo.maxMatchPercentage.toString())
-          .div(100)
-          .round(0)
-          .toString()
-      );
-      const eligibleMatchAmount = arenaInfo.maxMatchAmount - (position?.matchAmount ?? 0n);
-      if (matchAmount > eligibleMatchAmount) {
-        matchAmount = eligibleMatchAmount;
-      }
-      if (matchAmount > rewardsRemaining) {
-        matchAmount = rewardsRemaining;
-      }
-      return matchAmount;
-    } catch (_) {
-      return 0n;
-    }
-  }, [rewardsRemaining, arenaInfo, position?.matchAmount, amount]);
-
   return (
     <div className="flex flex-col gap-[2em] pt-14 m-auto items-center w-[100%]">
       <div className="flex justify-between w-[300px]">
         <div className="font-forma text-2xl uppercase text-white text-center">Lock in</div>
         <div className="flex gap-[1em] items-center">
           <div className="uppercase text-light-gray text-xl">
-            {lockedIn ? "Enabled" : "Disabled"}
+            {mustLockIn ? (
+              <Popup
+                content={
+                  <span>
+                    Your existing position is already locked
+                    <br />
+                    in, so you must remain locked in.
+                  </span>
+                }
+              >
+                <Lock className="m-auto ml-[3px] text-ec-blue" size={16} />
+              </Popup>
+            ) : lockedIn ? (
+              <span className="text-green">Enabled</span>
+            ) : (
+              <span className="text-pink">Disabled</span>
+            )}
           </div>
-          <Switcher checked={lockedIn} onChange={(v) => setInnerLock(v.target.checked)} />
+          {!mustLockIn && (
+            <Switcher checked={lockedIn} onChange={(v) => setInnerLock(v.target.checked)} />
+          )}
         </div>
       </div>
       <div className="max-w-[350px] w-[100%]">
@@ -98,10 +87,13 @@ export default function EnterTabLockPhase({
         </div>
         <div className="flex uppercase justify-between text-2xl text-light-gray py-[0.8em] mx-[0.8em] border-dashed border-b-[1px] border-light-gray ">
           <div>Match amount</div>
-          <FormattedNominalNumber
-            className={lockedIn && matchAmount ? "text-green" : ""}
-            value={lockedIn ? matchAmount : 0n}
-            suffix=" APT"
+          <MatchAmount
+            lockedIn={lockedIn}
+            mustLockIn={mustLockIn}
+            rewardsRemaining={rewardsRemaining}
+            arenaInfo={arenaInfo}
+            position={position}
+            amount={amount}
           />
         </div>
         <div className="pt-[2em] grid place-items-center">
