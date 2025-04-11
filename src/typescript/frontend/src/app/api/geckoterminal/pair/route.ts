104d371cc6f8ba69310d6acb07f2fd57c6409169
Bogdan Crisan <bogdan@crbl.studio>
2025-04-11 12:59:00 +0200

    fix(ECO-3179): Use addresses instead of names in geckoterminal API

--- src/typescript/frontend/src/app/api/geckoterminal/pair/route.ts
+++ src/typescript/frontend/src/app/api/geckoterminal/pair/route.ts
@@ -6,5 +6,5 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 export async function GET(request: NextRequest): Promise<NextResponse<PairResponse>> {
-  return pair(request);
+  return pair(request, { geckoTerminal: true });
 }
