07e592ed3d15b7011f92ddb8e72996a6f4f3844c
0xForkh <0xforkhunter@protonmail.com>
2025-04-11 14:18:32 +0300

    show loading for transactions in Arena Position tab

--- src/typescript/frontend/src/components/pages/arena/tabs/ArenaLoading.tsx
+++ src/typescript/frontend/src/components/pages/arena/tabs/ArenaLoading.tsx
@@ -13,35 +13,33 @@ export const ArenaLoading = ({ text = "Loading" }: { text?: string }) => {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="backdrop-blur-sm inset-0 fixed bg-black bg-opacity-0 flex items-center justify-center z-50">
-      <div className="flex flex-col border-r-2 justify-center items-center w-[500px] h-[300px] bg-black border border-dark-gray border-solid">
-        <div className="w-[200px] flex flex-col items-center gap-4">
-          <div className="flex flex-row">
-            <EmojiAsImage
-              className="translate-x-4"
-              size="40px"
-              emojis={emoji("collision")}
-              set="apple"
-            />
-            {Array.from({ length: 3 }).map((_, i) => {
-              return (
-                <BondingCurveArrow
-                  key={`progress-bar-element-${i}`}
-                  color={i < position ? "econiaBlue" : "darkGray"}
-                />
-              );
-            })}
-            <EmojiAsImage
-              className="-translate-x-4"
-              size="40px"
-              emojis={emoji("rocket")}
-              set="apple"
-            />
-          </div>
-          <Text className="tracking-widest" textTransform="uppercase" textScale={"pixelHeading4"}>
-            {text}...
-          </Text>
+    <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
+      <div className="w-[200px] flex flex-col items-center gap-4">
+        <div className="flex flex-row">
+          <EmojiAsImage
+            className="translate-x-4"
+            size="40px"
+            emojis={emoji("collision")}
+            set="apple"
+          />
+          {Array.from({ length: 3 }).map((_, i) => {
+            return (
+              <BondingCurveArrow
+                key={`progress-bar-element-${i}`}
+                color={i < position ? "econiaBlue" : "darkGray"}
+              />
+            );
+          })}
+          <EmojiAsImage
+            className="-translate-x-4"
+            size="40px"
+            emojis={emoji("rocket")}
+            set="apple"
+          />
         </div>
+        <Text className="tracking-widest" textTransform="uppercase" textScale={"pixelHeading4"}>
+          {text}...
+        </Text>
       </div>
     </div>
   );
