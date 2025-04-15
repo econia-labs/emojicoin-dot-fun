c3b1d32fe123a30b86090212cda7ad31d811b55a
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-14 21:28:19 -0700

    Format

--- src/typescript/frontend/src/utils/picker-data/complete-picker-data.ts
+++ src/typescript/frontend/src/utils/picker-data/complete-picker-data.ts
@@ -4,7 +4,7 @@ import type { EmojiMartData } from "components/pages/emoji-picker/types";
  * Some of the emojis in the larger set of chat emojis are not included by default in the emoji-mart
  * data. If there's ever a desire to replace the data with the complete set of emoji data, this
  * function can be filled out instead of being a stub.
- * 
+ *
  * For example: "handshake: medium-light skin tone, medium-dark skin tone"
  */
 export const completePickerData = (data: EmojiMartData): EmojiMartData => data;
