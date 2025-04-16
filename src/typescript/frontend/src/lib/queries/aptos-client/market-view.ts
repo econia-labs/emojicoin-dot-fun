59338ff0ce537f755ab25daf95a2d31484883a95
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-04-15 20:39:42 -0700

    chore(ECO-3099): Make terminology for move modules consistent everywhere (#810)

--- src/typescript/frontend/src/lib/queries/aptos-client/market-view.ts
+++ src/typescript/frontend/src/lib/queries/aptos-client/market-view.ts
@@ -7,7 +7,7 @@ import { MarketView } from "@/move-modules/emojicoin-dot-fun";
 import { getAptosClient } from "@/sdk/utils/aptos-client";
 import { toMarketView } from "@/sdk-types";
 
-const fetchContractMarketView = async (marketAddress: `0x${string}`) => {
+const fetchOnChainMarketView = async (marketAddress: `0x${string}`) => {
   const aptos = getAptosClient();
   const res = await MarketView.view({
     aptos,
@@ -17,11 +17,11 @@ const fetchContractMarketView = async (marketAddress: `0x${string}`) => {
   return stringifyJSON(toMarketView(res));
 };
 
-const cachedContractMarketView = unstable_cache(fetchContractMarketView, ["fetch-market-view"], {
+const cachedOnChainMarketView = unstable_cache(fetchOnChainMarketView, ["fetch-market-view"], {
   revalidate: 10,
 });
 
-export const wrappedCachedContractMarketView = async (marketAddress: `0x${string}`) => {
-  const cached = await cachedContractMarketView(marketAddress);
+export const wrappedCachedOnChainMarketView = async (marketAddress: `0x${string}`) => {
+  const cached = await cachedOnChainMarketView(marketAddress);
   return parseJSON<ReturnType<typeof toMarketView>>(cached);
 };
