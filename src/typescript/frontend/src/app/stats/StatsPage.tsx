f48e1d8277000e87caecc61700131662047680d2
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-05-21 14:41:01 -0700

    Ensure the default visual sort matches the default sort on the backend

--- src/typescript/frontend/src/app/stats/StatsPage.tsx
+++ src/typescript/frontend/src/app/stats/StatsPage.tsx
@@ -82,6 +82,7 @@ export default function StatsPageComponent({
           items={data}
           serverSideOrderHandler={handleNavigation}
           isLoading={isLoading}
+          defaultSortColumn={columnSortStrings[sortBy]}
         />
       </div>
       <StatsButtonsBlock
