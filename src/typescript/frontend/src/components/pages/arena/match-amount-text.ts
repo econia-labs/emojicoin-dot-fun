62ad296404f2a81a61c69853e589d26d312a8520
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-05-29 17:33:43 -0700

    feat(ECO-3280): Add info popups (#857)

---
+++ src/typescript/frontend/src/components/pages/arena/match-amount-text.ts
@@ -0,0 +1,6 @@
+const MATCH_AMOUNT_TEXT = [
+  "Match amount is the amount of APT automatically added to the melee escrow's position size.",
+  "For example, if you enter with 10 APT and get matched 5 APT, 15 APT worth of emojicoins will be bought and deposited to your melee escrow.",
+];
+
+export default MATCH_AMOUNT_TEXT;
---
+++ src/typescript/frontend/src/components/pages/arena/tabs/enter-tab/summary/ArenaExitButton.tsx
@@ -0,0 +1,49 @@
+import { cn } from "lib/utils/class-name";
+import { useScramble } from "use-scramble";
+
+import StyledButton from "@/components/button/styled";
+
+import ArenaExitInfo from "./ArenaExitInfo";
+
+const textClassName = cn("text-ec-blue font-pixelar uppercase leading-6 text-2xl");
+
+export default function ArenaExitButton({
+  text = "Exit",
+  onClick,
+  summaryPage = false,
+}: {
+  text?: string;
+  onClick: () => void;
+  summaryPage?: boolean;
+}) {
+  const { ref, replay } = useScramble({
+    text,
+    overdrive: false,
+    speed: 0.5,
+  });
+
+  return (
+    <div className="relative flex gap-2 justify-between">
+      <div className="absolute -bottom-5 w-full flex items-center justify-center gap-1">
+        <span className="uppercase font-pixelar text-light-gray whitespace-nowrap text-sm">
+          {"Does this sell?"}
+        </span>
+        <ArenaExitInfo summaryPage={summaryPage} />
+      </div>
+      <StyledButton onClick={onClick} onMouseOver={replay} onFocus={replay}>
+        <span className={textClassName}> {"{ "} </span>
+        <div className="flex flex-row">
+          <span
+            className={textClassName}
+            ref={ref}
+            style={{
+              minWidth: `${text.length + 1}ch`,
+              textAlign: "center",
+            }}
+          />
+        </div>
+        <span className={textClassName}> {" }"} </span>
+      </StyledButton>
+    </div>
+  );
+}
