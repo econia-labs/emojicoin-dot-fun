ff6c2cb8333ca3052ac7d8d5cd8e9120ab01bc17
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-06-03 15:59:40 -0700

    Clarify the AIP-40 standardization in the account address schema function

--- src/typescript/sdk/src/utils/validation/account-address.ts
+++ src/typescript/sdk/src/utils/validation/account-address.ts
@@ -1,6 +1,9 @@
 import { AccountAddress } from "@aptos-labs/ts-sdk";
 import { z } from "zod";
 
+/**
+ * NOTE: This function will standardize the input string to adhere to the AIP-40 standard.
+ */
 export const AccountAddressSchema = z
   .string()
   .refine((arg) => arg && AccountAddress.isValid({ input: arg }).valid, {
