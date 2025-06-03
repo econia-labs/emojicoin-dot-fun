c5a6df69b6a9957098669f98378d77e7dfe4ed56
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-06-03 15:58:42 -0700

    Convert account address to properly formatted `0x...` string in account address schema

--- src/typescript/sdk/src/utils/validation/account-address.ts
+++ src/typescript/sdk/src/utils/validation/account-address.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const AccountAddressSchema = z
   .string()
-  .refine((arg) => AccountAddress.isValid({ input: arg }).valid, {
+  .refine((arg) => arg && AccountAddress.isValid({ input: arg }).valid, {
     message: "Invalid account address format",
-  });
+  })
+  .transform((v) => AccountAddress.from(v).toString());
