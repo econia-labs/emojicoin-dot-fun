186ddb822086adbd4b1557fdb131c4d4104f91e8
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-06-09 17:53:24 -0700

    Explain `e.preventDefault()` in `SharePopup`

--- src/typescript/frontend/src/components/pages/arena/tabs/profile-tab/melee-breakdown/SharePopup.tsx
+++ src/typescript/frontend/src/components/pages/arena/tabs/profile-tab/melee-breakdown/SharePopup.tsx
@@ -14,6 +14,9 @@ export default function SharePopup({ className }: { className?: ClassValue }) {
         className={cn("text-ec-blue cursor-pointer", className)}
         size={16}
         onClick={(e) => {
+          // Prevent default to avoid the popup appearing on mobile while the modal is open. Without this,
+          // the popup text won't close, because on mobile, `<Popup />` actually uses `Popover` internally, which
+          // is a tooltip on click, and remains indefinitely.
           e.preventDefault();
           setIsPnlModalOpen(true);
         }}
