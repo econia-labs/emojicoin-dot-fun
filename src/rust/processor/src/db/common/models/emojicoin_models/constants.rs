1e30a201b9ad53fcbb40162d901d4cd421d08c6b
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-06-23 16:36:54 -0700

    `contract` => `module`

--- src/rust/processor/src/db/common/models/emojicoin_models/constants.rs
+++ src/rust/processor/src/db/common/models/emojicoin_models/constants.rs
@@ -74,11 +74,11 @@ mod tests {
     use crate::utils::util::standardize_address;
 
     #[test]
-    fn ensure_contract_address_is_standardized() {
+    fn ensure_module_address_is_standardized() {
         if standardize_address(env!("EMOJICOIN_MODULE_ADDRESS")) != env!("EMOJICOIN_MODULE_ADDRESS")
         {
             panic!(
-                "The non-standardized contract address: {} is invalid because it doesn't match the standardized address: {}",
+                "The non-standardized module address: {} is invalid because it doesn't match the standardized address: {}",
                 env!("EMOJICOIN_MODULE_ADDRESS"),
                 standardize_address(env!("EMOJICOIN_MODULE_ADDRESS"))
             );
