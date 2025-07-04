945da1cdc55e9fe901a67813a90ba398deb90bc8
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-07-03 18:07:47 -0700

    Move some types/constants around

--- src/typescript/frontend/src/app/api/arena/candlesticks/fetch-cached-arena-candlesticks.ts
+++ src/typescript/frontend/src/app/api/arena/candlesticks/fetch-cached-arena-candlesticks.ts
@@ -3,30 +3,21 @@ import "server-only";
 import { unstable_cache } from "next/cache";
 
 import type { DatabaseJsonType, SupportedPeriod } from "@/sdk/index";
-import { Period, TableName } from "@/sdk/index";
+import { Period, PERIODS_IN_ONE_DAY, TableName } from "@/sdk/index";
 import { ORDER_BY, postgrest } from "@/sdk/indexer-v2";
 import type { AnyNumberString } from "@/sdk-types";
 
+// -------------------------------------------------------------------------------------------------
 // NOTE:
 // The explanations below are all based on the fact that the arena duration is less than one day.
 // If the arena duration is ever more than 24 hours, this endpoint will just not return all
 // candlesticks.
+// -------------------------------------------------------------------------------------------------
 
-const MAX_LIMIT = 500;
-
-const MINUTES_IN_ONE_DAY = 1440;
-const PERIODS_IN_ONE_DAY = {
-  [Period.Period1M]: MINUTES_IN_ONE_DAY,
-  [Period.Period5M]: MINUTES_IN_ONE_DAY / 5,
-  [Period.Period15M]: MINUTES_IN_ONE_DAY / 15,
-  [Period.Period30M]: MINUTES_IN_ONE_DAY / 30,
-  [Period.Period1H]: MINUTES_IN_ONE_DAY / 60,
-  [Period.Period4H]: MINUTES_IN_ONE_DAY / 240,
-  [Period.Period1D]: 1,
-} as const;
+const MAX_ROW_LIMIT = 500;
 
 // Quick sanity check at build time.
-if (Math.ceil(PERIODS_IN_ONE_DAY[Period.Period1M] / MAX_LIMIT) > 3) {
+if (Math.ceil(PERIODS_IN_ONE_DAY[Period.Period1M] / MAX_ROW_LIMIT) > 3) {
   throw new Error("The caching function wasn't designed to make more than three fetches.");
 }
 
@@ -38,7 +29,7 @@ const fetchCachedArenaCandlesticks = unstable_cache(
   async ({ meleeID, period }: { meleeID: AnyNumberString; period: SupportedPeriod }) => {
     const maxNumCandlesticks = PERIODS_IN_ONE_DAY[period];
     // Put a hard cap on the number of fetches to do.
-    const maxNumFetches = Math.ceil(maxNumCandlesticks / MAX_LIMIT);
+    const maxNumFetches = Math.ceil(maxNumCandlesticks / MAX_ROW_LIMIT);
 
     let i = 0;
     const res: DatabaseJsonType["arena_candlesticks"][] = [];
@@ -48,15 +39,14 @@ const fetchCachedArenaCandlesticks = unstable_cache(
         .select("*")
         .eq("melee_id", meleeID.toString())
         .eq("period", period)
-        .limit(MAX_LIMIT)
+        .limit(MAX_ROW_LIMIT)
         .order("start_time", ORDER_BY.ASC)
         .overrideTypes<DatabaseJsonType["arena_candlesticks"][]>()
         .then((res) => res.data ?? []);
 
-      // Unshift because it's in ascending order.
-      res.unshift(...next);
+      res.unshift(...next); // Unshift because it's in ascending order.
       i += 1;
-      if (next.length !== MAX_LIMIT) break;
+      if (next.length !== MAX_ROW_LIMIT) break;
     }
 
     return res;
