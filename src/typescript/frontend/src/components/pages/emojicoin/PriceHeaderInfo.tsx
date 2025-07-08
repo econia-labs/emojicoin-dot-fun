973ce8bc6971317ea8efa59c9eb24022158647ef
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-07-08 13:30:12 -0700

    Add more accurate avg execution price calculation function in case it's needed later
    Explain the avg exec price vs amm internal spot price disparity more explicitly

--- src/typescript/frontend/src/components/pages/emojicoin/PriceHeaderInfo.tsx
+++ src/typescript/frontend/src/components/pages/emojicoin/PriceHeaderInfo.tsx
@@ -4,7 +4,11 @@ export default function PriceHeaderInfo() {
   return (
     <div className="flex flex-row">
       Price
-      <Info infoIconClassName="w-[13px] ml-1 mb-[2px]">The average execution price</Info>
+      <Info infoIconClassName="w-[13px] ml-1 mb-[2px]">
+        The reported average execution price includes fees and may be affected by integer
+        truncation. In some cases, it may appear significantly different from the AMM&apos;s
+        internal spot price.
+      </Info>
     </div>
   );
 }
