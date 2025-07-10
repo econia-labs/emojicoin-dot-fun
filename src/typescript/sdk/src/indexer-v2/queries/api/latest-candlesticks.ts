cf9a09b10d80b2ea7398b4b0c9397da899c1633a
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-07-09 18:01:10 -0700

    Update the period types and latest candlesticks fetch helper functions to use the new stuff and throw if response is invalid
    Update tests to check the new latest candlesticks conversion function

--- src/typescript/sdk/src/indexer-v2/queries/api/latest-candlesticks.ts
+++ src/typescript/sdk/src/indexer-v2/queries/api/latest-candlesticks.ts
@@ -1,7 +1,46 @@
-import type { DatabaseJsonType } from "../../..";
-import { DatabaseRpc } from "../../..";
+import type { ArenaCandlestickModel, CandlestickModel, DatabaseJsonType } from "../../..";
+import { DatabaseRpc, Period, toArenaCandlestickModel, toCandlestickModel } from "../../..";
 import type { AnyNumberString } from "../../../types/types";
 import { postgrest } from "../client";
+
+export type HomogenousCandlesticksJson =
+  | DatabaseJsonType["candlesticks"][]
+  | DatabaseJsonType["arena_candlesticks"][];
+
+type HomogenousCandlestickModels = CandlestickModel[] | ArenaCandlestickModel[];
+
+/**
+ * A utility function to convert to the model while avoiding an unintended union and instead force a
+ * homogenous array output type.
+ */
+export const convertToCandlestickModels = (data: HomogenousCandlesticksJson) =>
+  "melee_id" in data[0]
+    ? (data as DatabaseJsonType["arena_candlesticks"][]).map(toArenaCandlestickModel)
+    : (data as DatabaseJsonType["candlesticks"][]).map(toCandlestickModel);
+
+const findOrThrow = <T extends HomogenousCandlestickModels>(models: T, period: Period) => {
+  const res = models.find((v) => v.period === period);
+  if (res) return res;
+  const msg = `Couldn't find \`${period}\` in periods: [${models.map((v) => v.period).join(",")}]`;
+  throw new Error(msg);
+};
+
+export function toLatestCandlesticks<
+  T extends HomogenousCandlesticksJson,
+  U extends T extends DatabaseJsonType["candlesticks"][] ? CandlestickModel : ArenaCandlestickModel,
+>(rows: T) {
+  const models = convertToCandlestickModels(rows);
+  return {
+    [Period.Period15S]: findOrThrow(models, Period.Period15S) as U,
+    [Period.Period1M]: findOrThrow(models, Period.Period1M) as U,
+    [Period.Period5M]: findOrThrow(models, Period.Period5M) as U,
+    [Period.Period15M]: findOrThrow(models, Period.Period15M) as U,
+    [Period.Period30M]: findOrThrow(models, Period.Period30M) as U,
+    [Period.Period1H]: findOrThrow(models, Period.Period1H) as U,
+    [Period.Period4H]: findOrThrow(models, Period.Period4H) as U,
+    [Period.Period1D]: findOrThrow(models, Period.Period1D) as U,
+  };
+}
 
 /**
  * Fetches the latest (aka current) arena candlesticks for a given meleeID.
@@ -11,12 +50,12 @@ import { postgrest } from "../client";
  */
 export const fetchArenaLatestCandlesticks = async (meleeID: AnyNumberString) =>
   await postgrest
-    .rpc(DatabaseRpc.ArenaLatestCandlesticks, { melee_id: meleeID.toString() }, { get: true })
+    .dbRpc(DatabaseRpc.ArenaLatestCandlesticks, { melee_id: meleeID.toString() }, { get: true })
     .select("*")
     .overrideTypes<DatabaseJsonType["arena_latest_candlesticks"][], { merge: false }>()
     // No rows returned means the arena hasn't been traded on yet, and it's confusing to return an
     // empty array when no candlesticks exist yet, so return `null` instead if there are no rows.
-    .then((res) => ((res.data?.length ?? 0) > 0 ? res.data : null));
+    .then((res) => (res.data?.length ? res.data : null));
 
 /**
  * Fetches the latest (aka current) market candlesticks for a given marketID.
@@ -25,9 +64,9 @@ export const fetchArenaLatestCandlesticks = async (meleeID: AnyNumberString) =>
  */
 export const fetchMarketLatestCandlesticks = async (marketID: AnyNumberString) =>
   await postgrest
-    .rpc(DatabaseRpc.MarketLatestCandlesticks, { market_id: marketID.toString() }, { get: true })
+    .dbRpc(DatabaseRpc.MarketLatestCandlesticks, { market_id: marketID.toString() }, { get: true })
     .select("*")
     .overrideTypes<DatabaseJsonType["market_latest_candlesticks"][], { merge: false }>()
     // No rows returned means the market doesn't exist, and it's confusing to return an empty array,
     // so return `null` instead if there are no rows.
-    .then((res) => ((res.data?.length ?? 0) > 0 ? res.data : null));
+    .then((res) => (res.data?.length ? res.data : null));
