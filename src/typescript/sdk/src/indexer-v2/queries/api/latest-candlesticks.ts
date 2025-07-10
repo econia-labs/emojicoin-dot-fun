aa54b8e7521dcddc00f8b5ea252315a21e891083
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-07-09 18:27:12 -0700

    Try to fix types and imports

--- src/typescript/sdk/src/indexer-v2/queries/api/latest-candlesticks.ts
+++ src/typescript/sdk/src/indexer-v2/queries/api/latest-candlesticks.ts
@@ -1,48 +1,7 @@
-import type { ArenaCandlestickModel, CandlestickModel, DatabaseJsonType } from "../../..";
-import { DatabaseRpc, Period, toArenaCandlestickModel, toCandlestickModel } from "../../..";
+import type { DatabaseJsonType } from "../../..";
+import { DatabaseRpc } from "../../..";
 import type { AnyNumberString } from "../../../types/types";
 import { postgrest } from "../client";
-
-export type HomogenousCandlesticksJson =
-  | DatabaseJsonType["candlesticks"][]
-  | DatabaseJsonType["arena_candlesticks"][];
-
-type HomogenousCandlestickModels = CandlestickModel[] | ArenaCandlestickModel[];
-
-/**
- * A utility function to convert to the model while avoiding an unintended union and instead force a
- * homogenous array output type.
- */
-export const convertToCandlestickModels = (data: HomogenousCandlesticksJson) =>
-  data.length === 0
-    ? []
-    : "melee_id" in data[0]
-      ? (data as DatabaseJsonType["arena_candlesticks"][]).map(toArenaCandlestickModel)
-      : (data as DatabaseJsonType["candlesticks"][]).map(toCandlestickModel);
-
-const findOrThrow = <T extends HomogenousCandlestickModels>(models: T, period: Period) => {
-  const res = models.find((v) => v.period === period);
-  if (res) return res;
-  const msg = `Couldn't find \`${period}\` in periods: [${models.map((v) => v.period).join(",")}]`;
-  throw new Error(msg);
-};
-
-export function toLatestCandlesticks<
-  T extends HomogenousCandlesticksJson,
-  U extends T extends DatabaseJsonType["candlesticks"][] ? CandlestickModel : ArenaCandlestickModel,
->(rows: T) {
-  const models = convertToCandlestickModels(rows);
-  return {
-    [Period.Period15S]: findOrThrow(models, Period.Period15S) as U,
-    [Period.Period1M]: findOrThrow(models, Period.Period1M) as U,
-    [Period.Period5M]: findOrThrow(models, Period.Period5M) as U,
-    [Period.Period15M]: findOrThrow(models, Period.Period15M) as U,
-    [Period.Period30M]: findOrThrow(models, Period.Period30M) as U,
-    [Period.Period1H]: findOrThrow(models, Period.Period1H) as U,
-    [Period.Period4H]: findOrThrow(models, Period.Period4H) as U,
-    [Period.Period1D]: findOrThrow(models, Period.Period1D) as U,
-  };
-}
 
 /**
  * Fetches the latest (aka current) arena candlesticks for a given meleeID.
