20b7032095755c233cf338338748ada509f90907
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-07-16 19:51:27 -0700

    Stabilize the cache for incomplete candlestick data by currying the function passed to `unstable_cache`

--- src/typescript/frontend/src/app/api/candlesticks/fetch-cached-candlesticks.ts
+++ src/typescript/frontend/src/app/api/candlesticks/fetch-cached-candlesticks.ts
@@ -4,6 +4,7 @@ import type { z } from "zod";
 
 import { fetchCandlesticksInRange } from "@/queries/market";
 import type { ChunkMetadata } from "@/sdk/indexer-v2/queries/api/candlesticks";
+import type { CandlesticksQueryArgs } from "@/sdk/indexer-v2/types/common";
 import type { AnyNumberString } from "@/sdk/types/types";
 
 import type { SupportedPeriodSchema } from "./supported-period-schema";
@@ -16,22 +17,41 @@ const fetchHistoricalCandlestickData = unstable_cache(fetchCandlesticksInRange, 
   tags: ["fetch-historical-candlestick-data"],
 });
 
-const fetchIncompleteCandlestickData = unstable_cache(fetchCandlesticksInRange, [], {
-  revalidate: INCOMPLETE_CACHE_REVALIDATION_TIME,
-  tags: ["fetch-incomplete-candlestick-data"],
-});
+// `unstable_cache` works by using the callback function's `cb.toString()` value as part of the
+// cache key. In order to ensure a hit despite varying input args, create the callback function
+// on the fly but ensure that `cb.toString()` is always constant by using a reference to the arg,
+const stableFetchIncompleteCandlestickData = ({
+  marketID,
+  period,
+  firstStartTime,
+  lastStartTime,
+}: CandlesticksQueryArgs) => {
+  const stableIncompleteFetcher = async ({
+    marketID,
+    period,
+  }: Omit<CandlesticksQueryArgs, "firstStartTime" | "lastStartTime">) => {
+    return fetchCandlesticksInRange({ marketID, period, firstStartTime, lastStartTime });
+  };
+
+  return unstable_cache(stableIncompleteFetcher, [], {
+    revalidate: INCOMPLETE_CACHE_REVALIDATION_TIME,
+    tags: ["fetch-incomplete-candlestick-data", marketID.toString(), period.toString()],
+  });
+};
 
 export async function fetchCachedChunkedCandlesticks(
   args: { marketID: AnyNumberString; period: z.infer<typeof SupportedPeriodSchema> } & ChunkMetadata
 ) {
   const { marketID, period, firstStartTime, lastStartTime, complete } = args;
 
-  const fetchFunction = complete ? fetchHistoricalCandlestickData : fetchIncompleteCandlestickData;
-
-  return await fetchFunction({
+  const fetchIncompleteCandlestickData = stableFetchIncompleteCandlestickData({
     marketID,
     period,
     firstStartTime,
     lastStartTime,
   });
+
+  return complete
+    ? await fetchHistoricalCandlestickData({ marketID, period, firstStartTime, lastStartTime })
+    : await fetchIncompleteCandlestickData({ marketID, period });
 }
