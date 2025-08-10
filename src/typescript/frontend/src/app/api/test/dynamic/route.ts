7fcf3aaadf8906d20e5d9b25458d1e946f233b7a
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-08-09 23:57:15 -0700

    Remove small commented line

--- src/typescript/frontend/src/app/api/test/dynamic/route.ts
+++ src/typescript/frontend/src/app/api/test/dynamic/route.ts
@@ -6,7 +6,6 @@ export const revalidate = 0;
 export const fetchCache = "force-no-store";
 
 export const GET = async (_req: NextRequest) => {
-  // const res = await fetchCachedDate();
   const url = new URL("/api/test/static", `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
   console.info(url);
   const res = await fetch(url).then((res) => res.json());
