4b46355379c92574bc8833a30e035e8a9543e98f
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-08-17 17:26:47 -0700

    Add workaround for no auth key yet

--- src/typescript/bff/src/middleware.ts
+++ src/typescript/bff/src/middleware.ts
@@ -1,7 +1,12 @@
-import type { NextRequest} from "next/server";
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+import { VERCEL_TARGET_ENV } from "@/sdk/const";
+
 export default async function middleware(request: NextRequest) {
+  if (VERCEL_TARGET_ENV === "preview") {
+    return NextResponse.next();
+  }
   const auth = request.headers.get("NEXT_PRIVATE_INTERNAL_AUTH_KEY");
   return auth === process.env.NEXT_PRIVATE_INTERNAL_AUTH_KEY
     ? NextResponse.next()
