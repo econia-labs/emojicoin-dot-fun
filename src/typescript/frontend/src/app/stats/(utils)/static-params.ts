0d3cb38e0afc3150816089047f4b2b9776bf61b6
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-08-25 20:50:27 -0700

    Format

--- src/typescript/frontend/src/app/stats/(utils)/static-params.ts
+++ src/typescript/frontend/src/app/stats/(utils)/static-params.ts
@@ -6,20 +6,17 @@ export async function generateStatsPageStaticParams() {
   const numMarkets = await fetchNumRegisteredMarkets();
   const maxPageNumber = getMaxPageNumber(numMarkets, STATS_MARKETS_PER_PAGE);
   return Array.from({ length: maxPageNumber }).flatMap((_, i) =>
-    statsSortByValues.flatMap(
-      (sort) =>
-        [
-          {
-            sort,
-            page: `${i + 1}`,
-            order: "asc",
-          } as const,
-          {
-            sort,
-            page: `${i + 1}`,
-            order: "desc",
-          } as const,
-        ]
-    )
+    statsSortByValues.flatMap((sort) => [
+      {
+        sort,
+        page: `${i + 1}`,
+        order: "asc",
+      } as const,
+      {
+        sort,
+        page: `${i + 1}`,
+        order: "desc",
+      } as const,
+    ])
   );
 }
